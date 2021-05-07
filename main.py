3bcf5d7fb5e81674d016f48e1d107b42e3459c4d
Rodja Trappe <rodja@zauberzeug.com>
2021-05-07 12:59:36 +0200

    adding timer

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from nice_gui import ui
+from datetime import datetime
 from icecream import ic
-
 
 ui.label('Hello, Nice GUI!')
 
-with ui.column() as col:
-    col.button('BUTTON', on_click=lambda _: col.label('Nice!'))
+with ui.column() as c:
+    c.button('BUTTON', on_click=lambda _: c.label('Nice!'))
+
+time = ui.label('Time: ')
+
+
+def update_time():
+    time.text = f'Time: {datetime.now().strftime("%H:%M:%S")}'
+    ic()
+
+
+ic()
+ui.timer(1, update_time)
--- nice_gui.py
+++ nice_gui.py
@@ -1,14 +1,18 @@
+from inspect import trace
 import traceback
+from typing import final
 import justpy as jp
 from icecream import ic
 from contextlib import contextmanager
+import asyncio
+import time
 
 
 class Group():
 
-    def label(self, text):
+    def label(self, text) -> jp.P:
 
-        p = jp.P(text=text, a=self.jp, classes='flex text-xl p-1 m-2')
+        return jp.P(text=text, a=self.jp, classes='flex text-xl p-1 m-2')
 
     def button(self, text, on_click=None):
 
@@ -20,10 +24,29 @@ class Group():
 
         d = jp.Div(text=text, a=self.jp, classes='w-48 text-xl m-2 p-1 bg-blue-700 text-white text-center')
         d.on('click', click)
+        return d
 
     @contextmanager
     def column(self):
         yield Column(self.jp)
+
+    def timer(self, invervall, callback):
+
+        async def loop():
+
+            while True:
+                start = time.time()
+                try:
+                    callback()
+                    jp.run_task(self.jp.update())
+                except:
+                    traceback.print_exc()
+                finally:
+                    dt = time.time() - start
+                    await asyncio.sleep(invervall - dt)
+
+        ic()
+        jp.run_task(loop())
 
 
 class Page(Group):
@@ -52,3 +75,4 @@ ui = jp.app
 ui.label = main.label
 ui.button = main.button
 ui.column = main.column
+ui.timer = main.timer
