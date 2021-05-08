e807bd3ce0b7723bf5e07cdc20c7df307a0ee1ea
Rodja Trappe <rodja@zauberzeug.com>
2021-05-08 06:46:31 +0200

    do not use global scope because otherwise reloading will dublicate UI

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from nice_gui import ui, app
+from nice_gui import NiceGui, app
 from datetime import datetime
 
-ui.label('Hello, Nice GUI!')
-ic()
-with ui.row() as row:
-    row.button('BUTTON', on_click=lambda: row.label('Nice!'))
-    with row.column() as column:
-        column.button('BUTTON2')
-        column.label("LABEL")
+with NiceGui() as ui:
+    ui.label('Hello, Nice GUI!')
+    with ui.row() as row:
+        row.button('BUTTON', on_click=lambda: row.label('Nice!'))
+        with row.column() as column:
+            column.button('BUTTON2')
+            column.label("LABEL")
 
-time = ui.label('Time:')
-def update_time():
-    time.text = f'Time: {datetime.now().strftime("%H:%M:%S")}'
-ui.timer(1.0, update_time)
+    time = ui.label('Time:')
+
+    def update_time():
+        time.text = f'Time: {datetime.now().strftime("%H:%M:%S")}'
+    ui.timer(1.0, update_time)
 
 if __name__ == '__main__':
     app.run()
--- nice_gui.py
+++ nice_gui.py
@@ -3,19 +3,21 @@ import justpy as jp
 from elements import Column, Page
 import icecream
 import traceback
+from contextlib import contextmanager
+
 
 icecream.install()
 
-page = Page()
-ic()
-ui = Column(page.view)
-jp.justpy(lambda: page.view, start_server=False)
+@contextmanager
+def NiceGui():
+    page = Page()
+    jp.justpy(lambda: page.view, start_server=False)
+    yield Column(page.view)
 
 app = jp.app
 
 
 def _run():
-    ic()
     uvicorn.run("main:app", host='0.0.0.0', port=80, lifespan='on', reload=True)
 
 app.run = _run
