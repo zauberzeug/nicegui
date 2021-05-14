25d2f084b3187071772019a286238f7c820e0cdd
Falko Schindler <falko@zauberzeug.com>
2021-05-14 09:27:25 +0200

    allow modifying element classes and style via builder pattern

--- nicegui/elements/element.py
+++ nicegui/elements/element.py
@@ -10,3 +10,23 @@ class Element:
         self.view_stack[-1].add(view)
         view.add_page(self.wp)
         self.view = view
+
+    def set_classes(self, classes: str):
+
+        self.view.classes = classes
+        return self
+
+    def add_classes(self, classes: str):
+
+        self.view.classes += ' ' + classes
+        return self
+
+    def set_style(self, style: str):
+
+        self.view.style = style
+        return self
+
+    def add_style(self, style: str):
+
+        self.view.style += ' ' + style
+        return self
