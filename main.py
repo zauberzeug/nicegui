ce8334c19b89d0e3f4f351f793f0c65a665a96ca
Falko Schindler <falko@zauberzeug.com>
2021-05-14 13:09:27 +0200

    radio and select: support options as list and dictionary

--- main.py
+++ main.py
@@ -17,8 +17,11 @@ with ui.row():
                 ui.input(label='Text', on_change=lambda e: output.set_text(e.value))
                 ui.number(label='Number', on_change=lambda e: output.set_text(e.value), value=3.1415927, decimals=2)
             with ui.column():
-                ui.radio(['A', 'B', 'C'], on_change=lambda e: output.set_text(e.value))
-                ui.select(['1', '2', '3'], on_change=lambda e: output.set_text(e.value))
+                ui.radio(options=['A', 'B', 'C'], value='A', on_change=lambda e: output.set_text(e.value))
+                ui.select(options=['a', 'b', 'c'], value='a', on_change=lambda e: output.set_text(e.value))
+            with ui.column():
+                ui.radio(options={1: 'A', 2: 'B', 3: 'C'}, on_change=lambda e: output.set_text(e.value))
+                ui.select(options={1: 'a', 2: 'b', 3: 'c'}, on_change=lambda e: output.set_text(e.value))
         with ui.row():
             ui.label('Output:')
             output = ui.label()
--- nicegui/elements/select.py
+++ nicegui/elements/select.py
@@ -1,13 +1,20 @@
 import justpy as jp
-from typing import Callable, List
+from typing import Callable, List, Dict, Union
 from .element import Element
 from ..utils import handle_exceptions, provide_arguments
 
 class Select(Element):
 
-    def __init__(self, options: List[str], value: str = None, on_change: Callable = None):
+    def __init__(self, options: Union[List, Dict], value: any = None, on_change: Callable = None):
 
-        view = jp.QSelect(value=value, options=options)
+        if isinstance(options, list):
+            options_ = options
+            value_ = value
+        else:
+            options_ = [{'label': value, 'value': key} for key, value in options.items()]
+            value_ = options.get(value)
+
+        view = jp.QSelect(value=value_, options=options_)
 
         if on_change is not None:
             view.on('input', handle_exceptions(provide_arguments(on_change, 'value')))
