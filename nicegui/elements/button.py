ffad1c4763bcba6cd0a249d7d139f362f20920e5
Falko Schindler <falko@zauberzeug.com>
2021-07-27 15:23:20 +0200

    text property for button element

--- nicegui/elements/button.py
+++ nicegui/elements/button.py
@@ -22,3 +22,32 @@ class Button(Element):
             view.on('click', handle_exceptions(provide_arguments(on_click)))
 
         super().__init__(view)
+
+    @property
+    def text(self):
+
+        return self.view.label
+
+    @text.setter
+    def text(self, text: any):
+
+        self.view.label = text
+
+    def set_text(self, text: str):
+
+        self.text = text
+
+    def bind_text_to(self, target, forward=lambda x: x):
+
+        self.text.bind_to(target, forward=forward, nesting=1)
+        return self
+
+    def bind_text_from(self, target, backward=lambda x: x):
+
+        self.text.bind_from(target, backward=backward, nesting=1)
+        return self
+
+    def bind_text(self, target, forward=lambda x: x, backward=lambda x: x):
+
+        self.text.bind(target, forward=forward, backward=backward, nesting=1)
+        return self
