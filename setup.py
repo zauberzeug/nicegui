39643192cd6cdcfdc281d5226a8fc6502ee65727
Rodja Trappe <rodja@zauberzeug.com>
2021-10-29 10:47:08 +0200

    adding mini setup py for local installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(
+    name='NiceGUI',
+    version='0.1',
+    author='Zauberzeug',
+    author_email='info@zauberzeug.com',
+    url='https://github.com/zauberzeug/nicegui/',
+    packages=['nicegui'],
+)
