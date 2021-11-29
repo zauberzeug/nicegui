9b957d741d98308e22f7df264d6feb8c0bd9cc20
Rodja Trappe <rodja@zauberzeug.com>
2021-11-29 06:56:16 +0100

    Revert "somehow binding on bidning brings image to full screen"
    This reverts commit b6000e6d2fcc7eb33c0dbed2d25bd5d602ceadba.

--- nicegui/elements/image.py
+++ nicegui/elements/image.py
@@ -25,14 +25,14 @@ class Image(Group):
         self.source = source
 
     def bind_source_to(self, target_object, target_name, forward=lambda x: x):
-        bind_to(self.view, 'src', target_object, target_name, forward=forward)
+        bind_to(self, 'source', target_object, target_name, forward=forward)
         return self
 
     def bind_source_from(self, target_object, target_name, backward=lambda x: x):
-        bind_from(self.view, 'src', target_object, target_name, backward=backward)
+        bind_from(self, 'source', target_object, target_name, backward=backward)
         return self
 
     def bind_source(self, target_object, target_name, forward=lambda x: x, backward=lambda x: x):
-        bind_from(self.view, 'src', target_object, target_name, backward=backward)
-        bind_to(self, 'src', target_object, target_name, forward=forward)
+        bind_from(self, 'source', target_object, target_name, backward=backward)
+        bind_to(self, 'source', target_object, target_name, forward=forward)
         return self
