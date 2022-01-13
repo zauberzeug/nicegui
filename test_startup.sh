4f17c924328985bd418c05966a5bbb6e3dbeac8d
Rodja Trappe <rodja@zauberzeug.com>
2022-01-13 10:17:20 +0100

    increased timeout to maybe fix the tests

--- test_startup.sh
+++ test_startup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 run() {
-    output=`{ timeout 5 python3 $1; } 2>&1`
+    output=`{ timeout 10 python3 $1; } 2>&1`
     exitcode=$?
     test $exitcode -eq 124 && exitcode=0 # exitcode 124 is comming from "timeout command above"
     echo $output | grep "JustPy ready to go" > /dev/null || exitcode=1
