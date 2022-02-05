391e866c34b9ce00b595b11d6c2261747879a9ee
Rodja Trappe <rodja@zauberzeug.com>
2022-02-05 08:42:36 +0100

    also build releases when tag with v* is pushed

---
+++ nicegui/helpers.py
@@ -0,0 +1,14 @@
+import inspect
+import time
+
+
+def measure(*, reset: bool = False, ms: bool = False):
+    global t
+    if 't' in globals() and not reset:
+        dt = time.time() - t
+        line = inspect.stack()[1][0].f_lineno
+        output = f'{dt * 1000:7.3f} ms' if ms else f'{dt:7.3f} s'
+        print(f'{inspect.stack()[1].filename}:{line}', output, flush=True)
+    if reset:
+        print('------------', flush=True)
+    t = time.time()
