dab4ddb5cd6fb6d5e50ff9dc19d9f227093bbf7a
Falko Schindler <falko@zauberzeug.com>
2022-04-12 13:35:06 +0200

    hide colors view completely

--- nicegui/elements/colors.js
+++ nicegui/elements/colors.js
@@ -1,5 +1,5 @@
 Vue.component("colors", {
-  template: `<span v-bind:id="jp_props.id"></span>`,
+  template: `<span v-bind:id="jp_props.id" style="display:none"></span>`,
   mounted() {
     var colors = this.$props.jp_props.options;
     for (var color in colors) {
