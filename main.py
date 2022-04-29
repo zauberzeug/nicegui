4e9967269ac6f36a0d90d4b4a3224e26d84113b1
Falko Schindler <falko@zauberzeug.com>
2022-04-29 07:28:47 +0200

    improve back-to-main-page links

--- main.py
+++ main.py
@@ -492,11 +492,11 @@ with example(ui.link):
 with example(ui.page):
     with ui.page('/other_page'):
         ui.label('Welcome to the other side')
-        ui.link('Back to main page', '')
+        ui.link('Back to main page', '#page')
 
     with ui.page('/dark_page', dark=True):
         ui.label('Welcome to the dark side')
-        ui.link('Back to main page', '')
+        ui.link('Back to main page', '#page')
 
     ui.link('Visit other page', 'other_page')
     ui.link('Visit dark page', 'dark_page')
