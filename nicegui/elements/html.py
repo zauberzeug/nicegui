0cbda33648b2a0cd43379f0b67737f3ba3619d58
Rodja Trappe <rodja@zauberzeug.com>
2022-08-01 05:51:08 +0200

    added some documentation for ui.add_body/head_html

--- nicegui/elements/html.py
+++ nicegui/elements/html.py
@@ -11,6 +11,8 @@ class Html(Element):
         """HTML Element
 
         Renders arbitrary HTML onto the page. `Tailwind <https://tailwindcss.com/>`_ can be used for styling.
+        You can also use `ui.add_head_html` to add html code into the head of the document and `ui.add_body_html`
+        to add it into the body.
 
         :param content: the HTML code to be displayed
         """
