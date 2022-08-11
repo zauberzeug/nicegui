54aa9da04944437513d0967e383d7eaa895ec477
Falko Schindler <falko@zauberzeug.com>
2022-08-11 07:45:57 +0200

    fix documentation

--- nicegui/timer.py
+++ nicegui/timer.py
@@ -25,7 +25,7 @@ class Timer:
         A timer will execute a callback repeatedly with a given interval.
 
         :param interval: the interval in which the timer is called (can be changed during runtime)
-        :param callback: function or coroutine to execute when interval elapses (can return `False` to prevent view update)
+        :param callback: function or coroutine to execute when interval elapses
         :param active: whether the callback should be executed or not (can be changed during runtime)
         :param once: whether the callback is only executed once after a delay specified by `interval` (default: `False`)
         """
