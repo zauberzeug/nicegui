8d11d747f41d8ab19886a5423f142375580180af
Falko Schindler <falko@zauberzeug.com>
2022-09-13 10:35:24 +0200

    raise exception when ui.shutdown is called with auto-reload activated

--- nicegui/lifecycle.py
+++ nicegui/lifecycle.py
@@ -22,6 +22,8 @@ def on_shutdown(self, handler: Union[Callable, Awaitable]):
 
 
 async def shutdown(self) -> None:
+    if globals.config.reload:
+        raise Exception('ui.shutdown is not supported when auto-reload is enabled')
     for socket in [s for page in jp.WebPage.sockets.values() for s in page.values()]:
         await socket.close()
     globals.server.should_exit = True
