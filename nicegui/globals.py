ee0fb9bdc5abcf4b86b152c9899a7e8dc4b79e85
Falko Schindler <falko@zauberzeug.com>
2022-09-23 18:06:42 +0200

    keep one view stack per task to allow async UI manipulation without mixing up the containers

--- nicegui/globals.py
+++ nicegui/globals.py
@@ -18,7 +18,7 @@ config: Optional[Config] = None
 server: Optional[Server] = None
 loop: Optional[asyncio.AbstractEventLoop] = None
 page_builders: Dict[str, 'PageBuilder'] = {}
-view_stack: List[jp.HTMLBaseComponent] = []
+view_stacks: Dict[List[jp.HTMLBaseComponent]] = {}
 tasks: List[asyncio.tasks.Task] = []
 log: logging.Logger = logging.getLogger('nicegui')
 connect_handlers: List[Union[Callable, Awaitable]] = []
@@ -34,11 +34,29 @@ def find_route(function: Callable) -> str:
     return routes[0]
 
 
+def get_task_id() -> int:
+    return id(asyncio.current_task()) if loop and loop.is_running() else 0
+
+
+def get_view_stack() -> List[jp.HTMLBaseComponent]:
+    task_id = get_task_id()
+    if task_id not in view_stacks:
+        view_stacks[task_id] = []
+    return view_stacks[task_id]
+
+
+def prune_view_stack() -> None:
+    task_id = get_task_id()
+    if not view_stacks[task_id]:
+        del view_stacks[task_id]
+
+
 @contextmanager
 def within_view(view: jp.HTMLBaseComponent) -> Generator[None, None, None]:
     child_count = len(view)
-    view_stack.append(view)
+    get_view_stack().append(view)
     yield
-    view_stack.pop()
+    get_view_stack().pop()
+    prune_view_stack()
     if len(view) != child_count:
         create_task(view.update())
