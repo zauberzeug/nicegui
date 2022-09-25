a50bc2a9880b14fe6f522ecf5f878f070563e907
Rodja Trappe <rodja@zauberzeug.com>
2022-09-25 07:01:28 +0200

    pytest: adding more analysis

--- tests/test_3d_scene.py
+++ tests/test_3d_scene.py
@@ -12,14 +12,16 @@ def test_moving_sphere_with_timer(screen: Screen):
     screen.open('/')
 
     def position():
-        for i in range(5):
+        for i in range(20):
             try:
                 pos = screen.selenium.execute_script('return scene.children[4].position.y')
+                ic(pos)
                 if pos is not None:
                     return pos
             except JavascriptException:
                 pass
             screen.wait(0.1)
+        raise Exception('Could not get position')
 
     pos = position()
     screen.wait(0.1)
