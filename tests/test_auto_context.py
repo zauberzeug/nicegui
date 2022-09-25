df6498c8ca55e35df9f3e8ab8e9b943074744abc
Rodja Trappe <rodja@zauberzeug.com>
2022-09-25 19:59:11 +0200

    fixed name

--- tests/test_auto_context.py
+++ tests/test_auto_context.py
@@ -45,6 +45,6 @@ card
 card
   B
 ''' in screen.render_content():
-        user.wait(0.1)
+        screen.wait(0.1)
     else:
         raise AssertionError(f'{screen.render_content()} should show cards with "A" and "B"')
