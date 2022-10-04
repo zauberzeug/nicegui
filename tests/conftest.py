3de7d9b60fc4a2f77fb0eab7c0d46db04649636f
Rodja Trappe <rodja@zauberzeug.com>
2022-10-04 20:02:39 +0200

    repair cleanup fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -49,8 +49,9 @@ def reset_globals() -> Generator[None, None, None]:
 
 @pytest.fixture(scope='session', autouse=True)
 def remove_all_screenshots() -> None:
-    for name in os.listdir(Screen.SCREENSHOT_DIR):
-        os.remove(os.path.join(Screen.SCREENSHOT_DIR, name))
+    if os.path.exists(Screen.SCREENSHOT_DIR):
+        for name in os.listdir(Screen.SCREENSHOT_DIR):
+            os.remove(os.path.join(Screen.SCREENSHOT_DIR, name))
 
 
 @pytest.fixture
