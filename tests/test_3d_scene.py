2a2cd2a5d156f327261a0e9b2a726ff848ac15c7
Falko Schindler <falko@zauberzeug.com>
2022-10-13 11:25:47 +0200

    fix 3d scene test

--- tests/test_3d_scene.py
+++ tests/test_3d_scene.py
@@ -6,15 +6,15 @@ from .screen import Screen
 
 def test_moving_sphere_with_timer(screen: Screen):
     with ui.scene() as scene:
-        sphere = scene.sphere().move(0, -5, 2)
-        ui.timer(0.03, lambda: sphere.move(sphere.x, sphere.y + 0.05, sphere.z))
+        sphere = scene.sphere()
+        ui.timer(0.03, lambda: sphere.move(0, 0, sphere.z + 0.01))
 
     screen.open('/')
 
     def position() -> None:
         for _ in range(20):
             try:
-                pos = screen.selenium.execute_script('return scene.children[4].position.y')
+                pos = screen.selenium.execute_script('return scene.children[4].position.z')
                 if pos is not None:
                     return pos
             except JavascriptException:
@@ -22,6 +22,5 @@ def test_moving_sphere_with_timer(screen: Screen):
             screen.wait(0.1)
         raise Exception('Could not get position')
 
-    pos = position()
     screen.wait(0.2)
-    assert position() > pos
+    assert position() > 0
