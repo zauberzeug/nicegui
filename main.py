14b464884e562ec9d9885f18b06b196c4e5a1899
Rodja Trappe <rodja@zauberzeug.com>
2022-12-03 06:51:54 +0100

    forgotten files

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from website import demo_card, reference
 from website.constants import ACCENT_COLOR, HEADER_HEIGHT, STATIC
 from website.example import bash_window, python_window
 
+ACCENT_COLOR = '#428BF5'
+HEADER_HEIGHT = '70px'
+STATIC = Path(__file__).parent / 'website' / 'static'
+
 ui.add_static_files('/favicon', Path(__file__).parent / 'website' / 'favicon')
 
 
