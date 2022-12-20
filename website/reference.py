0075e1f6915076febc7411f0b4cb4e62bbaf65cd
Falko Schindler <falko@zauberzeug.com>
2022-12-20 16:17:36 +0100

    move fastapi example so that links match

--- website/reference.py
+++ website/reference.py
@@ -771,7 +771,7 @@ You can also set `respond=False` to send a command without waiting for a respons
         ui.add_static_files('/examples', 'examples')
         ui.label('Some NiceGUI Examples').classes('text-h5')
         ui.link('AI interface', '/examples/ai_interface/main.py')
-        ui.link('Custom FastAPI app', '/examples/custom_fastapi_app/main.py')
+        ui.link('Custom FastAPI app', '/examples/fastapi/main.py')
         ui.link('Authentication', '/examples/authentication/main.py')
 
     @example('''#### API Responses
