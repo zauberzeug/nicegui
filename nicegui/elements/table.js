55f1e43de1fc7e4c8590552a5a0ffc7240a61eee
Falko Schindler <falko@zauberzeug.com>
2023-01-05 16:28:45 +0100

    #228 add support for checkboxRenderer in ui.table

--- nicegui/elements/table.js
+++ nicegui/elements/table.js
@@ -15,6 +15,32 @@ export default {
           this.gridOptions.columnDefs[column].cellRenderer = (params) => (params.value ? params.value : "");
         }
       }
+
+      // Code for CheckboxRenderer https://blog.ag-grid.com/binding-boolean-values-to-checkboxes-in-ag-grid/
+      function CheckboxRenderer() {}
+      CheckboxRenderer.prototype.init = function (params) {
+        this.params = params;
+        this.eGui = document.createElement("input");
+        this.eGui.type = "checkbox";
+        this.eGui.checked = params.value;
+        this.checkedHandler = this.checkedHandler.bind(this);
+        this.eGui.addEventListener("click", this.checkedHandler);
+      };
+      CheckboxRenderer.prototype.checkedHandler = function (e) {
+        let checked = e.target.checked;
+        let colId = this.params.column.colId;
+        this.params.node.setDataValue(colId, checked);
+      };
+      CheckboxRenderer.prototype.getGui = function (params) {
+        return this.eGui;
+      };
+      CheckboxRenderer.prototype.destroy = function (params) {
+        this.eGui.removeEventListener("click", this.checkedHandler);
+      };
+      this.gridOptions.components = {
+        checkboxRenderer: CheckboxRenderer,
+      };
+
       this.grid = new agGrid.Grid(this.$el, this.gridOptions);
       this.gridOptions.api.addGlobalListener(this.handle_event);
     },
