477805dd60bdb0067d91f0d82368c8158717f6e0
Rodja Trappe <rodja@zauberzeug.com>
2023-01-09 05:34:22 +0100

    reworked documentation

--- nicegui/functions/lifecycle.py
+++ nicegui/functions/lifecycle.py
@@ -1,13 +1,13 @@
 '''Lifecycle functions
 
-You can run a function or coroutine as a parallel task by passing it to one of the following register methods:
+You can register coroutines or functions to be called for the following events:
 
 - `ui.lifecycle.on_startup`: Called when NiceGUI is started or restarted.
 - `ui.lifecycle.on_shutdown`: Called when NiceGUI is shut down or restarted.
 - `ui.lifecycle.on_connect`: Called for each client which connects. (nicegui.Client is passed as optional argument)
 - `ui.lifecycle.on_disconnect`: Called for each client which disconnects. (nicegui.Client is passed as optional argument)
 
-When NiceGUI is shut down or restarted, the startup tasks will be automatically canceled.
+When NiceGUI is shut down or restarted, the all tasks still in execution will be automatically canceled.
 '''
 from typing import Awaitable, Callable, Union
 
@@ -41,6 +41,10 @@ def on_shutdown(handler: Union[Callable, Awaitable]) -> None:
 
 
 async def shutdown() -> None:
+    '''Programmatically shut down NiceGUI.
+
+    Only possible when auto-reload is disabled.'''
+
     if globals.reload:
         raise Exception('ui.shutdown is not supported when auto-reload is enabled')
     globals.server.should_exit = True
