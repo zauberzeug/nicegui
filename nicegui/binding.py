52bb564d05f00c472ceda6446a39694f8e24d28f
Falko Schindler <falko@zauberzeug.com>
2023-01-10 18:23:09 +0100

    avoid holding object references in binding loop

--- nicegui/binding.py
+++ nicegui/binding.py
@@ -21,6 +21,7 @@ async def loop():
             if getattr(target_obj, target_name) != value:
                 setattr(target_obj, target_name, value)
                 propagate(target_obj, target_name, visited)
+            del link, source_obj, target_obj
         if time.time() - t > 0.01:
             logging.warning(f'binding propagation for {len(active_links)} active links took {time.time() - t:.3f} s')
         await asyncio.sleep(globals.binding_refresh_interval)
