6f81dd7244699b72dc11c27d7a8d6e37505c6c22
Falko Schindler <falko@zauberzeug.com>
2023-01-12 16:33:05 +0100

    #240 avoid duplicate favicon routes for index page

--- nicegui/favicon.py
+++ nicegui/favicon.py
@@ -13,8 +13,9 @@ def create_favicon_route(path: str, favicon: Optional[str]) -> None:
     if favicon and is_remote_url(favicon):
         return
     fallback = Path(__file__).parent / 'static' / 'favicon.ico'
-    globals.app.add_route(f'{"" if path == "/" else path}/favicon.ico',
-                          lambda _: FileResponse(favicon or globals.favicon or fallback))
+    path = f'{"" if path == "/" else path}/favicon.ico'
+    globals.app.remove_route(path)
+    globals.app.add_route(path, lambda _: FileResponse(favicon or globals.favicon or fallback))
 
 
 def get_favicon_url(page: 'page', prefix: str) -> str:
