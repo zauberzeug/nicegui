8ab4c684f251e69290413fd6f4c7a9ae9afd8e87
Falko Schindler <falko@zauberzeug.com>
2023-01-25 21:13:28 +0100

    #263 add more tests for ui.date

--- tests/test_date.py
+++ tests/test_date.py
@@ -11,3 +11,42 @@ def test_date(screen: Screen):
 
     screen.click('31')
     screen.should_contain('Tue, Jan 31')
+
+
+def test_date_with_range(screen: Screen):
+    ui.date().props('range')
+
+    screen.open('/')
+    screen.click('16')
+    screen.click('19')
+    screen.should_contain('4 days')
+
+    screen.click('26')
+    screen.click('29')
+    screen.should_contain('4 days')
+
+
+def test_date_with_multi_selection(screen: Screen):
+    ui.date().props('multiple')
+
+    screen.open('/')
+    screen.click('16')
+    screen.click('19')
+    screen.should_contain('2 days')
+
+    screen.click('26')
+    screen.click('29')
+    screen.should_contain('4 days')
+
+
+def test_date_with_range_and_multi_selection(screen: Screen):
+    ui.date().props('range multiple')
+
+    screen.open('/')
+    screen.click('16')
+    screen.click('19')
+    screen.should_contain('4 days')
+
+    screen.click('26')
+    screen.click('29')
+    screen.should_contain('8 days')
