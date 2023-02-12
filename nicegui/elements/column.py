3cbdca6cbcfe03c1a4b8f72d32b7e6cae5ea6b8d
Rodja Trappe <rodja@zauberzeug.com>
2023-02-12 06:45:27 +0100

    using tailwind classes for row/column

--- nicegui/elements/column.py
+++ nicegui/elements/column.py
@@ -9,4 +9,4 @@ class Column(Element):
         Provides a container which arranges its child in a row.
         '''
         super().__init__('div')
-        self.classes('column items-start gap-4')
+        self.classes('flex flex-column items-start gap-4')
--- nicegui/elements/row.py
+++ nicegui/elements/row.py
@@ -9,4 +9,4 @@ class Row(Element):
         Provides a container which arranges its child in a row.
         '''
         super().__init__('div')
-        self.classes('row items-start gap-4')
+        self.classes('flex flex-row items-start gap-4')
