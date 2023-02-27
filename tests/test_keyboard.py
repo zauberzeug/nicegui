bd0dd2971fb08b3e4988f30dcdd45cd882cd0d16
Falko Schindler <falko@zauberzeug.com>
2023-02-27 17:08:17 +0100

    fix keyboard test (after removing the invisible element in a previous commit)

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -1,5 +1,3 @@
-from selenium.webdriver.common.by import By
-
 from nicegui import ui
 
 from .screen import Screen
@@ -7,10 +5,9 @@ from .screen import Screen
 
 def test_keyboard(screen: Screen):
     result = ui.label()
-    keyboard = ui.keyboard(on_key=lambda e: result.set_text(f'{e.key, e.action}'))
+    ui.keyboard(on_key=lambda e: result.set_text(f'{e.key, e.action}'))
 
     screen.open('/')
-    assert screen.selenium.find_element(By.ID, keyboard.id)
     screen.wait(1.0)
     screen.type('t')
     screen.should_contain('t, KeyboardAction(keydown=False, keyup=True, repeat=False)')
