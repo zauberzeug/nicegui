1fc253529a76c6812fcb367c4c86c1cb513262e7
Rino Beeli <40661605+rbeeli@users.noreply.github.com>
2023-03-01 17:20:22 +0100

    Remove unnecessary "import json" statements

--- nicegui/client.py
+++ nicegui/client.py
@@ -1,5 +1,4 @@
 import asyncio
-import json
 import time
 import uuid
 from pathlib import Path
--- nicegui/element.py
+++ nicegui/element.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import json
 import re
 from abc import ABC
 from copy import deepcopy
