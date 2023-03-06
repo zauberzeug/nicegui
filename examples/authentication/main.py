bdaab55d2db09f1dde63bd7dddf71621bec37479
Rodja Trappe <rodja@zauberzeug.com>
2023-03-06 05:10:18 +0100

    restructured and enhanced auth example

--- examples/authentication/main.py
+++ examples/authentication/main.py
@@ -29,36 +29,37 @@ def is_authenticated(request: Request) -> bool:
 
 @ui.page('/')
 def main_page(request: Request) -> None:
-    if is_authenticated(request):
-        session = session_info[request.session['id']]
-        with ui.column().classes('absolute-center items-center'):
-            ui.label(f'Hello {session["username"]}!').classes('text-2xl')
-            ui.button('', on_click=lambda: ui.open('/logout')).props('outline round icon=logout')
-    else:
+    if not is_authenticated(request):
         return RedirectResponse('/login')
+    session = session_info[request.session['id']]
+    with ui.column().classes('absolute-center items-center'):
+        ui.label(f'Hello {session["username"]}!').classes('text-2xl')
+        ui.button('', on_click=lambda: ui.open('/logout')).props('outline round icon=logout')
 
 
 @ui.page('/login')
 def login(request: Request) -> None:
+    def try_login() -> None:  # local function to avoid passing username and password as arguments
+        if (username.value, password.value) in users:
+            session_info[request.session['id']] = {'username': username.value, 'authenticated': True}
+            ui.open('/')
+        else:
+            ui.notify('Wrong username or password', color='negative')
+
     if is_authenticated(request):
         return RedirectResponse('/')
     request.session['id'] = str(uuid.uuid4())  # NOTE this stores a new session ID in the cookie of the client
     with ui.card().classes('absolute-center'):
-        username = ui.input('Username')
-        password = ui.input('Password').classes('w-full').props('type=password')
-        ui.button('Log in', on_click=lambda: try_login(request.session['id'], username.value, password.value))
-
-
-def try_login(session_id: str, username: str, password: str) -> None:
-    if (username, password) in users:
-        session_info[session_id] = {'username': username, 'authenticated': True}
-    ui.open('/')
+        username = ui.input('Username').on('keydown.enter', try_login)
+        password = ui.input('Password').props('type=password').on('keydown.enter', try_login)
+        ui.button('Log in', on_click=try_login)
 
 
 @ui.page('/logout')
 def logout(request: Request) -> None:
     if is_authenticated(request):
         session_info.pop(request.session['id'])
+        request.session['id'] = None
         return RedirectResponse('/login')
     return RedirectResponse('/')
 
