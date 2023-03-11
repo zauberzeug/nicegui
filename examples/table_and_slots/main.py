943be81fa78f64f4cf86421825b391a6f6abff9a
Dominique CLAUSE <dom.clause@gmail.com>
2023-03-11 18:36:08 +0100

    Add an example at using the QTable component for more complex use cases.

---
+++ examples/table_and_slots/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from nicegui import ui
+
+
+class Demo:
+    filter = ''
+    fields = [
+        {'name': 'name', 'label': 'Name', 'field': 'name', 'required': True},
+        {'name': 'age', 'label': 'Age', 'field': 'age', 'sortable': True},
+    ]
+
+    data = [
+        {'name': 'Alice', 'age': 18},
+        {'name': 'Bob', 'age': 21},
+        {'name': 'Carol'},
+    ]
+
+
+with ui.table(title='Table', columns=Demo.fields, rows=Demo.data, selection='single') \
+        .bind_value(Demo, 'filter') as table:
+    with table.add_slot('top-right'):
+        with ui.input(placeholder='Search').props('type="search"').bind_value(Demo, 'filter') as search:
+            with search.add_slot('append'):
+                ui.icon('search')
+
+    with table.add_slot('top-row'):
+        with ui.tr():
+            with ui.td().props('colspan="100%"'):
+                ui.label('This is a top row').classes('text-center')
+
+    with table.add_slot('bottom-row'):
+        with ui.tr():
+            with ui.td().props('colspan="100%"'):
+                ui.label('This is a bottom row').classes('text-center')
+
+    with table.add_slot('bottom'):
+        ui.label('Bottom slot')
+
+ui.label('').bind_text_from(table, 'selected', lambda val: f'Current selection: {val.__repr__()}')
+
+ui.run()
--- main.py
+++ main.py
@@ -244,6 +244,7 @@ The command searches for `main.py` in in your current directory and makes the ap
             example_link('Search as you type', 'using public API of thecocktaildb.com to search for cocktails')
             example_link('Menu and Tabs', 'uses Quasar to create foldable menu and tabs inside a header bar')
             example_link('Trello Cards', 'shows Trello-like cards that can be dragged and dropped into columns')
+            example_link('Table and slots', 'shows how to use a component slot with the example of the QTable component')
 
     with ui.row().classes('bg-primary w-full min-h-screen mt-16'):
         link_target('why')
