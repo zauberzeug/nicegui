55d55d96f00515b5199ca5d1a26196a81f863c76
Alon Budker <alonio6@gmail.com>
2023-03-12 14:51:12 +0200

    fix: Aligned mute and unmute buttons with their actual function

--- website/reference.py
+++ website/reference.py
@@ -260,8 +260,8 @@ To overlay an SVG, make the `viewBox` exactly the size of the image and provide 
         a = ui.audio('https://cdn.pixabay.com/download/audio/2022/02/22/audio_d1718ab41b.mp3')
         a.on('ended', lambda _: ui.notify('Audio playback completed'))
 
-        ui.button(on_click=lambda: a.props('muted')).props('outline icon=volume_up')
-        ui.button(on_click=lambda: a.props(remove='muted')).props('outline icon=volume_off')
+        ui.button(on_click=lambda: a.props('muted')).props('outline icon=volume_off')
+        ui.button(on_click=lambda: a.props(remove='muted')).props('outline icon=volume_up')
 
     @example(ui.video, menu)
     def image_example():
