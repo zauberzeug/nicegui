e1b3bac74f0e0ca7a8ac9701db57f79cec106fcb
Rino Beeli <40661605+rbeeli@users.noreply.github.com>
2023-03-16 01:06:11 +0100

    Support fallback to Python's json module if orjson not available

--- nicegui/json/__init__.py
+++ nicegui/json/__init__.py
@@ -1,15 +1,31 @@
 """
 Custom json module. Provides dumps and loads implementations
-wrapping the `orjson` package.
+wrapping the orjson package. If the orjson package is not available,
+the standard Python json module is used.
 
-Custom module required in order to override json-module used
+This custom module is required in order to override the json-module used
 in socketio.AsyncServer, which expects a module as parameter
 to override Python's default json module.
 """
 
-from nicegui.json.orjson_wrapper import dumps, loads
+try:
+    # orjson not available on all platforms, fallback to Python's json module if not available
+    import orjson
+    raise ImportError()
+    has_orjson = True
+except:
+    has_orjson = False
+    print('Using Python json')
+
+
+if has_orjson:
+    from nicegui.json.orjson_wrapper import NiceGUIJSONResponse, dumps, loads
+else:
+    from nicegui.json.builtin_wrapper import NiceGUIJSONResponse, dumps, loads
+
 
 __all__ = [
     'dumps',
-    'loads'
+    'loads',
+    'NiceGUIJSONResponse'
 ]
