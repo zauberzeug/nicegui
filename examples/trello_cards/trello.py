f40bbe1a9e58278100c9accb0d1438cc61bc6ba9
Rodja Trappe <rodja@zauberzeug.com>
2023-03-17 15:45:46 +0100

    some more styling

--- examples/trello_cards/trello.py
+++ examples/trello_cards/trello.py
@@ -9,17 +9,17 @@ class column(ui.column):
 
     def __init__(self, name: str) -> None:
         super().__init__()
-        with self.classes('bg-gray-200 w-60 p-4 rounded shadow-2'):
-            ui.label(name).classes('text-bold')
+        with self.classes('bg-grey-5 w-60 p-4 rounded shadow-2'):
+            ui.label(name).classes('text-bold ml-1')
         self.on('dragover.prevent', self.highlight)
         self.on('dragleave', self.unhighlight)
         self.on('drop', self.move_card)
 
     def highlight(self) -> None:
-        self.classes(add='bg-gray-400')
+        self.classes(add='bg-grey-2')
 
     def unhighlight(self) -> None:
-        self.classes(remove='bg-gray-400')
+        self.classes(remove='bg-grey-2')
 
     def move_card(self) -> None:
         self.unhighlight()
@@ -34,7 +34,7 @@ class card(ui.card):
     def __init__(self, text: str) -> None:
         super().__init__()
         self.text = text
-        with self.props('draggable').classes('w-full cursor-pointer'):
+        with self.props('draggable').classes('w-full cursor-pointer bg-grey-1'):
             ui.label(self.text)
         self.on('dragstart', self.handle_dragstart)
 
