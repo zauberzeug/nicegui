f16f51f454fb8fcdbbc14c9912c92e410dcba8ca
Rodja Trappe <rodja@zauberzeug.com>
2023-03-18 14:57:31 +0100

    renaming

--- examples/trello_cards/main.py
+++ examples/trello_cards/main.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-import trello
+import draganddrop as dnd
 
 from nicegui import ui
 
 with ui.row():
-    with trello.column('Next'):
-        trello.card('Improve Documentation')
-        trello.card('Simplify Layouting')
-        trello.card('Provide Deployment')
-    with trello.column('Doing'):
-        trello.card('Release Standalone-Mode')
-    with trello.column('Done'):
-        trello.card('Invent NiceGUI')
-        trello.card('Test in own Projects')
-        trello.card('Publish as Open Source')
+    with dnd.column('Next'):
+        dnd.card('Improve Documentation')
+        dnd.card('Simplify Layouting')
+        dnd.card('Provide Deployment')
+    with dnd.column('Doing'):
+        dnd.card('Release Standalone-Mode')
+    with dnd.column('Done'):
+        dnd.card('Invent NiceGUI')
+        dnd.card('Test in own Projects')
+        dnd.card('Publish as Open Source')
 
 ui.run(standalone=True)
