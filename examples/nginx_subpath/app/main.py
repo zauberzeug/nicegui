c49168d30c16d645c6ef146b639d2bcb1cf32a5a
Rodja Trappe <rodja@zauberzeug.com>
2023-03-19 06:56:27 +0100

    styling

--- examples/nginx_subpath/app/main.py
+++ examples/nginx_subpath/app/main.py
@@ -10,7 +10,7 @@ def subpage():
 
 @ui.page('/')
 def index():
-    with ui.card().classes('mx-auto px-24 pt-12 pb-24 items-center'):
+    with ui.card().classes('mx-auto px-24 pt-12 pb-24 items-center text-center'):
         ui.label('This demonstrates hosting of a NiceGUI app on a subpath.').classes('text-h5')
         ui.label('As you can see the entire app is available below "/nicegui".').classes('text-lg')
         ui.label('But the code here does not need to know that.').classes('text-lg')
