5d6945c3434cc67cb961983669084748aa4d9c35
Rodja Trappe <rodja@zauberzeug.com>
2023-03-19 05:50:02 +0100

    cleanup, styling and fixing subpage link

--- examples/nginx_subpath/app/main.py
+++ examples/nginx_subpath/app/main.py
@@ -3,7 +3,9 @@ from nicegui import ui
 
 @ui.page('/subpage')
 def subpage():
-    ui.label('This is a subpage.')
+    ui.label('This is a subpage').classes('text-h5 mx-auto mt-12')
+    # TODO: this is not working properly yet
+    # ui.button('back', on_click=lambda: ui.open('/')).classes('mx-auto')
 
 
 @ui.page('/')
@@ -12,7 +14,7 @@ def index():
         ui.label('This demonstrates hosting of a NiceGUI app on a subpath.').classes('text-h5')
         ui.label('As you can see the entire app is available below "/nicegui".').classes('text-lg')
         ui.label('But the code here does not need to know that.').classes('text-lg')
-        ui.link('Navigate to a subpage.', subpage).classes('text-lg')
+        ui.link('Navigate to a subpage.', 'subpage').classes('text-lg')
 
 
 ui.run()
