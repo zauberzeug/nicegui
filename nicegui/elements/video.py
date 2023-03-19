9f8002a92ad11701a63f9c0573d7ea2adadff3a8
Falko Schindler <falko@zauberzeug.com>
2023-03-19 11:19:26 +0100

    #555 allow looping audio and video

--- nicegui/elements/video.py
+++ nicegui/elements/video.py
@@ -7,7 +7,11 @@ register_component('video', __file__, 'video.js')
 class Video(Element):
 
     def __init__(self, src: str, *,
-                 type: str = 'video/mp4', controls: bool = True, autoplay: bool = False, muted: bool = False) -> None:
+                 type: str = 'video/mp4',
+                 controls: bool = True,
+                 autoplay: bool = False,
+                 muted: bool = False,
+                 loop: bool = False) -> None:
         """Video
 
         :param src: URL of the video source
@@ -15,6 +19,7 @@ class Video(Element):
         :param controls: whether to show the video controls, like play, pause, and volume (default: `True`)
         :param autoplay: whether to start playing the video automatically (default: `False`)
         :param muted: whether the video should be initially muted (default: `False`)
+        :param loop: whether the video should loop (default: `False`)
 
         See `here <https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video#events>`_
         for a list of events you can subscribe to using the generic event subscription `on()`.
@@ -25,3 +30,4 @@ class Video(Element):
         self._props['controls'] = controls
         self._props['autoplay'] = autoplay
         self._props['muted'] = muted
+        self._props['loop'] = loop
