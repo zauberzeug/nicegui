ba8b94b4ac2b83db15a3f3b9344cd9056fcffca7
Rodja Trappe <rodja@zauberzeug.com>
2023-03-20 14:15:06 +0100

    loading correct route on page init

--- examples/single_page_app/router_frame.js
+++ examples/single_page_app/router_frame.js
@@ -3,9 +3,15 @@ export default {
   mounted() {
     window.addEventListener("popstate", (event) => {
       if (event.state && event.state.page) {
-        console.log(event.state.page);
         this.$emit("open", event.state.page);
       }
+    });
+    console.log(window.location.pathname);
+    this.$nextTick(() => {
+      // FIXME this delay is a hack to make sure the event can actually be handled by the backend
+      setTimeout(() => {
+        this.$emit("open", window.location.pathname);
+      }, 1000);
     });
   },
   props: {},
