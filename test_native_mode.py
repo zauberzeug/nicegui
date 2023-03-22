275ec7eb3aabb1de0747ea92eecca02f02dae4e5
Rodja Trappe <rodja@zauberzeug.com>
2023-03-22 04:40:50 +0100

    trying again without firefox

--- test_native_mode.py
+++ test_native_mode.py
@@ -2,11 +2,19 @@
 from nicegui import Client, ui
 
 
+def log(msg: str):
+    print(msg, flush=True)
+    with open('output.log', 'a') as f:
+        f.write(msg)
+
+
 @ui.page('/')
 async def page(client: Client):
+    log('loading')
     await client.connected()
+    log('connected')
     result = await ui.run_javascript('return "Roundtrip works!"')
-    with open('result', 'w') as f:
-        f.write(result)
+    log(result)
+
 
 ui.run(reload=False, native=True)
