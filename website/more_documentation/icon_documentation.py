9a98bf9b1a779efc9286f9cd0628792fc4c77f82
Rodja Trappe <rodja@zauberzeug.com>
2023-03-30 18:59:55 +0200

    demos on how to add lottie files

--- website/more_documentation/icon_documentation.py
+++ website/more_documentation/icon_documentation.py
@@ -8,6 +8,7 @@ def main_demo() -> None:
 
 def more() -> None:
     ui.add_head_html('<link href="https://unpkg.com/eva-icons@1.1.3/style/eva-icons.css" rel="stylesheet">')
+    ui.add_body_html('<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>')
 
     @text_demo('Eva icons', '''
         You can use [Eva icons](https://akveo.github.io/eva-icons/#/) in your app.
@@ -15,3 +16,14 @@ def more() -> None:
     async def eva_icons():
         # ui.add_head_html('<link href="https://unpkg.com/eva-icons@1.1.3/style/eva-icons.css" rel="stylesheet">')
         ui.element('i').classes('eva eva-github').classes('text-5xl')
+
+    @text_demo('Lottie', '''
+        You can also use [Lottie files](https://lottiefiles.com/) with animations.
+    ''')
+    async def lottie():
+        # ui.add_body_html('<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>')
+
+        ui.html('''
+        <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_MKCnqtNQvg.json"  
+        background="transparent"  speed="1" style="width: 80px; height: 80px;" loop autoplay></lottie-player>
+        ''')
--- website/more_documentation/image_documentation.py
+++ website/more_documentation/image_documentation.py
@@ -1,5 +1,21 @@
 from nicegui import ui
+from website.documentation_tools import text_demo
 
 
 def main_demo() -> None:
     ui.image('https://picsum.photos/id/377/640/360')
+
+
+def more() -> None:
+    ui.add_body_html('<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>')
+
+    @text_demo('Lottie files', '''
+        You can also use [Lottie files](https://lottiefiles.com/) with animations.
+    ''')
+    async def lottie():
+        # ui.add_body_html('<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>')
+
+        ui.html('''
+        <lottie-player src="https://assets1.lottiefiles.com/datafiles/HN7OcWNnoqje6iXIiZdWzKxvLIbfeCGTmvXmEm1h/data.json"  
+        background="transparent"  speed="1" style="width: 300px; height: 300px;" loop autoplay></lottie-player>
+        ''')
