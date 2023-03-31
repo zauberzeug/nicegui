b67edacd6023d44a18d47ad7f33684e17e3d3f1b
Falko Schindler <falko@zauberzeug.com>
2023-03-31 09:49:07 +0200

    #660 review

--- .github/workflows/update_citation.py
+++ .github/workflows/update_citation.py
@@ -1,19 +1,19 @@
+import os
 import sys
-import yaml
-import os
-from datetime import datetime
 
-
-import os
 import requests
 import yaml
 
 
 def get_infos() -> str:
-    headers = {'Accept': 'application/json'}
+    headers = {
+        'Accept': 'application/json',
+    }
     params = {
         'access_token': os.environ['ZENODO_TOKEN'],
-        'q': 'nicegui', 'sort': 'mostrecent', 'status': 'published'
+        'q': 'nicegui',
+        'sort': 'mostrecent',
+        'status': 'published',
     }
     try:
         response = requests.get('https://zenodo.org/api/records', params=params, headers=headers)
@@ -29,6 +29,6 @@ def get_infos() -> str:
 if __name__ == '__main__':
     with open('CITATION.cff', 'r') as file:
         citation = yaml.safe_load(file)
-    citation['doi'], citation['version'],  citation['date-released'] = get_infos()
+    citation['doi'], citation['version'], citation['date-released'] = get_infos()
     with open('CITATION.cff', 'w') as file:
         yaml.dump(citation, file, sort_keys=False, default_flow_style=False)
