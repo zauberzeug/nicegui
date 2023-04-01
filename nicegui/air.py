29737dabb591ccfa0fa6a36f0277085211f00e18
Falko Schindler <falko@zauberzeug.com>
2023-04-01 16:54:38 +0200

    on air: serve rest endpoints via socketio

--- nicegui/air.py
+++ nicegui/air.py
@@ -1,4 +1,9 @@
+from typing import Any, Dict
+
+from fastapi.testclient import TestClient
 from socketio import AsyncClient
+
+from . import globals
 
 RELAY_HOST = 'http://192.168.0.111'
 
@@ -7,6 +12,17 @@ class Air:
 
     def __init__(self) -> None:
         self.relay = AsyncClient()
+        self.test_client = TestClient(globals.app)
+
+        @self.relay.on('get')
+        def on_get(data: Dict[str, Any]) -> Dict[str, Any]:
+            print(f'get {data["path"]}', flush=True)
+            response = self.test_client.get(data['path'])
+            return {
+                'status_code': response.status_code,
+                'content': response.content,
+                'media_type': response.headers.get('content-type'),
+            }
 
     async def connect(self) -> None:
         await self.relay.connect(RELAY_HOST, socketio_path='/on_air/socket.io', headers={'NiceGUI': 'register_relay'})
