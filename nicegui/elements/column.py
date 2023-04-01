bb4fc2bd9240be5f53a4b7c2ff1f9d0294437a2e
Rodja Trappe <rodja@zauberzeug.com>
2023-04-01 06:14:17 +0200

    fixed docu

--- nicegui/elements/column.py
+++ nicegui/elements/column.py
@@ -6,7 +6,7 @@ class Column(Element):
     def __init__(self) -> None:
         '''Column Element
 
-        Provides a container which arranges its child in a row.
+        Provides a container which arranges its child in a column.
         '''
         super().__init__('div')
         self._classes = ['nicegui-column']
