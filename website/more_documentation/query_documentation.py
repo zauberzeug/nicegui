024b9d74a2bb047c121240e79cc3e656e684d383
Falko Schindler <falko@zauberzeug.com>
2023-04-03 11:10:13 +0200

    fix typo in query documentation

--- website/more_documentation/query_documentation.py
+++ website/more_documentation/query_documentation.py
@@ -5,8 +5,8 @@ def main_demo() -> None:
     def set_background(color: str) -> None:
         ui.query('body').style(f'background-color: {color}')
 
-    # ui.button('Blue', on_click=lambda: set_background(#ddeeff'))
-    # ui.button('Orange', on_click=lambda: set_background(#ffeedd'))
+    # ui.button('Blue', on_click=lambda: set_background('#ddeeff'))
+    # ui.button('Orange', on_click=lambda: set_background('#ffeedd'))
     # END OF DEMO
     ui.button('Blue', on_click=lambda e: e.sender.parent_slot.parent.style('background-color: #ddeeff'))
     ui.button('Orange', on_click=lambda e: e.sender.parent_slot.parent.style('background-color: #ffeedd'))
