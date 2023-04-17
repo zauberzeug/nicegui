43ffe613ca1e61d5ac34071ec76d46f4f5366fcb
Falko Schindler <falko@zauberzeug.com>
2023-04-17 17:55:33 +0200

    introduce ui.dark to control dark mode

---
+++ nicegui/elements/dark.js
@@ -0,0 +1,19 @@
+export default {
+  mounted() {
+    this.update();
+  },
+  updated() {
+    this.update();
+  },
+  methods: {
+    update() {
+      Quasar.Dark.set(this.value === null ? "auto" : this.value);
+      tailwind.config.darkMode = this.auto ? "media" : "class";
+      if (this.value) document.body.classList.add("dark");
+      else document.body.classList.remove("dark");
+    },
+  },
+  props: {
+    value: Boolean,
+  },
+};
