ad76177a57e269506fbed050ec3a591cc820a0a5
Falko Schindler <falko@zauberzeug.com>
2023-04-23 19:54:04 +0200

    #276 fix documentation for input's on_change parameter

--- nicegui/elements/input.py
+++ nicegui/elements/input.py
@@ -32,7 +32,7 @@ class Input(ValueElement, DisableableElement):
         :param value: the current value of the text input
         :param password: whether to hide the input (default: False)
         :param password_toggle_button: whether to show a button to toggle the password visibility (default: False)
-        :param on_change: callback to execute when the input is confirmed by leaving the focus
+        :param on_change: callback to execute when the value changes
         :param validation: dictionary of validation rules, e.g. ``{'Too short!': lambda value: len(value) < 3}``
         """
         super().__init__(tag='q-input', value=value, on_value_change=on_change)
