a3a1540ce58255df9ec68d0329b6f591dd1c95de
Falko Schindler <falko@zauberzeug.com>
2023-05-03 13:41:59 +0200

    chat: add pytests

---
+++ tests/test_chat.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.common.by import By
+
+from nicegui import ui
+
+from .screen import Screen
+
+
+def test_no_html(screen: Screen):
+    ui.chat_message('<strong>HTML</strong>')
+
+    screen.open('/')
+    screen.should_contain('<strong>HTML</strong>')
+
+
+def test_html(screen: Screen):
+    ui.chat_message('<strong>HTML</strong>', text_html=True)
+
+    screen.open('/')
+    screen.should_contain('HTML')
+    screen.should_not_contain('<strong>HTML</strong>')
+
+
+def test_newline(screen: Screen):
+    ui.chat_message('Hello\nNiceGUI!')
+    ui.chat_message('Hi\nnice guy!', text_html=True)
+
+    screen.open('/')
+    assert screen.find('Hello').find_element(By.TAG_NAME, 'br')
+    assert screen.find('Hi').find_element(By.TAG_NAME, 'br')
