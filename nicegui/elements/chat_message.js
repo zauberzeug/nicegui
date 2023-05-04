8f0746e020e8e2deb820fa65746ab7e0ff9b59d1
Falko Schindler <falko@zauberzeug.com>
2023-05-04 13:54:34 +0200

    simplify chat_message.js

--- nicegui/elements/chat_message.js
+++ nicegui/elements/chat_message.js
@@ -1,20 +1,3 @@
 export default {
-  template: `
-    <q-chat-message
-      :text="text"
-      :name="name"
-      :label="label"
-      :stamp="stamp"
-      :avatar="avatar"
-      :sent=sent
-    />
-  `,
-  props: {
-    text: Array,
-    name: String,
-    label: String,
-    stamp: String,
-    avatar: String,
-    sent: Boolean,
-  },
+  template: `<q-chat-message v-bind="$attrs" />`,
 };
