bac537843aeec09f07304a9e9aaa006d0187898f
Rodja Trappe <rodja@zauberzeug.com>
2023-05-05 07:38:22 +0200

    Allow all page parameters

--- examples/modularization/example_c.py
+++ examples/modularization/example_c.py
@@ -8,13 +8,13 @@ router = APIRouter(prefix='/c')
 @router.page('/')
 def example_page():
     with theme.frame('- Example C -'):
-        with ui.column().classes('items-center'):
-            ui.label('Example C').classes('text-h4 text-grey-8')
-            for i in range(1, 4):
-                ui.link(f'Item {i}', f'/c/items/{i}').classes('text-xl text-grey-8')
+        ui.label('Example C').classes('text-h4 text-grey-8')
+        for i in range(1, 4):
+            ui.link(f'Item {i}', f'/c/items/{i}').classes('text-xl text-grey-8')
 
 
-@router.page('/items/{id}')
+@router.page('/items/{id}', dark=True)
 def item(id: str):
     with theme.frame(f'- Example C{id} -'):
         ui.label(f'Item  #{id}').classes('text-h4 text-grey-8')
+        ui.link('go back', router.prefix).classes('text-xl text-grey-8')
--- nicegui/api_router.py
+++ nicegui/api_router.py
@@ -7,5 +7,5 @@ from .page import page as ui_page
 
 class APIRouter(fastapi.APIRouter):
 
-    def page(self, func: Callable) -> Callable:
-        return ui_page(func, api_router=self)
+    def page(self, *args, **kwargs) -> Callable:
+        return ui_page(*args, api_router=self, **kwargs)
