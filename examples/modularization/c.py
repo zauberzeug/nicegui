d5e460f325f438cf106634fab11f2b1ffda70ced
Rodja Trappe <rodja@zauberzeug.com>
2023-05-05 05:40:15 +0200

    adding demo with sub pages and prefix

--- examples/modularization/c.py
+++ examples/modularization/c.py
@@ -2,10 +2,19 @@ import theme
 
 from nicegui import APIRouter, ui
 
-router = APIRouter()
+router = APIRouter(prefix='/c')
 
 
-@router.page('/c')
+@router.page('/')
 def example_page():
     with theme.frame('- Example C -'):
-        ui.label('Example C').classes('text-h4 text-grey-8')
+        with ui.column().classes('items-center'):
+            ui.label('Example C').classes('text-h4 text-grey-8')
+            for i in range(1, 4):
+                ui.link(f'Item {i}', f'/c/items/{i}').classes('text-xl text-grey-8')
+
+
+@router.page('/items/{id}')
+def item(id: str):
+    with theme.frame(f'- Example C{id} -'):
+        ui.label(f'Item  #{id}').classes('text-h4 text-grey-8')
