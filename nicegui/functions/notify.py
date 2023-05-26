6e4e167f045dc9288cabd2a5e8905547e5a9ad8d
Falko Schindler <falko@zauberzeug.com>
2023-05-26 09:21:14 +0200

    Revert "Revert "code review""
    This reverts commit 61de2eb37b58c65d9de7260a8226fc63bf98e55a.

--- nicegui/functions/notify.py
+++ nicegui/functions/notify.py
@@ -4,14 +4,19 @@ from typing_extensions import Literal
 
 from .. import globals, outbox
 
+ARG_MAP = {
+    'close_button': 'closeBtn',
+    'multi_line': 'multiLine',
+}
+
 
 def notify(message: Any, *,
            position: Literal['top-left', 'top-right', 'bottom-left', 'bottom-right', 'top', 'bottom', 'left', 'right', 'center'] = 'bottom',
            close_button: Union[bool, str] = False,
            type: Optional[Literal['positive', 'negative', 'warning', 'info', 'ongoing']] = None,
            color: Optional[str] = None,
-           multi_line: Optional[bool] = False,
-           **_kwargs,
+           multi_line: bool = False,
+           **kwargs,
            ) -> None:
     """Notification
 
@@ -22,18 +27,11 @@ def notify(message: Any, *,
     :param close_button: optional label of a button to dismiss the notification (default: `False`)
     :param type: optional type ("positive", "negative", "warning", "info" or "ongoing")
     :param color: optional color name
-    :param multi_line: optional boolean to enable multi-line notifications
+    :param multi_line: enable multi-line notifications
 
     Note: You can pass additional keyword arguments according to `Quasar's Notify API <https://quasar.dev/quasar-plugins/notify#notify-api>`_.
     """
-    _arg_map = {'close_button': 'closeBtn',
-                'multi_line':   'multiLine',
-                }
-    options = {_arg_map.get(key, key): value
-               for key, value
-               in locals().items()
-               if not key.startswith('_') and value is not None
-               }
+    options = {ARG_MAP.get(key, key): value for key, value in locals().items() if key != 'kwargs' and value is not None}
     options['message'] = str(message)
-    options.update(_kwargs)
+    options.update(kwargs)
     outbox.enqueue_message('notify', options, globals.get_client().id)
