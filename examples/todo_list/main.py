6584db44a87b29d75a592c52192ab339ba5729b6
Rodja Trappe <rodja@zauberzeug.com>
2023-05-26 04:54:34 +0200

    review

--- examples/todo_list/main.py
+++ examples/todo_list/main.py
@@ -39,27 +39,27 @@ def rename(item: TodoItem, name: str) -> None:
     render_list.refresh()
 
 
+@ui.refreshable
+def render_list():
+    if not items:
+        ui.label('List is empty.')
+        return
+    ui.linear_progress(sum(item.done for item in items) / len(items), show_value=False)
+    with ui.row().classes('justify-center w-full'):
+        ui.label(f'Completed: {sum(item.done for item in items)}')
+        ui.label(f'Remaining: {sum(not item.done for item in items)}')
+    for item in items:
+        with ui.row().classes('items-center'):
+            ui.checkbox(value=item.done, on_change=lambda _, item=item: toggle(item))
+            input = ui.input(value=item.name).classes('flex-grow')
+            input.on('keydown.enter', lambda _, item=item, input=input: rename(item, input.value))
+            ui.button(on_click=lambda _, item=item: remove(item)).props('flat fab-mini icon=delete color=grey')
+
+
 with ui.card().classes('w-80 items-stretch'):
     ui.label('Todo list').classes('text-semibold text-2xl')
-    add_input = ui.input('New item')
-    add_input.on('keydown.enter', lambda: add(add_input.value))
-
-    @ui.refreshable
-    def render_list():
-        if not items:
-            ui.label('List is empty.')
-            return
-        ui.linear_progress(sum(item.done for item in items) / len(items), show_value=False)
-        with ui.row():
-            ui.label(f'Completed: {sum(item.done for item in items)}')
-            ui.label(f'Remaining: {sum(not item.done for item in items)}')
-        for item in items:
-            with ui.row().classes('items-center'):
-                ui.checkbox(value=item.done, on_change=lambda _, item=item: toggle(item))
-                input = ui.input(value=item.name).classes('flex-grow')
-                input.on('keydown.enter', lambda _, item=item, input=input: rename(item, input.value))
-                ui.button(on_click=lambda _, item=item: remove(item)).props('flat fab-mini icon=delete color=grey')
-
     render_list()
+    add_input = ui.input('New item').classes('mx-12')
+    add_input.on('keydown.enter', lambda: add(add_input.value))
 
 ui.run()
