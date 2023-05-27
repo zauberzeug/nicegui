56758960f13a0b1d07f3776f83e86843f4218fc3
Rodja Trappe <rodja@zauberzeug.com>
2023-05-27 06:40:56 +0200

    add some demo for clearable prop

--- website/more_documentation/textarea_documentation.py
+++ website/more_documentation/textarea_documentation.py
@@ -1,7 +1,19 @@
 from nicegui import ui
 
+from ..documentation_tools import text_demo
+
 
 def main_demo() -> None:
     ui.textarea(label='Text', placeholder='start typing',
                 on_change=lambda e: result.set_text('you typed: ' + e.value))
     result = ui.label()
+
+
+def more() -> None:
+
+    @text_demo('Clearable', '''
+        The `clearable` prop from [Quasar](https://quasar.dev/) adds a button to the input that clears the text.    
+    ''')
+    async def clearable():
+        i = ui.textarea(value='some text').props('clearable')
+        ui.label().bind_text_from(i, 'value')
