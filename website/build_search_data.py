05830854106db2ec1bd1bbef195e8253b9e12756
Rodja Trappe <rodja@zauberzeug.com>
2023-06-09 16:23:17 +0200

    using correct url for each search data item

--- website/build_search_data.py
+++ website/build_search_data.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python3
 import ast
-import glob
 import json
 import os
 import textwrap
+import urllib.parse
+from pathlib import Path
+
+from icecream import ic
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(dir_path)
 
 
 class DemoVisitor(ast.NodeVisitor):
+
+    def __init__(self, topic: str) -> None:
+        super().__init__()
+        self.topic = topic
+
     def visit_FunctionDef(self, node):
         for decorator in node.decorator_list:
             if isinstance(decorator, ast.Call):
                 function = decorator.func
                 if isinstance(function, ast.Name) and function.id == 'text_demo':
                     title = decorator.args[0].s
+                    ic(self.topic, title)
                     content = ' '.join([l.strip() for l in decorator.args[1].s.splitlines()]).strip()
-                    url = "/docs/" + node.name
+                    anchor = title.lower().replace(' ', '_')
+                    url = f'/documentation/{self.topic}#{anchor}'
                     documents.append({
                         'title': title,
                         'content': content,
@@ -26,7 +36,7 @@ class DemoVisitor(ast.NodeVisitor):
                 elif isinstance(function, ast.Attribute) and function.attr == 'text_demo':
                     title = decorator.args[0].s
                     content = textwrap.dedent(decorator.args[1].s)
-                    url = "/docs/" + node.name
+                    url = '/docs/' + node.name
                     documents.append({
                         'title': title,
                         'content': content,
@@ -36,10 +46,10 @@ class DemoVisitor(ast.NodeVisitor):
 
 
 documents = []
-for file in glob.glob("more_documentation/*.py"):
-    with open(file, "r") as source:
+for file in Path('./more_documentation').glob('*.py'):
+    with open(file, 'r') as source:
         tree = ast.parse(source.read())
-        DemoVisitor().visit(tree)
+        DemoVisitor(file.stem.split('_')[0]).visit(tree)
 
 with open('static/search_data.json', 'w') as f:
-    json.dump(documents, f)
+    json.dump(documents, f, indent=2)
