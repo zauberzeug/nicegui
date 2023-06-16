fa017a523d246b4970d1482c1aece4e5f8326d55
Falko Schindler <falko@zauberzeug.com>
2023-06-16 08:36:38 +0200

    code review

--- website/more_documentation/image_documentation.py
+++ website/more_documentation/image_documentation.py
@@ -10,17 +10,17 @@ def main_demo() -> None:
 def more() -> None:
     ui.add_body_html('<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>')
 
-    @text_demo('base64 string', '''
-        You can also use [Lottie files](https://lottiefiles.com/) with animations.
+    @text_demo('Base64 string', '''
+        You can also use a Base64 string as image source.
     ''')
-    async def base64():
+    def base64():
         base64 = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
         ui.image(base64).classes('w-2 h-2 m-auto')
 
     @text_demo('Lottie files', '''
         You can also use [Lottie files](https://lottiefiles.com/) with animations.
     ''')
-    async def lottie():
+    def lottie():
         # ui.add_body_html('<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>')
 
         src = 'https://assets1.lottiefiles.com/datafiles/HN7OcWNnoqje6iXIiZdWzKxvLIbfeCGTmvXmEm1h/data.json'
