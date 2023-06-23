b6480425a9acd86c4feb30e1c75ed303a120bfe0
Falko Schindler <falko@zauberzeug.com>
2023-06-23 12:20:14 +0200

    fix input binding test

--- nicegui/elements/input.js
+++ nicegui/elements/input.js
@@ -26,6 +26,9 @@ export default {
     };
   },
   watch: {
+    value(newValue) {
+      this.inputValue = newValue;
+    },
     inputValue(newValue) {
       this.$emit("update:value", newValue);
     },
