f96a8b9151f9abd27d7182c96d55b70ae5cf3e3a
Eli Khazan <33314467+elik-vayyar@users.noreply.github.com>
2023-06-24 23:05:21 +0300

    feat(on_scroll): broken attempt at scroll event for review

--- nicegui/elements/scroll_area.py
+++ nicegui/elements/scroll_area.py
@@ -1,12 +1,23 @@
+from typing import Optional, Callable, Any
+
 from ..element import Element
+from ..events import handle_event, ScrollEventArguments
 
 
 class ScrollArea(Element):
 
-    def __init__(self) -> None:
+    def __init__(self, *,
+                 on_scroll: Optional[Callable[..., Any]] = None) -> None:
         """Scroll Area
 
         A way of customizing the scrollbars by encapsulating your content.
         """
         super().__init__('q-scroll-area')
         self._classes = ['nicegui-scroll']
+
+        def scroll_handle(info):
+            handle_event(on_scroll, ScrollEventArguments(
+                sender=self, client=self.client, info=info))
+
+        if on_scroll:
+            self.on('scroll', scroll_handle)
