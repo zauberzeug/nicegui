afe4aeec210581b7a9dd3b93c16ce513a39a55f8
Falko Schindler <falko@zauberzeug.com>
2023-06-30 08:02:45 +0200

    code review

--- examples/sqlite_database/main.py
+++ examples/sqlite_database/main.py
@@ -6,7 +6,6 @@ from tortoise.contrib.fastapi import register_tortoise
 
 from nicegui import app, ui
 
-
 register_tortoise(
     app,
     db_url='sqlite://db.sqlite3',
@@ -23,25 +22,24 @@ async def list_of_users() -> None:
 
     users: List[models.User] = await models.User.all()
     for user in reversed(users):
-        with ui.card().classes('w-full'):
-            with ui.row().classes('justify-between w-full items-center'):
-                ui.input('Name', on_change=lambda u=user: u.save()).bind_value(user, 'name') \
-                    .on('blur', list_of_users.refresh)
-                ui.input('Age', on_change=lambda u=user: u.save()).classes('w-20').bind_value(user, 'age') \
-                    .on('blur', list_of_users.refresh)
-                ui.button(on_click=lambda u=user: delete(u), icon='delete') \
-                    .props('flat').classes('ml-auto')
+        with ui.card():
+            with ui.row().classes('items-center'):
+                ui.input('Name', on_change=user.save) \
+                    .bind_value(user, 'name').on('blur', list_of_users.refresh)
+                ui.number('Age', on_change=user.save, format='%.0f') \
+                    .bind_value(user, 'age').on('blur', list_of_users.refresh).classes('w-20')
+                ui.button(icon='delete', on_click=lambda u=user: delete(u)).props('flat')
 
 
 @ui.page('/')
 async def index():
     async def create() -> None:
-        await models.User.create(name=name.value, age=age.value)
+        await models.User.create(name=name.value, age=age.value or 0)
         name.value = ''
         age.value = None
         list_of_users.refresh()
 
-    with ui.column().classes('w-96 mx-auto'):
+    with ui.column().classes('mx-auto'):
         with ui.row().classes('w-full items-center px-4'):
             name = ui.input(label='Name')
             age = ui.number(label='Age', format='%.0f').classes('w-20')
