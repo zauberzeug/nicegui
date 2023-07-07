121943882ee4e5cc9268374db87a5ecc17564454
Falko Schindler <falko@zauberzeug.com>
2023-07-07 18:07:59 +0200

    update examples

--- examples/map/leaflet.py
+++ examples/map/leaflet.py
@@ -5,14 +5,14 @@ from nicegui import ui
 from nicegui.dependencies import register_vue_component
 from nicegui.element import Element
 
-register_vue_component('leaflet', Path(__file__).parent / 'leaflet.js')
+component = register_vue_component(Path('leaflet.js'), base_path=Path(__file__).parent)
 
 
 class leaflet(Element):
 
     def __init__(self) -> None:
-        super().__init__('leaflet')
-        self.use_component('leaflet')
+        super().__init__(component.tag)
+        self.use_component(component)
         ui.add_head_html('<link href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" rel="stylesheet"/>')
         ui.add_head_html('<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>')
 
