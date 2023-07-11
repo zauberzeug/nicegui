d86cc1d9309a32212a7e220df9d19fd02c8131ac
Falko Schindler <falko@zauberzeug.com>
2023-07-11 16:33:50 +0200

    avoid running mermaid renderings simultaneously

--- nicegui/elements/mermaid.js
+++ nicegui/elements/mermaid.js
@@ -1,4 +1,8 @@
 import mermaid from "mermaid";
+
+let is_running = false;
+const queue = [];
+
 export default {
   template: `<div></div>`,
   data: () => ({
@@ -8,12 +12,18 @@ export default {
     this.update(this.content);
   },
   methods: {
-    update(content) {
+    async update(content) {
       if (this.last_content === content) return;
       this.last_content = content;
       this.$el.innerHTML = content;
       this.$el.removeAttribute("data-processed");
-      mermaid.run({ nodes: [this.$el] });
+      queue.push(this.$el);
+      if (is_running) return;
+      is_running = true;
+      while (queue.length) {
+        await mermaid.run({ nodes: [queue.shift()] });
+      }
+      is_running = false;
     },
   },
   props: {
