1f2e637009f00f00c816ed8b432f864ad0379ae1
Falko Schindler <falko@zauberzeug.com>
2023-07-14 16:49:26 +0200

    fix Tailwind configuration for automatic dark mode

--- nicegui/elements/dark_mode.js
+++ nicegui/elements/dark_mode.js
@@ -9,7 +9,7 @@ export default {
     update() {
       Quasar.Dark.set(this.value === null ? "auto" : this.value);
       if (window.tailwind) {
-        tailwind.config.darkMode = this.auto ? "media" : "class";
+        tailwind.config.darkMode = this.value === null ? "media" : "class";
         if (this.value) document.body.classList.add("dark");
         else document.body.classList.remove("dark");
       }
