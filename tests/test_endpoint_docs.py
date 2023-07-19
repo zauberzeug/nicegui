ad48f3fc7aa26123e01a65f4a7e1fcfcbb45fd2d
Falko Schindler <falko@zauberzeug.com>
2023-07-19 13:20:32 +0200

    simplify endpoint documentation tests

--- tests/test_endpoint_docs.py
+++ tests/test_endpoint_docs.py
@@ -1,72 +1,44 @@
-from nicegui import ui, __version__ as version
-
 import requests
+
+from nicegui import __version__
+
 from .screen import PORT, Screen
 
 
 def test_endpoint_documentation_default(screen: Screen):
-    @ui.page('/')
-    def page():
-        ui.label('Hello, world!')
-
     screen.ui_run_kwargs['endpoint_documentation'] = ''
-    screen.open('/openapi.json')
+    screen.open('/')
 
     response = requests.get(f'http://localhost:{PORT}/openapi.json')
-    assert response.status_code == 200
-
-    schema = response.json()
-    assert len(schema['paths']) == 0
-    assert schema['paths'] == {}
+    assert list(response.json()['paths']) == []
 
 
 def test_endpoint_documentation_page_only(screen: Screen):
-    @ui.page('/')
-    def page():
-        ui.label('Hello, world!')
-
     screen.ui_run_kwargs['endpoint_documentation'] = 'page'
-    screen.open('/openapi.json')
+    screen.open('/')
 
     response = requests.get(f'http://localhost:{PORT}/openapi.json')
-    assert response.status_code == 200
-
-    schema = response.json()
-    assert len(schema['paths']) == 1
-    assert '/' in schema['paths']
+    assert list(response.json()['paths']) == ['/']
 
 
 def test_endpoint_documentation_internal_only(screen: Screen):
-    @ui.page('/')
-    def page():
-        ui.label('Hello, world!')
-
     screen.ui_run_kwargs['endpoint_documentation'] = 'internal'
-    screen.open('/openapi.json')
+    screen.open('/')
 
     response = requests.get(f'http://localhost:{PORT}/openapi.json')
-    assert response.status_code == 200
-
-    schema = response.json()
-    assert len(schema['paths']) == 2
-    assert '/' not in schema['paths']
-    assert f'/_nicegui/{version}/libraries/{{key}}' in schema['paths']
-    assert f'/_nicegui/{version}/components/{{key}}' in schema['paths']
+    assert list(response.json()['paths']) == [
+        f'/_nicegui/{__version__}/libraries/{{key}}',
+        f'/_nicegui/{__version__}/components/{{key}}',
+    ]
 
 
-def test_endpoint_documentation_internal_all(screen: Screen):
-    @ui.page('/')
-    def page():
-        ui.label('Hello, world!')
-
+def test_endpoint_documentation_all(screen: Screen):
     screen.ui_run_kwargs['endpoint_documentation'] = 'all'
-    screen.open('/openapi.json')
+    screen.open('/')
 
     response = requests.get(f'http://localhost:{PORT}/openapi.json')
-    assert response.status_code == 200
-
-    schema = response.json()
-    assert len(schema['paths']) == 3
-    assert '/' in schema['paths']
-    assert f'/_nicegui/{version}/libraries/{{key}}' in schema['paths']
-    assert f'/_nicegui/{version}/components/{{key}}' in schema['paths']
+    assert list(response.json()['paths']) == [
+        '/',
+        f'/_nicegui/{__version__}/libraries/{{key}}',
+        f'/_nicegui/{__version__}/components/{{key}}',
+    ]
