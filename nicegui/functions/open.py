0396cf60f943b85d555e2e7a5e7f9ad8b2291b09
Brian Landry <brian.landry@gmail.com>
2023-07-23 14:20:05 -0700

    Removed unused param in open.py docstring

--- nicegui/functions/open.py
+++ nicegui/functions/open.py
@@ -12,7 +12,6 @@ def open(target: Union[Callable[..., Any], str]) -> None:
     User events like button clicks provide such a socket.
 
     :param target: page function or string that is a an absolute URL or relative path from base URL
-    :param socket: optional WebSocket defining the target client
     """
     path = target if isinstance(target, str) else globals.page_routes[target]
     globals.get_client().open(path)
