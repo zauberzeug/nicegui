64040e7802568c78f140a8aacfccf271dc6b5efc
Falko Schindler <falko@zauberzeug.com>
2023-07-25 07:21:53 +0200

    fix color picker demo

--- website/more_documentation/color_picker_documentation.py
+++ website/more_documentation/color_picker_documentation.py
@@ -2,5 +2,5 @@ from nicegui import ui
 
 
 def main_demo() -> None:
-    picker = ui.color_picker(on_pick=lambda e: button.style(f'background-color:{e.color}!important'))
-    button = ui.button(on_click=picker.open, icon='colorize')
+    with ui.button(icon='colorize') as button:
+        ui.color_picker(on_pick=lambda e: button.style(f'background-color:{e.color}!important'))
