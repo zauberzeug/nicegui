85c2df43c4c7df684507c885cc93ec7dd14f5c79
Falko Schindler <falko@zauberzeug.com>
2023-07-26 15:41:41 +0200

    fix pytest after refactoring on main branch

--- tests/test_endpoint_docs.py
+++ tests/test_endpoint_docs.py
@@ -2,13 +2,13 @@ import requests
 
 from nicegui import __version__
 
-from .screen import PORT, Screen
+from .screen import Screen
 
 
 def test_endpoint_documentation_default(screen: Screen):
     screen.open('/')
 
-    response = requests.get(f'http://localhost:{PORT}/openapi.json')
+    response = requests.get(f'http://localhost:{Screen.PORT}/openapi.json')
     assert set(response.json()['paths']) == set()
 
 
@@ -16,7 +16,7 @@ def test_endpoint_documentation_page_only(screen: Screen):
     screen.ui_run_kwargs['endpoint_documentation'] = 'page'
     screen.open('/')
 
-    response = requests.get(f'http://localhost:{PORT}/openapi.json')
+    response = requests.get(f'http://localhost:{Screen.PORT}/openapi.json')
     assert set(response.json()['paths']) == {'/'}
 
 
@@ -24,7 +24,7 @@ def test_endpoint_documentation_internal_only(screen: Screen):
     screen.ui_run_kwargs['endpoint_documentation'] = 'internal'
     screen.open('/')
 
-    response = requests.get(f'http://localhost:{PORT}/openapi.json')
+    response = requests.get(f'http://localhost:{Screen.PORT}/openapi.json')
     assert set(response.json()['paths']) == {
         f'/_nicegui/{__version__}/libraries/{{key}}',
         f'/_nicegui/{__version__}/components/{{key}}',
@@ -35,7 +35,7 @@ def test_endpoint_documentation_all(screen: Screen):
     screen.ui_run_kwargs['endpoint_documentation'] = 'all'
     screen.open('/')
 
-    response = requests.get(f'http://localhost:{PORT}/openapi.json')
+    response = requests.get(f'http://localhost:{Screen.PORT}/openapi.json')
     assert set(response.json()['paths']) == {
         '/',
         f'/_nicegui/{__version__}/libraries/{{key}}',
