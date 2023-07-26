1a593764c77c6a8aa1831735fa993d13aa1aaa14
Rodja Trappe <rodja@zauberzeug.com>
2023-07-26 08:23:45 +0200

    allow configuration of socket.io transport options because fly.io requires a header to find the right instance which is only supported by browsers with polling, not websockets

--- nicegui/globals.py
+++ nicegui/globals.py
@@ -45,6 +45,7 @@ binding_refresh_interval: float
 tailwind: bool
 air: Optional['Air'] = None
 socket_io_js_extra_headers: Dict = {}
+socket_io_js_transports: List = ['websocket', 'polling']
 
 _socket_id: Optional[str] = None
 slot_stacks: Dict[int, List['Slot']] = {}
