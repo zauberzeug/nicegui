31389108f9617f79c9e0e894fb482b20ab2f85d7
Natan <nkeddem@gmail.com>
2023-08-06 22:18:47 -0400

    added chart on change callback argument and caller

--- nicegui/elements/chart.py
+++ nicegui/elements/chart.py
@@ -1,14 +1,17 @@
-from typing import Dict, List
+from typing import Callable, Dict, List, Optional
 
 from ..element import Element
 
 
-class Chart(Element,
-            component='chart.js',
-            libraries=['lib/highcharts/*.js'],
-            extra_libraries=['lib/highcharts/modules/*.js']):
-
-    def __init__(self, options: Dict, *, type: str = 'chart', extras: List[str] = []) -> None:
+class Chart(
+    Element,
+    component='chart.js',
+    libraries=['lib/highcharts/*.js'],
+    extra_libraries=['lib/highcharts/modules/*.js'],
+):
+    def __init__(
+        self, options: Dict, *, type: str = 'chart', extras: List[str] = [], on_change: Optional[Callable] = None
+    ) -> None:
         """Chart
 
         An element to create a chart using `Highcharts <https://www.highcharts.com/>`_.
@@ -21,12 +24,14 @@ class Chart(Element,
         :param options: dictionary of Highcharts options
         :param type: chart type (e.g. "chart", "stockChart", "mapChart", ...; default: "chart")
         :param extras: list of extra dependencies to include (e.g. "annotations", "arc-diagram", "solid-gauge", ...)
+        :param on_change: callback to execute when value changes
         """
         super().__init__()
         self._props['type'] = type
         self._props['options'] = options
         self._props['extras'] = extras
         self.libraries.extend(library for library in self.extra_libraries if library.path.stem in extras)
+        self.on('change', on_change)
 
     @property
     def options(self) -> Dict:
