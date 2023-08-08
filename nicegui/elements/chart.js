28a5beaef01b4c179421af85c772ad80deece925
Natan <nkeddem@gmail.com>
2023-08-07 20:46:37 -0400

    added chart js event emits

--- nicegui/elements/chart.js
+++ nicegui/elements/chart.js
@@ -5,25 +5,58 @@ export default {
       const imports = this.extras.map((extra) => import(window.path_prefix + extra));
       Promise.allSettled(imports).then(() => {
         this.seriesCount = this.options.series ? this.options.series.length : 0;
-        // if (this.on_change_set === true) { // I am setting self._props['on_change_set'] = True; not sure why it isn't working.
+        if (!this.options.plotOptions) {
+          this.options.plotOptions = {};
+        }
+        if (!this.options.plotOptions.series) {
+          this.options.plotOptions.series = {};
+        }
+        if (!this.options.plotOptions.series.point) {
+          this.options.plotOptions.series.point = {};
+        }
+        if (!this.options.plotOptions.series.point.events) {
+          this.options.plotOptions.series.point.events = {};
+        }
+        // if (this.on_event_set === true) {
         if (true) {
-          if ("plotOptions" in this.options) {
-            if ("series" in this.options.plotOptions) {
-              this.options.plotOptions.series.point = {
-                events: {
-                  drop: (e) => {
-                    this.$emit("on_point_drop", {
-                      type: "point_drop",
-                      series_name: e.target.series.name,
-                      series_index: e.target.series.index,
-                      series_xdata: e.target.series.xData,
-                      series_ydata: e.target.series.yData,
-                    });
-                  },
-                },
-              };
-            }
-          }
+          this.options.plotOptions.series.point.events.click = (e) => {
+            this.$emit("on_event", {
+              event_type: "point_click",
+              point_index: e.point.index,
+              point_x: e.point.x,
+              point_y: e.point.y,
+              series_index: e.point.series.index,
+            });
+          };
+        }
+        // if (this.on_event_set === true) {
+        if (true) {
+          this.options.plotOptions.series.point.events.dragStart = (e) => {
+            this.$emit("on_point_drag_drop", {
+              event_type: "point_drag_start",
+            });
+          };
+          this.options.plotOptions.series.point.events.drag = (e) => {
+            this.$emit("on_point_drag_drop", {
+              event_type: "point_drag",
+              point_index: e.target.index,
+              point_x: e.target.x,
+              point_y: e.target.y,
+              series_index: e.target.series.index,
+            });
+          };
+          this.options.plotOptions.series.point.events.drop = (e) => {
+            this.$emit("on_point_drag_drop", {
+              event_type: "point_drop",
+              point_index: e.target.index,
+              point_x: e.target.x,
+              point_y: e.target.y,
+              series_name: e.target.series.name,
+              series_index: e.target.series.index,
+              series_x: e.target.series.xData,
+              series_y: e.target.series.yData,
+            });
+          };
         }
         this.chart = Highcharts[this.type](this.$el, this.options);
         this.chart.reflow();
