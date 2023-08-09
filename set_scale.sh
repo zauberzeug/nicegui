6d231be24ba4cf29b1c4cf652ef99178acaebf73
Rodja Trappe <rodja@zauberzeug.com>
2023-08-09 05:39:47 +0200

    httpx is already a dependency of NiceGUI

---
+++ set_scale.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+fly scale count app=1  --region fra
+fly scale count app=1  --region iad
+fly scale count app=1  --region jnb
+fly scale count app=1  --region lax
+fly scale count app=1  --region lhr
+fly scale count app=1  --region bom
+fly scale count app=1  --region mad
+fly scale count app=1  --region mia
+fly scale count app=1  --region nrt
+fly scale count app=1  --region sea
+fly scale count app=1  --region sin
