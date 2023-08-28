433f0f2e1b08645a00f31009ecedda521224898a
Falko Schindler <falko@zauberzeug.com>
2023-08-28 10:43:15 +0200

    code review

--- nicegui/middlewares.py
+++ nicegui/middlewares.py
@@ -1,8 +1,11 @@
-from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
 
 
 class RedirectWithPrefixMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request, call_next):
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         prefix = request.headers.get('X-Forwarded-Prefix', '')
         response = await call_next(request)
         if 'Location' in response.headers:
