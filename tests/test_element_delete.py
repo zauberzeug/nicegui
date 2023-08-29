ad9916e743d470988377a2bdb908cfb0edcee927
Falko Schindler <falko@zauberzeug.com>
2023-08-29 10:23:25 +0200

    test removal of parent element (currently failing)

--- tests/test_element_delete.py
+++ tests/test_element_delete.py
@@ -1,3 +1,5 @@
+import pytest
+
 from nicegui import ui
 
 from .screen import Screen
@@ -51,6 +53,28 @@ def test_clear(screen: Screen):
     screen.should_not_contain('Label A')
     screen.should_not_contain('Label B')
     screen.should_not_contain('Label C')
+    assert a.is_deleted
+    assert b.is_deleted
+    assert c.is_deleted
+
+
+@pytest.mark.skip(reason='needs fix in element.py')  # TODO
+def test_remove_parent(screen: Screen):
+    with ui.element() as container:
+        with ui.row() as row:
+            a = ui.label('Label A')
+            b = ui.label('Label B')
+            c = ui.label('Label C')
+
+    ui.button('Remove parent', on_click=lambda: container.remove(row))
+
+    screen.open('/')
+    screen.click('Remove parent')
+    screen.wait(0.5)
+    screen.should_not_contain('Label A')
+    screen.should_not_contain('Label B')
+    screen.should_not_contain('Label C')
+    assert row.is_deleted
     assert a.is_deleted
     assert b.is_deleted
     assert c.is_deleted
