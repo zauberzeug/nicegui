ec7736246160a65b443489da42d69639827b32d0
Rodja Trappe <rodja@zauberzeug.com>
2023-08-29 16:45:35 +0200

    redirect middleware: only use prefix for / urls fixes #1517

--- nicegui/middlewares.py
+++ nicegui/middlewares.py
@@ -8,7 +8,7 @@ class RedirectWithPrefixMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         prefix = request.headers.get('X-Forwarded-Prefix', '')
         response = await call_next(request)
-        if 'Location' in response.headers:
+        if 'Location' in response.headers and response.headers['Location'].startswith('/'):
             new_location = prefix + response.headers['Location']
             response.headers['Location'] = new_location
         return response
