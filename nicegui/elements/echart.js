62b1d400691c1439d4a6afb3856df79a38cfe29f
thetableman <78121198+thetableman@users.noreply.github.com>
2023-09-08 07:46:19 +0930

    Update echart.js

--- nicegui/elements/echart.js
+++ nicegui/elements/echart.js
@@ -7,6 +7,20 @@ export default {
     convertDynamicProperties(this.options, true);
     this.chart.setOption(this.options);
     this.chart.resize();
+    function unpack(e) {
+      return {
+        component_type: e.componentType,
+        series_type: e.seriesType,
+        series_index: e.seriesIndex,
+        series_name: e.seriesName,
+        name: e.name,
+        data_index: e.dataIndex,
+        data: e.data,
+        data_type: e.dataType,
+        value: e.value
+      };
+    }
+    this.chart.on('click', e => this.$emit("pointClick", unpack(e)));
   },
   beforeDestroy() {
     this.destroyChart();
