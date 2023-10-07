2edde9a9bb3e881a6f15b7233e76d69aa52071a8
Falko Schindler <falko@zauberzeug.com>
2023-10-07 21:10:29 +0200

    review

--- nicegui/functions/open.py
+++ nicegui/functions/open.py
@@ -12,10 +12,9 @@ def open(target: Union[Callable[..., Any], str], new_tab: bool = False) -> None:
     This is a browser setting and cannot be changed by the application.
     You might want to use `ui.link` and its `new_tab` parameter instead.
 
-    Note: When using an `auto-index page </documentation#auto-index_page>`_ (eg. no `@page` decorator), 
+    Note: When using an `auto-index page </documentation#auto-index_page>`_ (e.g. no `@page` decorator), 
     all clients (i.e. browsers) connected to the page will open the target URL unless a socket is specified.
     User events like button clicks provide such a socket.
-
 
     :param target: page function or string that is a an absolute URL or relative path from base URL
     :param new_tab: whether to open the target in a new tab (might be blocked by the browser)
