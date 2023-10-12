a0df896c213b6115d2ac0fe929f2b9a056c62960
Rodja Trappe <rodja@zauberzeug.com>
2023-10-12 05:04:17 +0200

    more fly instances and reduced limits

--- set_scale.sh
+++ set_scale.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-fly scale count app=2  --region fra -y
-fly scale count app=2  --region iad -y
+fly scale count app=3  --region fra -y
+fly scale count app=3  --region iad -y
 fly scale count app=1  --region jnb -y
-fly scale count app=2  --region lax -y
-fly scale count app=2  --region lhr -y
-fly scale count app=1  --region bom -y
-fly scale count app=1  --region mad -y
-fly scale count app=2  --region mia -y
-fly scale count app=2  --region nrt -y
-fly scale count app=2  --region sea -y
-fly scale count app=2  --region sin -y
+fly scale count app=3  --region lax -y
+fly scale count app=3  --region lhr -y
+fly scale count app=2  --region bom -y
+fly scale count app=2  --region mad -y
+fly scale count app=3  --region mia -y
+fly scale count app=3  --region nrt -y
+fly scale count app=3  --region sea -y
+fly scale count app=3  --region sin -y
