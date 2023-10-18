0114f1d56b777a5c23b8c6902ed683f8bf963396
Cam <camerond.barts@gmail.com>
2023-10-18 15:13:21 -0400

    Added LangChain handler to the examples.
    Modeled after the [Streamlit handler](https://github.com/langchain-ai/langchain/blob/f4bec9686d6a2d07a0f0540b34f9313b0cd5ae89/libs/langchain/langchain/callbacks/streamlit/streamlit_callback_handler.py#L225C4-L225C4).

---
+++ examples/chat_with_ai/log_callback_handler.py
@@ -0,0 +1,54 @@
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.schema import AgentAction, AgentFinish
+from typing import Dict, Any, Optional
+from nicegui.element import Element
+
+
+class NiceGuiLogElementCallbackHandler(BaseCallbackHandler):
+    """Callback Handler that writes to the log element of NicGui."""
+
+    def __init__(self, element: Element) -> None:
+        """Initialize callback handler."""
+        self.element = element
+
+    def print_text(self, message: str) -> None:
+        self.element.push(message)
+
+    def on_chain_start(
+        self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any
+    ) -> None:
+        """Print out that we are entering a chain."""
+        self.print_text(
+            f"\n\n> Entering new {serialized['id'][-1]} chain...",
+        )
+
+    def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> None:
+        """Print out that we finished a chain."""
+        self.print_text("\n> Finished chain.")
+        self.print_text(f"\nOutputs: {outputs}")
+
+    def on_agent_action(self, action: AgentAction, **kwargs: Any) -> Any:
+        """Run on agent action."""
+        self.print_text(action.log)
+
+    def on_tool_end(
+        self,
+        output: str,
+        observation_prefix: Optional[str] = None,
+        llm_prefix: Optional[str] = None,
+        **kwargs: Any,
+    ) -> None:
+        """If not the final action, print out observation."""
+        if observation_prefix is not None:
+            self.print_text(f"\n{observation_prefix}")
+        self.print_text(output)
+        if llm_prefix is not None:
+            self.print_text(f"\n{llm_prefix}")
+
+    def on_text(self, text: str, **kwargs: Any) -> None:
+        """Run when agent ends."""
+        self.print_text(text)
+
+    def on_agent_finish(self, finish: AgentFinish, **kwargs: Any) -> None:
+        """Run on agent end."""
+        self.print_text(finish.log)
