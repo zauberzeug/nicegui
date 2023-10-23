0dfb32f80e5a5f8e6441e8585cb44a62cf396425
Falko Schindler <falko@zauberzeug.com>
2023-10-23 19:14:34 +0200

    update dependencies

--- nicegui/elements/lib/mermaid/mermaid.core.mjs
+++ nicegui/elements/lib/mermaid/mermaid.core.mjs
@@ -1,13 +1,14 @@
 import "ts-dedent";
-import { K } from "./mermaid-aad43469.js";
+import { L } from "./mermaid-485fd1a4.js";
 import "dayjs";
 import "@braintree/sanitize-url";
 import "d3";
 import "dompurify";
 import "khroma";
 import "lodash-es/memoize.js";
+import "lodash-es/merge.js";
 import "stylis";
 import "lodash-es/isEmpty.js";
 export {
-  K as default
+  L as default
 };
