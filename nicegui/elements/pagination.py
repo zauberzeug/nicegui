f4be80d18f4e21a833044dd5f422309ba532f178
Falko Schindler <falko@zauberzeug.com>
2023-10-27 07:54:58 +0200

    code review

--- nicegui/elements/pagination.py
+++ nicegui/elements/pagination.py
@@ -6,19 +6,18 @@ from nicegui.elements.mixins.value_element import ValueElement
 
 class Pagination(ValueElement, DisableableElement):
 
-    """Pagination
-
-        A pagination element wrapping Quasar's
-        `QPagination <https://quasar.dev/vue-components/pagination>`_ component.
-
-        :param min: minimum (start) value in the pagination element (e.g. 1)
-        :param max: miximum (end) value in the pagination element (e.g. 2, 10)
-        :param value: initial or current selection (defaults to min if no value provided)
-        :param on_change: callback to be invoked when the value changes
-    """
-
     def __init__(self, min: int, max: int, *, direction_links: bool = False,
                  value: Optional[int] = ..., on_change: Optional[Callable[..., Any]] = None) -> None:
+        """Pagination
+
+        A pagination element wrapping Quasar's `QPagination <https://quasar.dev/vue-components/pagination>`_ component.
+
+        :param min: minimum page number
+        :param max: maximum page number
+        :param direction_links: whether to show first/last page links
+        :param value: initial page (defaults to `min` if no value is provided)
+        :param on_change: callback to be invoked when the value changes
+        """
         super().__init__(tag='q-pagination', value=value, on_value_change=on_change)
         self._props['min'] = min
         self._props['max'] = max
--- tests/test_pagination.py
+++ tests/test_pagination.py
@@ -3,16 +3,18 @@ from nicegui import ui
 from .screen import Screen
 
 
-def test_min(screen: Screen):
-
-    ui.pagination(min=1, max=10)
-
-    screen.open('/')
-    screen.click('1')
-
-def test_max(screen: Screen):
-
-    ui.pagination(min=1, max=10)
+def test_pagination(screen: Screen):
+    p = ui.pagination(1, 10, direction_links=True)
+    ui.label().bind_text_from(p, 'value', lambda v: f'Page {v}')
 
     screen.open('/')
-    screen.click('10')
+    screen.should_contain('Page 1')
+
+    screen.click('7')
+    screen.should_contain('Page 7')
+
+    screen.click('keyboard_arrow_left')
+    screen.should_contain('Page 6')
+
+    screen.click('keyboard_arrow_right')
+    screen.should_contain('Page 7')
