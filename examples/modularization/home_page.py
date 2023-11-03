4e45816b875a6c53cdb0a3108293273711480a30
Falko Schindler <falko@zauberzeug.com>
2023-11-03 11:10:12 +0100

    code review

--- examples/modularization/home_page.py
+++ examples/modularization/home_page.py
@@ -1,7 +1,5 @@
 from message import message
 
-from nicegui import ui
-
 
 def content() -> None:
     message('This is the home page.').classes('font-bold')
--- examples/modularization/message.py
+++ examples/modularization/message.py
@@ -3,6 +3,6 @@ from nicegui import ui
 
 class message(ui.label):
 
-    def __init__(self, text: str):
+    def __init__(self, text: str) -> None:
         super().__init__(text)
         self.classes('text-h4 text-grey-8')
