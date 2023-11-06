f5d29f00eb19bc1c35f62f353b517cef84d02810
Falko Schindler <falko@zauberzeug.com>
2023-11-06 17:32:25 +0100

    raise RuntimeError if a page layout element is nested

--- nicegui/page_layout.py
+++ nicegui/page_layout.py
@@ -43,6 +43,7 @@ class Header(ValueElement):
         :param wrap: whether the header should wrap its content (default: `True`)
         :param add_scroll_padding: whether to automatically prevent link targets from being hidden behind the header (default: `True`)
         """
+        _check_current_slot()
         with context.get_client().layout:
             super().__init__(tag='q-header', value=value, on_value_change=None)
         self._classes.append('nicegui-header')
@@ -106,6 +107,7 @@ class Drawer(Element):
         :param top_corner: whether the drawer expands into the top corner (default: `False`)
         :param bottom_corner: whether the drawer expands into the bottom corner (default: `False`)
         """
+        _check_current_slot()
         with context.get_client().layout:
             super().__init__('q-drawer')
         if value is None:
@@ -224,6 +226,7 @@ class Footer(ValueElement):
         :param elevated: whether the footer should have a shadow (default: `False`)
         :param wrap: whether the footer should wrap its content (default: `True`)
         """
+        _check_current_slot()
         with context.get_client().layout:
             super().__init__(tag='q-footer', value=value, on_value_change=None)
         self.classes('nicegui-footer')
@@ -264,3 +267,9 @@ class PageSticky(Element):
         super().__init__('q-page-sticky')
         self._props['position'] = position
         self._props['offset'] = [x_offset, y_offset]
+
+
+def _check_current_slot() -> None:
+    parent = context.get_slot().parent
+    if parent != parent.client.content:
+        raise RuntimeError('Layout elements must not be nested but must be direct children of the page content.')
