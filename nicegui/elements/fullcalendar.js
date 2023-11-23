179589d5d6625032a8e6d89e77209ff8d0a2781b
frankvp11 <frankvanpaassen3@gmail.com>
2023-11-23 10:08:11 -0500

    "fixed" it working !!!?!? !?! it now shows a scuffed version on the screen

--- nicegui/elements/fullcalendar.js
+++ nicegui/elements/fullcalendar.js
@@ -1,3 +1,7 @@
+// import FullCalendar from "@fullcalendar/vue3";
+// import FullCalendar from "../elements/lib/fullcalendar/index.global.js";
+// import FullCalendar from "/index.global.min.js";
+import FullCalendar from "/_nicegui/1.4.0/libraries/f2236eb73ecd4b599de1d20d128079e5/index.global.min.js"; 
 export default {
   template: "<div></div>",
   props: {
