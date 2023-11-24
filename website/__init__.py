1c531cab169f9fde43d646159756d3c566f37c47
Falko Schindler <falko@zauberzeug.com>
2023-11-25 00:13:41 +0100

    remove obsolete code

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,8 @@
-from . import anti_scroll_hack, documentation_pages, fly, main_page, svg
+from . import anti_scroll_hack, documentation, fly, main_page, svg
 
 __all__ = [
     'anti_scroll_hack',
-    'documentation_pages',
+    'documentation',
     'fly',
     'main_page',
     'svg',
