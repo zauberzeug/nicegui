01ad722a3178aabbb1b861e031d8edf8b627140c
frankvp11 <frankvanpaassen3@gmail.com>
2023-12-01 11:02:36 -0500

    use vue3 version

--- nicegui/elements/fullcalendar.js
+++ nicegui/elements/fullcalendar.js
@@ -1,7 +1,8 @@
 // import FullCalendar from "@fullcalendar/vue3";
 // import FullCalendar from "../elements/lib/fullcalendar/index.global.js";
 // import FullCalendar from "/index.global.min.js";
-import FullCalendar from "/_nicegui/1.4.0/libraries/f2236eb73ecd4b599de1d20d128079e5/index.global.min.js"; 
+// import FullCalendar from "/_nicegui/1.4.0/libraries/f2236eb73ecd4b599de1d20d128079e5/index.global.min.js"; 
+import FullCalendar from "/_nicegui/1.4.0/libraries/1f5b0e0b0b0b0b0b0b0b0b0b0b0b0b0b/index.global.min.js";
 export default {
   template: "<div></div>",
   props: {
