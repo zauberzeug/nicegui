922e23ef72d9a96c42cc488535bf38017b1c49bb
Falko Schindler <falko@zauberzeug.com>
2023-12-01 10:31:27 +0100

    keep search index in memory

--- website/documentation/search.py
+++ website/documentation/search.py
@@ -1,28 +1,38 @@
-import json
 from pathlib import Path
+
+from fastapi.responses import JSONResponse
+
+from nicegui import app
 
 from ..examples import examples
 from .content import registry
 
 PATH = Path(__file__).parent.parent / 'static' / 'search_index.json'
+search_index: list[dict[str, str]] = []
+
+
+@app.get('/static/search_index.json')
+def _get_search_index() -> JSONResponse:
+    return JSONResponse(search_index)
 
 
 def build_search_index() -> None:
     """Build search index."""
-    index = [
+    search_index.clear()
+    search_index.extend([
         {
             'title': f'{documentation.heading.replace("*", "")}: {part.title}',
-            'content': part.description,
+            'content': part.description or '',
             'url': f'{documentation.name}#{part.link_target}',
         }
         for documentation in registry.values()
         for part in documentation.parts
-    ] + [
+    ])
+    search_index.extend([
         {
             'title': f'Example: {example.title}',
             'content': example.description,
             'url': example.url,
         }
         for example in examples
-    ]
-    PATH.write_text(json.dumps(index, indent=2))
+    ])
