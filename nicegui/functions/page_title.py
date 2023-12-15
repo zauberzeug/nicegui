6f07092bfda3371241b391e817bd5f549b403b2d
Falko Schindler <falko@zauberzeug.com>
2023-12-15 12:50:18 +0100

    introduce `ui.page_title`

---
+++ nicegui/functions/page_title.py
@@ -0,0 +1,12 @@
+from .. import context, json
+
+
+def page_title(title: str) -> None:
+    """Set the page title for the current client.
+
+    :param title: page title
+    """
+    client = context.get_client()
+    client.title = title
+    if client.has_socket_connection:
+        client.run_javascript(f'document.title = {json.dumps(title)}')
