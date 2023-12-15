1ecbd5f75c9fb82473ffd91298b78fdb718e77c4
Rodja Trappe <rodja@zauberzeug.com>
2023-12-15 06:47:32 +0100

    cleanup

--- website/documentation/content/leaflet_documentation.py
+++ website/documentation/content/leaflet_documentation.py
@@ -42,7 +42,7 @@ def markers() -> None:
     m.on('click', lambda e: m.marker(location=e.args['latlng']))
 
 
-@doc.demo('Add Vector Layers', '''
+@doc.demo('Vector Layers', '''
     Leaflet supports a set of [vector layers](https://leafletjs.com/reference.html#:~:text=VideoOverlay-,Vector%20Layers,-Path) like circle, polygon etc.
     These can be added with the `generic_layer` method.
     We are happy to review any pull requests to add more specific layers to simplify usage.
