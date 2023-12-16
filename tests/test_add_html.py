e916aa9a000a0e66c0c9bd6604d9265dcd76f73b
Falko Schindler <falko@zauberzeug.com>
2023-12-16 21:51:38 +0100

    allow setting head and body html when client is already connected

---
+++ tests/test_add_html.py
@@ -0,0 +1,16 @@
+from nicegui import ui
+
+from .screen import Screen
+
+
+def test_add_head_html(screen: Screen) -> None:
+    ui.add_head_html(r'<style>.my-label {background: rgb(0, 0, 255)}</style>')
+    ui.label('Label').classes('my-label')
+    ui.button('Green', on_click=lambda: ui.add_head_html(r'<style>.my-label {background: rgb(0, 255, 0)}</style>'))
+
+    screen.open('/')
+    assert screen.find('Label').value_of_css_property('background-color') == 'rgba(0, 0, 255, 1)'
+
+    screen.click('Green')
+    screen.wait(0.5)
+    assert screen.find('Label').value_of_css_property('background-color') == 'rgba(0, 255, 0, 1)'
