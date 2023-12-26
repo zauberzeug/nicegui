36251120f4ed526aa1c3ed6453a8fe884e89ec80
Rodja Trappe <rodja@zauberzeug.com>
2023-12-26 05:57:14 +0000

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,2 @@
 
-pushd website
-./build_search_index.py
-popd
 fly deploy --wait-timeout 360 --build-arg VERSION=$(git describe --abbrev=0 --tags --match 'v*' 2>/dev/null | sed 's/^v//' || echo '0.0.0') 
