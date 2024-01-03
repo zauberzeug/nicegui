700b1c567a04e6946a49dbf81434ea81c175fd0a
Simon Robinson <78121198+thetableman@users.noreply.github.com>
2024-01-04 09:09:26 +1030

    fix_function_name

--- website/documentation/content/expansion_documentation.py
+++ website/documentation/content/expansion_documentation.py
@@ -30,7 +30,7 @@ def expansion_with_caption():
 @doc.demo('Expansion with Grouping', '''
     An expansion group can be defined to enable coordinated open/close states a.k.a. 'accordion mode'.
 ''')
-def expansion_with_caption():
+def expansion_with_grouping():
     with ui.expansion(text='Expand One!', group='group'):
         ui.label('inside expansion one')
     with ui.expansion(text='Expand Two!', group='group'):
