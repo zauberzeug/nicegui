5da8520e35995b12dbbe73bccd68b0334e23aaca
John Paul <iamjohnpaulr5@gmail.com>
2024-01-06 00:45:58 +0530

    Update main.py with major clean code

--- main.py
+++ main.py
@@ -9,47 +9,51 @@ import prometheus
 from nicegui import app, ui
 from website import anti_scroll_hack, documentation, fly, main_page, svg
 
+# Start Prometheus monitoring
 prometheus.start_monitor(app)
 
-# session middleware is required for demo in documentation and prometheus
+# Session middleware is required for demo in documentation and Prometheus
 app.add_middleware(SessionMiddleware, secret_key=os.environ.get('NICEGUI_SECRET_KEY', ''))
 
+# Initialize Fly related modules
 on_fly = fly.setup()
 anti_scroll_hack.setup()
 
+# Add static files
 app.add_static_files('/favicon', str(Path(__file__).parent / 'website' / 'favicon'))
 app.add_static_files('/fonts', str(Path(__file__).parent / 'website' / 'fonts'))
 app.add_static_files('/static', str(Path(__file__).parent / 'website' / 'static'))
 app.add_static_file(local_file=svg.PATH / 'logo.png', url_path='/logo.png')
 app.add_static_file(local_file=svg.PATH / 'logo_square.png', url_path='/logo_square.png')
 
+# Build documentation search index
 documentation.build_search_index()
 
-
+# Dark mode endpoint
 @app.post('/dark_mode')
 async def _post_dark_mode(request: Request) -> None:
     app.storage.browser['dark_mode'] = (await request.json()).get('value')
 
-
+# Main page
 @ui.page('/')
 def _main_page() -> None:
     main_page.create()
 
-
+# Documentation pages
 @ui.page('/documentation')
 def _documentation_page() -> None:
     documentation.render_page(documentation.registry[''], with_menu=False)
-
 
 @ui.page('/documentation/{name}')
 def _documentation_detail_page(name: str) -> None:
     documentation.render_page(documentation.registry[name])
 
-
+# Status endpoint
 @app.get('/status')
 def _status():
     return 'Ok'
 
-
+# Run the application
 # NOTE: do not reload on fly.io (see https://github.com/zauberzeug/nicegui/discussions/1720#discussioncomment-7288741)
 ui.run(uvicorn_reload_includes='*.py, *.css, *.html', reload=not on_fly, reconnect_timeout=10.0)
+
