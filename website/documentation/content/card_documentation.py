5dbded10c3e199c73d0221c22a347dd4943efc8f
Falko Schindler <falko@zauberzeug.com>
2024-01-06 14:39:58 +0100

    remove obsolete documentation

--- website/documentation/content/card_documentation.py
+++ website/documentation/content/card_documentation.py
@@ -20,34 +20,4 @@ def card_without_shadow() -> None:
         ui.label('See, no shadow!')
 
 
-@doc.demo('The issue with nested borders', '''
-    The following example shows a table nested in a card.
-    Cards have a default padding in NiceGUI, so the table is not flush with the card's border.
-    The table has the `flat` and `bordered` props set, so it should have a border.
-    However, due to the way QCard is designed, the border is not visible (first card).
-    There are two ways to fix this:
-
-    - To get the original QCard behavior, use the `tight` method (second card).
-        It removes the padding and the table border collapses with the card border.
-    
-    - To preserve the padding _and_ the table border, move the table into another container like a `ui.row` (third card).
-
-    See https://github.com/zauberzeug/nicegui/issues/726 for more information.
-''')
-def custom_context_menu() -> None:
-    columns = [{'name': 'age', 'label': 'Age', 'field': 'age'}]
-    rows = [{'age': '16'}, {'age': '18'}, {'age': '21'}]
-
-    with ui.row():
-        with ui.card():
-            ui.table(columns, rows).props('flat bordered')
-
-        with ui.card().tight():
-            ui.table(columns, rows).props('flat bordered')
-
-        with ui.card():
-            with ui.row():
-                ui.table(columns, rows).props('flat bordered')
-
-
 doc.reference(ui.card)
