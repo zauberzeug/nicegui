56c755a29709a26638e9eb0110278220490e4e10
Falko Schindler <falko@zauberzeug.com>
2024-01-14 15:51:38 +0100

    fix copy-to-clipboard button on safari (fixes #2377)

--- nicegui/elements/code.py
+++ nicegui/elements/code.py
@@ -3,7 +3,6 @@ import time
 from typing import Optional
 
 from ..element import Element
-from ..functions.javascript import run_javascript
 from .button import Button as button
 from .markdown import Markdown as markdown
 from .markdown import remove_indentation
@@ -27,16 +26,16 @@ class Code(Element):
 
         with self:
             self.markdown = markdown(f'```{language}\n{self.content}\n```').classes('overflow-auto')
-            self.copy_button = button(icon='content_copy', on_click=self.copy_to_clipboard) \
+            self.copy_button = button(icon='content_copy', on_click=self.show_checkmark) \
                 .props('round flat size=sm').classes('absolute right-2 top-2 opacity-20 hover:opacity-80')
+            self.copy_button._props['onclick'] = f'navigator.clipboard.writeText(`{self.content}`)'
 
         self._last_scroll: float = 0.0
         self.markdown.on('scroll', self._handle_scroll)
         timer(0.1, self._update_copy_button)
 
-    async def copy_to_clipboard(self) -> None:
-        """Copy the code to the clipboard."""
-        run_javascript('navigator.clipboard.writeText(`' + self.content + '`)')
+    async def show_checkmark(self) -> None:
+        """Show a checkmark icon for 3 seconds."""
         self.copy_button.props('icon=check')
         await asyncio.sleep(3.0)
         self.copy_button.props('icon=content_copy')
