328e3ad6e3b630da4161b53092b6869046dc4b02
Falko Schindler <falko@zauberzeug.com>
2024-01-19 10:00:18 +0100

    fix copy-to-clipboard button with special characters (fixes #2400)

--- nicegui/elements/code.py
+++ nicegui/elements/code.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 from typing import Optional
 
+from .. import json
 from ..element import Element
 from .button import Button as button
 from .markdown import Markdown as markdown
@@ -28,7 +29,7 @@ class Code(Element):
             self.markdown = markdown(f'```{language}\n{self.content}\n```').classes('overflow-auto')
             self.copy_button = button(icon='content_copy', on_click=self.show_checkmark) \
                 .props('round flat size=sm').classes('absolute right-2 top-2 opacity-20 hover:opacity-80')
-            self.copy_button._props['onclick'] = f'navigator.clipboard.writeText(`{self.content}`)'
+            self.copy_button._props['onclick'] = f'navigator.clipboard.writeText({json.dumps(self.content)})'
 
         self._last_scroll: float = 0.0
         self.markdown.on('scroll', self._handle_scroll)
