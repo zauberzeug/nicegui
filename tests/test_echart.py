45ee898cea83e3ec0223355d2209f8325abdf69d
CrystalWindSnake <568166495@qq.com>
2024-01-21 01:47:17 +0800

    add unit tests

--- tests/test_echart.py
+++ tests/test_echart.py
@@ -1,3 +1,7 @@
+from pyecharts import options
+from pyecharts.charts import Bar
+from pyecharts.commons import utils
+
 from nicegui import ui
 from nicegui.testing import Screen
 
@@ -76,3 +80,39 @@ def test_run_method(screen: Screen):
     screen.open('/')
     screen.click('Get Width')
     screen.should_contain('Width: 600px')
+
+
+
+def test_create_from_pyecharts(screen: Screen):
+    xaxis_formatter = r'(val, idx) => `x for ${val}`'
+    yaxis_formatter = r'(val, idx) => `${val} kg`'
+    get_options_js = "echarts.getInstanceByDom(document.querySelector('.nicegui-echart')).getOption()"
+
+    ui.echart.from_pyecharts(
+        Bar()
+        .add_xaxis(['A', 'B', 'C'])
+        .add_yaxis('series A', [0.1,0.2,0.3],)
+        .set_global_opts(
+            xaxis_opts=options.AxisOpts(axislabel_opts={':formatter': xaxis_formatter}),
+            yaxis_opts=options.AxisOpts(axislabel_opts={'formatter': utils.JsCode(yaxis_formatter)}),
+        )
+    )
+    
+    label = ui.label('')
+
+    async def get_xAxis_formatter():
+        type_str,formatter= await ui.run_javascript(f"const fm={get_options_js}.xAxis[0].axisLabel.formatter;[typeof fm,fm.toString()]")
+        label.set_text(f"xAxis formatter is {type_str}: {formatter}")
+    ui.button('Get xAxis formatter', on_click=get_xAxis_formatter)
+
+    async def get_yAxis_formatter():
+        type_str,formatter= await ui.run_javascript(f"const fm={get_options_js}.yAxis[0].axisLabel.formatter;[typeof fm,fm.toString()]")
+        label.set_text(f"yAxis formatter is {type_str}: {formatter}")
+    ui.button('Get yAxis formatter', on_click=get_yAxis_formatter)
+
+    screen.open('/')
+    screen.click('Get xAxis formatter')
+    screen.should_contain(f"formatter is function: {xaxis_formatter}")
+
+    screen.click('Get yAxis formatter')
+    screen.should_contain(f"formatter is function: {yaxis_formatter}")
