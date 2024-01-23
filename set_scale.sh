b79ed7174679b0a8fd762ff779c1ce0403df8bfb
Rodja Trappe <rodja@zauberzeug.com>
2024-01-23 06:42:12 +0100

    adjust number of servers

--- set_scale.sh
+++ set_scale.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-fly scale count app=7  --region fra -y
+fly scale count app=5  --region fra -y
 fly scale count app=3  --region iad -y
 fly scale count app=1  --region jnb -y
 fly scale count app=3  --region lax -y
-fly scale count app=6  --region lhr -y
-fly scale count app=4  --region cdg -y
+fly scale count app=4  --region lhr -y
+fly scale count app=3  --region cdg -y
 fly scale count app=2  --region bom -y
-fly scale count app=3  --region mad -y
+fly scale count app=2  --region mad -y
 fly scale count app=3  --region mia -y
 fly scale count app=3  --region nrt -y
 fly scale count app=3  --region sea -y
