c9639126d1b83fcff7a2cf68e1c0dc6b2143de64
Rodja Trappe <rodja@zauberzeug.com>
2024-01-25 05:22:41 +0100

    fix example for Python 3.8

--- examples/websockets/main.py
+++ examples/websockets/main.py
@@ -5,13 +5,14 @@ NOTE: NiceGUI already handles all the communication for you so you don't need to
 This example is only for advanced use cases where you want to allow other, non-NiceGUI clients to connect to your server.
 """
 import asyncio
+from typing import Set
 
 import websockets
 from websockets.server import WebSocketServerProtocol
 
 from nicegui import app, ui
 
-CONNECTIONS: set[WebSocketServerProtocol] = set()
+CONNECTIONS: Set[WebSocketServerProtocol] = set()
 
 ui.label('Websockets demo').classes('text-2xl')
 ui.label('Run this in the console to connect:')
