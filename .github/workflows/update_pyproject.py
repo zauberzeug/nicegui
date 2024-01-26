8ad2757973ccb32db1f51d0c404ea8120ad3b72d
Rodja Trappe <rodja@zauberzeug.com>
2024-01-26 17:01:05 +0100

    use correct version (#2345)

--- .github/workflows/update_pyproject.py
+++ .github/workflows/update_pyproject.py
@@ -6,5 +6,6 @@ from pathlib import Path
 if __name__ == '__main__':
     path = Path('pyproject.toml')
     content = path.read_text(encoding='utf-8')
-    content = re.sub(r'version = "[0-9]+\.[0-9]+\.[0-9]+-dev"', f'version = "{sys.argv[1]}-dev"', content)
+    version = sys.argv[1].removeprefix('v')
+    content = re.sub(r'version = "[0-9]+\.[0-9]+\.[0-9]+-dev"', f'version = "{version}-dev"', content)
     path.write_text(content, encoding='utf-8')
