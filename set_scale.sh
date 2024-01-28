18597010f656a25b49ff0bbba2b1dadef8b9e615
Rodja Trappe <rodja@zauberzeug.com>
2024-01-28 13:08:26 +0100

    fix merge conflict

--- set_scale.sh
+++ set_scale.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-<<<<<<< Updated upstream
-fly scale count app=5  --region fra -y
-fly scale count app=3  --region iad -y
-fly scale count app=1  --region jnb -y
-fly scale count app=3  --region lax -y
-fly scale count app=4  --region lhr -y
-fly scale count app=3  --region cdg -y
-fly scale count app=2  --region bom -y
-fly scale count app=2  --region mad -y
-fly scale count app=3  --region mia -y
-fly scale count app=3  --region nrt -y
-fly scale count app=3  --region sea -y
-fly scale count app=3  --region sin -y
-=======
 fly scale count app=1  --region fra -y # Frankfurt, Germany
 fly scale count app=1  --region ams -y # Amsterdam, Netherlands
 fly scale count app=1  --region mad -y # Madrid, Spain
@@ -31,4 +17,3 @@ fly scale count app=1  --region jnb -y # Johannesburg, South Africa
 fly scale count app=1  --region bom -y # Mumbai, India
 fly scale count app=1  --region nrt -y # Tokyo, Japan
 fly scale count app=1  --region sin -y # Singapor
->>>>>>> Stashed changes
