b2ae750a0047d9096f55022f689ee0829a14cd1a
Falko Schindler <falko@zauberzeug.com>
2024-02-09 20:19:06 +0100

    introduce `ui.add_style` for CSS, SASS and SCSS

--- nicegui/ui.py
+++ nicegui/ui.py
@@ -97,6 +97,7 @@ __all__ = [
     'refreshable',
     'refreshable_method',
     'state',
+    'add_style',
     'update',
     'page',
     'drawer',
@@ -206,6 +207,7 @@ from .functions.on import on
 from .functions.open import open  # pylint: disable=redefined-builtin
 from .functions.page_title import page_title
 from .functions.refreshable import refreshable, refreshable_method, state
+from .functions.style import add_style
 from .functions.update import update
 from .page import page
 from .page_layout import Drawer as drawer
