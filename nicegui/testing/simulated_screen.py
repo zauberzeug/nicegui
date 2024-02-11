500aa8d1372629b61f4efc4ce7ca588a312006c1
Rodja Trappe <rodja@zauberzeug.com>
2024-02-11 16:49:19 +0100

    docstrings

--- nicegui/testing/simulated_screen.py
+++ nicegui/testing/simulated_screen.py
@@ -9,7 +9,7 @@ from nicegui.elements.mixins.content_element import ContentElement
 
 
 def app_loop(func: Callable) -> Callable:
-    """Decorator to run ensure a function is run in the event loop of the app."""
+    """Decorator to ensure a function is run in the event loop of the app."""
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         if not core.loop:
@@ -24,8 +24,28 @@ class SimulatedScreen:
         self.test_client = test_client
 
     def open(self, path: str) -> None:
+        """Open the given path."""
         response = self.test_client.get(path)
         assert response.status_code == 200
+
+    @app_loop
+    def should_contain(self, string: str) -> None:
+        """Assert that the page contains an input with the given value."""
+        if self._find(context.get_client().page_container, string) is not None:
+            return
+        for m in context.get_client().outbox.messages:
+            if m[1] == 'notify' and string in m[2]['message']:
+                return
+        raise AssertionError(f'text "{string}" not found on current screen')
+
+    @app_loop
+    def click(self, target_text: str) -> None:
+        """Click on the element containing the given text."""
+        element = self._find(context.get_client().page_container, target_text)
+        assert element
+        for listener in element._event_listeners.values():
+            if listener.type == 'click' and listener.element_id == element.id:
+                element._handle_event({'listener_id': listener.id, 'args': {}})
 
     def _find(self, element: ui.element, string: str) -> ui.element | None:
         text = element._text or ''
@@ -39,20 +59,3 @@ class SimulatedScreen:
             if found:
                 return found
         return None
-
-    @app_loop
-    def should_contain(self, string: str) -> None:
-        if self._find(context.get_client().page_container, string) is not None:
-            return
-        for m in context.get_client().outbox.messages:
-            if m[1] == 'notify' and string in m[2]['message']:
-                return
-        raise AssertionError(f'text "{string}" not found on current screen')
-
-    @app_loop
-    def click(self, target_text: str) -> None:
-        element = self._find(context.get_client().page_container, target_text)
-        assert element
-        for listener in element._event_listeners.values():
-            if listener.type == 'click' and listener.element_id == element.id:
-                element._handle_event({'listener_id': listener.id, 'args': {}})
