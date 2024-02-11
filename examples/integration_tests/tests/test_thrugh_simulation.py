969761c08b7b02d9b16c4e2276abc0bafaa4ad5e
Rodja Trappe <rodja@zauberzeug.com>
2024-02-11 14:05:09 +0100

    fix click in simulated screen

--- examples/integration_tests/tests/test_thrugh_simulation.py
+++ examples/integration_tests/tests/test_thrugh_simulation.py
@@ -1,3 +1,4 @@
+import asyncio
 import importlib
 
 from nicegui.testing import SimulatedScreen
@@ -5,8 +6,31 @@ from nicegui.testing import SimulatedScreen
 from .. import main
 
 
-def test_quick(simulated_screen: SimulatedScreen) -> None:
+def test_markdown_message(simulated_screen: SimulatedScreen) -> None:
     importlib.reload(main)
 
     simulated_screen.open('/')
     simulated_screen.should_contain('Try running')
+
+
+def test_button_click(simulated_screen: SimulatedScreen) -> None:
+    importlib.reload(main)
+
+    simulated_screen.open('/')
+    simulated_screen.click('Click me')
+    simulated_screen.should_contain('Button clicked!')
+
+
+def test_sub_page(simulated_screen: SimulatedScreen) -> None:
+    importlib.reload(main)
+
+    simulated_screen.open('/subpage')
+    simulated_screen.should_contain('This is a subpage')
+
+
+def test_with_connected(simulated_screen: SimulatedScreen) -> None:
+    importlib.reload(main)
+
+    simulated_screen.open('/with_connected')
+    simulated_screen.should_contain('This is a subpage')
+    simulated_screen.should_contain('Connected!')
--- nicegui/testing/simulated_screen.py
+++ nicegui/testing/simulated_screen.py
@@ -12,19 +12,30 @@ class SimulatedScreen:
         routes = dict((v, k) for k, v in Client.page_routes.items())
         routes[path]()
 
-    def _find(self, element: ui.element, string: str) -> bool:
+    def _find(self, element: ui.element, string: str) -> ui.element | None:
         text = element._text or ''
         label = element._props.get('label') or ''
         content = element.content if isinstance(element, ContentElement) else ''
-        return (
-            any(string in t for t in [text, label, content]) or
-            any(self._find(child, string) for child in element)
-        )
+        for t in [text, label, content]:
+            if string in t:
+                return element
+        for child in element:
+            found = self._find(child, string)
+            if found:
+                return found
+        return None
 
     def should_contain(self, string: str) -> None:
-        assert self._find(context.get_client().page_container, string)
+        if self._find(context.get_client().page_container, string) is not None:
+            return
+        for m in context.get_client().outbox.messages:
+            if m[1] == 'notify' and string in m[2]['message']:
+                return
+        raise AssertionError(f'text "{string}" not found on current screen')
 
-    def click(self, element: ui.element) -> None:
+    def click(self, target_text: str) -> None:
+        element = self._find(context.get_client().page_container, target_text)
+        assert element
         for listener in element._event_listeners.values():
             if listener.type == 'click' and listener.element_id == element.id:
                 element._handle_event({'listener_id': listener.id, 'args': {}})
