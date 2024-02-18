95fd232e0f6e8b2f83eae3e10e28b18df258c206
Rodja Trappe <rodja@zauberzeug.com>
2024-02-18 03:31:19 +0100

    cleanup

--- examples/chat_app/test_chat_app.py
+++ examples/chat_app/test_chat_app.py
@@ -1,27 +1,30 @@
-import asyncio
-
 import pytest
 
 import main
 from nicegui.testing import SimulatedScreen
 
-# pylint: disable=missing-function-docstring
-
 
 @pytest.mark.module_under_test(main)
-async def test_start_screen(screen: SimulatedScreen) -> None:
+async def test_basic_startup_appearance(screen: SimulatedScreen) -> None:
+    """Test the basic appearance of the chat app."""
     with await screen.open('/'):
         await screen.should_contain('simple chat app')
         await screen.should_contain('https://robohash.org/')
         await screen.should_contain('message')
-        await asyncio.sleep(1)
         await screen.should_contain('No messages yet')
 
 
 @pytest.mark.module_under_test(main)
-async def test_sending_message(screen: SimulatedScreen) -> None:
+async def test_sending_messages(screen: SimulatedScreen) -> None:
+    """Test sending messages from two different screens."""
     with await screen.open('/') as screenA:
-        screen.type(target='message', content='Hello, chat!', confirmation='keydown.enter')
-        await screen.should_contain('Hello, chat!')
-    with await screen.open('/') as screenB:
-        await screen.should_contain('Hello, chat!')
+        screen.type(target='message', content='Hello from screen A!', confirmation='keydown.enter')
+        await screen.should_contain('Hello from screen A!')
+        await screen.should_contain('message')
+    with await screen.open('/'):
+        await screen.should_contain('Hello from screen A!')
+        screen.type(target='message', content='Hello, from screen B!', confirmation='keydown.enter')
+        await screen.should_contain('message')
+    with screenA:
+        await screen.should_contain('Hello from screen A!')
+        await screen.should_contain('Hello, from screen B!')
