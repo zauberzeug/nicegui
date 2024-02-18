693dfbf9ad47f62c67efd6686fea6af42e1737cc
Rodja Trappe <rodja@zauberzeug.com>
2024-02-18 03:40:52 +0100

    ignore test_ files when building link examples

--- examples/chat_app/test_chat_app.py
+++ examples/chat_app/test_chat_app.py
@@ -6,7 +6,7 @@ from nicegui.testing import SimulatedScreen
 
 @pytest.mark.module_under_test(main)
 async def test_basic_startup_appearance(screen: SimulatedScreen) -> None:
-    """Test the basic appearance of the chat app."""
+    """Test basic appearance of the chat app."""
     with await screen.open('/'):
         await screen.should_contain('simple chat app')
         await screen.should_contain('https://robohash.org/')
