56a26c7042b58f6830ed9c7f10930e713abfb7df
Alejandro Gil <ZeroCool940711@users.noreply.github.com>
2024-02-21 20:33:35 -0700

    Added all the supported configurations for both the linear and circular progress bar as parameters.

--- nicegui/elements/progress.py
+++ nicegui/elements/progress.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Union
 
 from .label import Label as label
 from .mixins.color_elements import TextColorElement
@@ -6,14 +6,28 @@ from .mixins.value_element import ValueElement
 
 
 class LinearProgress(ValueElement, TextColorElement):
-    VALUE_PROP = 'value'
+    VALUE_PROP = "value"
 
-    def __init__(self,
-                 value: float = 0.0, *,
-                 size: Optional[str] = None,
-                 show_value: bool = True,
-                 color: Optional[str] = 'primary',
-                 ) -> None:
+    def __init__(
+        self,
+        value: float = 0.0,
+        *,
+        min: float = 0.0,  # pylint: disable=redefined-builtin
+        max: Optional[Union[str, int]] = 1.0,  # pylint: disable=redefined-builtin
+        size: Optional[Union[str, int]] = None,
+        show_value: bool = True,
+        color: Optional[str] = "primary",
+        reverse: bool = False,
+        buffer: Optional[float] = None,
+        track_color=None,
+        dark: bool = False,
+        rounded: bool = False,
+        animation_speed: int = 2100,
+        indeterminate: bool = False,
+        query: bool = False,
+        instant_feedback: bool = False,
+        stripe: bool = False,
+    ) -> None:
         """Linear Progress
 
         A linear progress bar wrapping Quasar's
@@ -24,25 +38,72 @@ class LinearProgress(ValueElement, TextColorElement):
         :param show_value: whether to show a value label in the center (default: `True`)
         :param color: color (either a Quasar, Tailwind, or CSS color or `None`, default: "primary")
         """
-        super().__init__(tag='q-linear-progress', value=value, on_value_change=None, text_color=color)
-        self._props['size'] = size if size is not None else '20px' if show_value else '4px'
+        if max > 1.0:
+            value = float(value / max)
+
+        super().__init__(
+            tag="q-linear-progress",
+            value=value,
+            on_value_change=None,
+            text_color=color,
+        )
+        self._props["min"] = min
+        self._props["max"] = max
+
+        if isinstance(size, int):
+            size = str(size) + "px"
+        self._props["size"] = size
+
+        self._props["reverse"] = reverse
+        self._props["buffer"] = buffer / max if buffer else None
+
+        self._props[
+            "show-value"
+        ] = True  # NOTE always activate the default slot because this is expected by ui.element
+
+        self._props["track-color"] = track_color
+        self._props["dark"] = dark
+        self._props["rounded"] = rounded
+        self._props["animation-speed"] = animation_speed
+        self._props["indeterminate"] = indeterminate
+        self._props["query"] = query
+        self._props["instant-feedback"] = instant_feedback
+        self._props["stripe"] = stripe
 
         if show_value:
             with self:
-                label().classes('absolute-center text-sm text-white').bind_text_from(self, 'value')
+                label().classes("absolute-center text-sm text-white").bind_text_from(
+                    self,
+                    "value",
+                    backward=lambda v=value: int(v * max).__round__(0)
+                    if max > 1.0
+                    else float(v * max),
+                )
 
 
 class CircularProgress(ValueElement, TextColorElement):
-    VALUE_PROP = 'value'
+    VALUE_PROP = "value"
 
-    def __init__(self,
-                 value: float = 0.0, *,
-                 min: float = 0.0,  # pylint: disable=redefined-builtin
-                 max: float = 1.0,  # pylint: disable=redefined-builtin
-                 size: str = 'xl',
-                 show_value: bool = True,
-                 color: Optional[str] = 'primary',
-                 ) -> None:
+    def __init__(
+        self,
+        value: float = 0.0,
+        *,
+        min: float = 0.0,  # pylint: disable=redefined-builtin
+        max: float = 1.0,  # pylint: disable=redefined-builtin
+        size: Optional[Union[str, int]] = "xl",
+        font_size: Optional[Union[str, int]] = "0.25em",
+        show_value: bool = True,
+        angle: int = 0,
+        color: Optional[str] = "primary",
+        center_color: Optional[str] = None,
+        track_color: Optional[str] = "grey-4",
+        indeterminate: bool = False,
+        reverse: bool = False,
+        instant_feedback: bool = False,
+        rounded: bool = False,
+        thickness: Optional[Union[float, int]] = 0.2,
+        animation_speed: int = 600,
+    ) -> None:
         """Circular Progress
 
         A circular progress bar wrapping Quasar's
@@ -55,13 +116,38 @@ class CircularProgress(ValueElement, TextColorElement):
         :param show_value: whether to show a value label in the center (default: `True`)
         :param color: color (either a Quasar, Tailwind, or CSS color or `None`, default: "primary")
         """
-        super().__init__(tag='q-circular-progress', value=value, on_value_change=None, text_color=color)
-        self._props['min'] = min
-        self._props['max'] = max
-        self._props['size'] = size
-        self._props['show-value'] = True  # NOTE always activate the default slot because this is expected by ui.element
-        self._props['track-color'] = 'grey-4'
+        super().__init__(
+            tag="q-circular-progress",
+            value=value,
+            on_value_change=None,
+            text_color=color,
+        )
+        self._props["min"] = min
+        self._props["max"] = max
+
+        if isinstance(size, int):
+            size = str(size) + "px"
+        self._props["size"] = size
+
+        if isinstance(font_size, int):
+            font_size = str(font_size) + "px"
+        self._props["font_size"] = font_size
+
+        self._props["angle"] = angle
+
+        self._props[
+            "show-value"
+        ] = True  # NOTE always activate the default slot because this is expected by ui.element
+
+        self._props["track-color"] = track_color
+        self._props["center-color"] = center_color
+        self._props["indeterminate"] = indeterminate
+        self._props["reverse"] = reverse
+        self._props["instant-feedback"] = instant_feedback
+        self._props["rounded"] = rounded
+        self._props["thickness"] = thickness
+        self._props["animation-speed"] = animation_speed
 
         if show_value:
             with self:
-                label().classes('absolute-center text-xs').bind_text_from(self, 'value')
+                label().classes("absolute-center text-xs").bind_text_from(self, "value")
