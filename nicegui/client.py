90fd5bbac34caeaf68dd5bcb0c67b47f5bc38721
Johannes Voscort <johannes.voscort@codecentric.de>
2024-02-23 13:18:26 +0100

    fix: order of remove elements

--- nicegui/client.py
+++ nicegui/client.py
@@ -294,12 +294,12 @@ class Client:
         """Remove the given elements from the client."""
         binding.remove(elements)
         element_ids = [element.id for element in elements]
-        for element_id in element_ids:
-            del self.elements[element_id]
         for element in elements:
             element._handle_delete()  # pylint: disable=protected-access
             element._deleted = True  # pylint: disable=protected-access
             self.outbox.enqueue_delete(element)
+        for element_id in element_ids:
+            del self.elements[element_id]
 
     def remove_all_elements(self) -> None:
         """Remove all elements from the client."""
