7d0e0ee1a08c795200085380195e682d792a0a47
Falko Schindler <falko@zauberzeug.com>
2024-02-24 16:26:59 +0100

    move navigate functions into a class

--- nicegui/ui.py
+++ nicegui/ui.py
@@ -201,10 +201,10 @@ from .elements.tooltip import Tooltip as tooltip
 from .elements.tree import Tree as tree
 from .elements.upload import Upload as upload
 from .elements.video import Video as video
-from .functions import navigate
 from .functions.download import download
 from .functions.html import add_body_html, add_head_html
 from .functions.javascript import run_javascript
+from .functions.navigate import Navigate as navigate
 from .functions.notify import notify
 from .functions.on import on
 from .functions.open import open  # pylint: disable=redefined-builtin
