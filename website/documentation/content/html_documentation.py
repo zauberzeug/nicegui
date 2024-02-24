8c23ccafb8fee3b414d9ebe8cf3e7f3fa3be584f
Peter Kleynjan <peter.kleynjan@quantis.nl>
2024-02-24 17:30:59 +0100

    allow other tags than div in ui.html

--- website/documentation/content/html_documentation.py
+++ website/documentation/content/html_documentation.py
@@ -8,4 +8,11 @@ def main_demo() -> None:
     ui.html('This is <strong>HTML</strong>.')
 
 
+@doc.demo('Producing in-line elements', '''
+    Use the `tag` parameter to produce something other than a div.
+''')
+def demo_inline() -> None:
+    ui.html('This is rendered as a paragraph.', tag='p')
+
+
 doc.reference(ui.html)
