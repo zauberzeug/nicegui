4d0f61d66d23ff0cfd0bffed4928acb07f9a7ab8
Rodja Trappe <rodja@zauberzeug.com>
2024-02-25 05:24:13 +0100

    do not warn when using ui.open

--- nicegui/functions/open.py
+++ nicegui/functions/open.py
@@ -7,5 +7,4 @@ from .navigate import Navigate as navigate
 
 def open(target: Union[Callable[..., Any], str, Element], new_tab: bool = False) -> None:  # pylint: disable=redefined-builtin
     """DEPRECATED: use `ui.navigate.to` instead"""
-    log.warning('"ui.open" is deprecated. Use "ui.navigate.to" instead.')
     navigate.to(target, new_tab)
