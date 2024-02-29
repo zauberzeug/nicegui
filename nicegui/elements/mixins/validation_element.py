38fcfe13fd8e5b297a8cb12aa357f94ad3d84eda
Falko Schindler <falko@zauberzeug.com>
2024-02-29 13:30:45 +0100

    set validation attribute before intitializing the value (fixes #2641)

--- nicegui/elements/mixins/validation_element.py
+++ nicegui/elements/mixins/validation_element.py
@@ -6,9 +6,9 @@ from .value_element import ValueElement
 class ValidationElement(ValueElement):
 
     def __init__(self, validation: Optional[Union[Callable[..., Optional[str]], Dict[str, Callable[..., bool]]]], **kwargs: Any) -> None:
-        super().__init__(**kwargs)
         self.validation = validation if validation is not None else {}
         self._error: Optional[str] = None
+        super().__init__(**kwargs)
 
     @property
     def error(self) -> Optional[str]:
