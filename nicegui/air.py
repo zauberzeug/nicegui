312c59f48396ffb027755d5037d0e555fcb87e7d
Rodja Trappe <rodja@zauberzeug.com>
2024-03-03 05:07:52 +0100

    registering signals does not work in pytests

--- nicegui/air.py
+++ nicegui/air.py
@@ -163,8 +163,11 @@ class Air:
     async def connect(self) -> None:
         """Connect to the NiceGUI On Air server."""
         # ensure that the connection is closed when the process is terminated
-        signal.signal(signal.SIGINT, lambda signum, frame: disconnect())
-        signal.signal(signal.SIGTERM, lambda signum, frame: disconnect())
+        try:
+            signal.signal(signal.SIGINT, lambda signum, frame: disconnect())
+            signal.signal(signal.SIGTERM, lambda signum, frame: disconnect())
+        except Exception:
+            pass
         if self.connecting:
             return
         self.connecting = True
