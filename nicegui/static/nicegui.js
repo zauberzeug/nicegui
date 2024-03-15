2e22c9d10bb591149f91e148e1765ad229a659bd
Falko Schindler <falko@zauberzeug.com>
2024-03-15 09:28:05 +0100

    move app creation into nicegui.js

--- nicegui/static/nicegui.js
+++ nicegui/static/nicegui.js
@@ -2,12 +2,26 @@ const True = true;
 const False = false;
 const None = undefined;
 
+let app = undefined;
+let mounted_app = undefined;
+
 const loaded_libraries = new Set();
 const loaded_components = new Set();
 
+function parseElements(raw_elements) {
+  return JSON.parse(
+    raw_elements
+      .replace(/&#36;/g, "$")
+      .replace(/&#96;/g, "`")
+      .replace(/&gt;/g, ">")
+      .replace(/&lt;/g, "<")
+      .replace(/&amp;/g, "&")
+  );
+}
+
 function getElement(id) {
   const _id = id instanceof HTMLElement ? id.id : id;
-  return window.app.$refs["r" + _id];
+  return mounted_app.$refs["r" + _id];
 }
 
 function runMethod(element_id, method_name, args) {
@@ -211,12 +225,12 @@ function download(src, filename, mediaType, prefix) {
   }
 }
 
-async function loadDependencies(element, prefix, version, app) {
+async function loadDependencies(element, prefix, version) {
   if (element.component) {
     const { name, key, tag } = element.component;
     if (!loaded_components.has(name) && !key.endsWith(".vue")) {
       const component = await import(`${prefix}/_nicegui/${version}/components/${key}`);
-      app = app.component(tag, component.default);
+      app.component(tag, component.default);
       loaded_components.add(name);
     }
   }
@@ -227,4 +241,96 @@ async function loadDependencies(element, prefix, version, app) {
       loaded_libraries.add(name);
     }
   }
+}
+
+function createApp(elements, options) {
+  return (app = Vue.createApp({
+    data() {
+      return {
+        elements,
+      };
+    },
+    render() {
+      return renderRecursively(this.elements, 0);
+    },
+    mounted() {
+      mounted_app = this;
+      window.client_id = options.query.client_id;
+      const url = window.location.protocol === "https:" ? "wss://" : "ws://" + window.location.host;
+      window.path_prefix = options.prefix;
+      window.socket = io(url, {
+        path: `${options.prefix}/_nicegui_ws/socket.io`,
+        query: options.query,
+        extraHeaders: options.extraHeaders,
+        transports: options.transports,
+      });
+      const messageHandlers = {
+        connect: () => {
+          window.socket.emit("handshake", window.client_id, (ok) => {
+            if (!ok) {
+              console.log("reloading because handshake failed for client_id " + window.client_id);
+              window.location.reload();
+            }
+            document.getElementById("popup").style.opacity = 0;
+          });
+        },
+        connect_error: (err) => {
+          if (err.message == "timeout") {
+            console.log("reloading because connection timed out");
+            window.location.reload(); // see https://github.com/zauberzeug/nicegui/issues/198
+          }
+        },
+        try_reconnect: async () => {
+          document.getElementById("popup").style.opacity = 1;
+          await fetch(window.location.href, { headers: { "NiceGUI-Check": "try_reconnect" } });
+          console.log("reloading because reconnect was requested");
+          window.location.reload();
+        },
+        disconnect: () => {
+          document.getElementById("popup").style.opacity = 1;
+        },
+        update: async (msg) => {
+          for (const [id, element] of Object.entries(msg)) {
+            if (element === null) {
+              delete this.elements[id];
+              continue;
+            }
+            if (element.component || element.libraries) {
+              await loadDependencies(element, options.prefix, options.version);
+            }
+            this.elements[id] = element;
+          }
+        },
+        run_javascript: (msg) => runJavascript(msg["code"], msg["request_id"]),
+        open: (msg) => {
+          const url = msg.path.startsWith("/") ? options.prefix + msg.path : msg.path;
+          const target = msg.new_tab ? "_blank" : "_self";
+          window.open(url, target);
+        },
+        download: (msg) => download(msg.src, msg.filename, msg.media_type, options.prefix),
+        notify: (msg) => Quasar.Notify.create(msg),
+      };
+      const socketMessageQueue = [];
+      let isProcessingSocketMessage = false;
+      for (const [event, handler] of Object.entries(messageHandlers)) {
+        window.socket.on(event, async (...args) => {
+          socketMessageQueue.push(() => handler(...args));
+          if (!isProcessingSocketMessage) {
+            while (socketMessageQueue.length > 0) {
+              const handler = socketMessageQueue.shift();
+              isProcessingSocketMessage = true;
+              try {
+                await handler();
+              } catch (e) {
+                console.error(e);
+              }
+              isProcessingSocketMessage = false;
+            }
+          }
+        });
+      }
+    },
+  }).use(Quasar, {
+    config: options.quasarConfig,
+  }));
 }
