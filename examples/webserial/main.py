f6f63ef5c0c9b8a6863a9801723f4270d34a1bb5
Falko Schindler <falko@zauberzeug.com>
2024-03-17 19:27:39 +0100

    more code review

--- examples/webserial/main.py
+++ examples/webserial/main.py
@@ -4,32 +4,37 @@ from pathlib import Path
 from nicegui import ui
 
 
-async def connect() -> None:
-    if not await ui.run_javascript('"serial" in navigator'):
-        ui.notify('WebSerial is not available in this browser.')
-        return
+@ui.page('/')
+def page():
+    async def connect() -> None:
+        if not await ui.run_javascript('"serial" in navigator'):
+            ui.notify('WebSerial is not available in this browser.')
+            return
 
-    if not await ui.run_javascript('connect()', timeout=100):
-        ui.notify('Could not connect to the device.')
-        return
+        if not await ui.run_javascript('connect()', timeout=100):
+            ui.notify('Could not connect to the device.')
+            return
 
-    ui.run_javascript('readLoop()')
-    state['connected'] = True
+        ui.run_javascript('readLoop()')
+        state['connected'] = True
 
+    async def disconnect() -> None:
+        ui.run_javascript('disconnect()', timeout=5)
+        state['connected'] = False
 
-async def disconnect() -> None:
-    await ui.run_javascript('disconnect()', timeout=5)
-    state['connected'] = False
+    state = {
+        'connected': False,
+        'button': False,
+    }
 
-state = {'connected': False}
+    ui.add_body_html(f'<script>{(Path(__file__).parent / "script.js").read_text()}</script>')
 
-ui.add_body_html(f'<script>{(Path(__file__).parent / "script.js").read_text()}</script>')
+    ui.button('Connect', on_click=connect).bind_visibility_from(state, 'connected', value=False)
+    ui.button('Disconnect', on_click=disconnect).bind_visibility_from(state, 'connected')
+    ui.switch('LED', on_change=lambda e: ui.run_javascript(f'send({e.value:d})')).bind_enabled_from(state, 'connected')
+    ui.switch('Button').props('disable').bind_value_from(state, 'button')
 
-ui.button('Connect', on_click=connect).bind_enabled_from(state, 'connected', lambda connected: not connected)
-ui.button('LED On', on_click=lambda: ui.run_javascript('send("1")')).bind_enabled_from(state, 'connected')
-ui.button('LED Off', on_click=lambda: ui.run_javascript('send("0")')).bind_enabled_from(state, 'connected')
-ui.button('Disconnect', on_click=disconnect).bind_enabled_from(state, 'connected')
+    ui.on('read', lambda e: state.update(button=e.args == 'LOW'))
 
-ui.on('read', lambda counts: ui.notify(f'Button pressed {counts} times!'))
 
 ui.run()
