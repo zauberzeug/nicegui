f07e12c3d58675a390f57e40562f4f3bc1268089
Falko Schindler <falko@zauberzeug.com>
2024-03-29 16:19:59 +0100

    add pytest

---
+++ tests/test_clipboard.py
@@ -0,0 +1,20 @@
+from nicegui import ui
+from nicegui.testing import Screen
+
+
+def test_clipboard(screen: Screen):
+    ui.button('Copy to clipboard', on_click=lambda: ui.clipboard.write('Hello, World!'))
+
+    async def read_clipboard():
+        ui.notify('Clipboard: ' + await ui.clipboard.read())
+    ui.button('Read from clipboard', on_click=read_clipboard)
+
+    screen.open('/')
+    screen.selenium.set_permissions('clipboard-read', 'granted')
+    screen.selenium.set_permissions('clipboard-write', 'granted')
+
+    screen.click('Copy to clipboard')
+    assert screen.selenium.execute_script('return navigator.clipboard.readText()') == 'Hello, World!'
+
+    screen.click('Read from clipboard')
+    screen.should_contain('Clipboard: Hello, World!')
