6a9e16e61894561fcba9e04462c8e0d5decff685
Rodja Trappe <rodja@zauberzeug.com>
2024-04-03 05:57:01 +0200

    make libsass optional

--- nicegui/functions/style.py
+++ nicegui/functions/style.py
@@ -1,9 +1,16 @@
 from pathlib import Path
 from typing import Union
 
-import sass
+from .. import optional_features
+
+try:
+    import sass
+    optional_features.register('sass')
+except ImportError:
+    pass
 
 from .. import helpers
+from ..logging import log
 from .html import add_head_html
 
 
@@ -17,5 +24,12 @@ def add_style(content: Union[str, Path], indented: bool = False) -> None:
     """
     if helpers.is_file(content):
         content = Path(content).read_text()
-    css = sass.compile(string=str(content).strip(), indented=indented)
+    if optional_features.has('sass'):
+        css = sass.compile(string=str(content).strip(), indented=indented)
+    else:
+        css = content
+        sass_features = ["$", "@mixin", "{&"]
+        if any(feature in css for feature in sass_features):
+            log.warning('SASS features detected in ui.add_style. Please run "pip install libsass".')
+
     add_head_html(f'<style>{css}</style>')
