4d5bdd115fe40cf23cbf4e33032aa94011cd0927
Falko Schindler <falko@zauberzeug.com>
2024-04-04 11:04:27 +0200

    add auto-close parameter

--- nicegui/elements/button_dropdown.py
+++ nicegui/elements/button_dropdown.py
@@ -13,6 +13,7 @@ class DropdownButton(Button, ValueElement):
                  on_click: Optional[Callable[..., Any]] = None,
                  color: Optional[str] = 'primary',
                  icon: Optional[str] = None,
+                 auto_close: Optional[bool] = False,
                  ) -> None:
         """Dropdown Button
 
@@ -29,10 +30,14 @@ class DropdownButton(Button, ValueElement):
         :param on_click: callback which is invoked when button is pressed
         :param color: the color of the button (either a Quasar, Tailwind, or CSS color or `None`, default: 'primary')
         :param icon: the name of an icon to be displayed on the button (default: `None`)
+        :param auto_close: whether the dropdown should close automatically when an item is clicked (default: `False`)
         """
         super().__init__(text=text, on_click=on_click, color=color, icon=icon,
                          value=value, on_value_change=on_value_change)
         self.tag = 'q-btn-dropdown'
+
+        if auto_close:
+            self._props['auto-close'] = True
 
     def open(self) -> None:
         """Open the dropdown."""
