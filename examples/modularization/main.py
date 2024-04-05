8ecb6f5ff45773229bcc571b76813a08137393ac
Michael Ikemann <michael.ikemann@gmail.com>
2024-04-05 13:46:43 +0200

    Added support for URL route parameters and query parameters

--- examples/modularization/main.py
+++ examples/modularization/main.py
@@ -5,6 +5,7 @@ import home_page
 import theme
 
 from nicegui import app, ui
+from nicegui.single_page import SinglePageRouter
 
 
 # here we use our custom page decorator directly and just put the content creation into a separate function
@@ -20,4 +21,5 @@ example_pages.create()
 # we can also use the APIRouter as described in https://nicegui.io/documentation/page#modularize_with_apirouter
 app.include_router(example_c.router)
 
+spr = SinglePageRouter("/").setup_page_routes()  # TODO Experimental, for performance comparison
 ui.run(title='Modularization Example')
