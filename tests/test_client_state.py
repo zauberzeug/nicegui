9dd4227ab630ea647cc70b827c5b7071ea8ddb0d
Michael Ikemann <michael.ikemann@gmail.com>
2024-04-06 14:06:01 +0200

    Exchanged quotes Added app.storage.client clear test

--- tests/test_client_state.py
+++ tests/test_client_state.py
@@ -3,15 +3,21 @@ from nicegui.testing import Screen
 
 
 def test_session_state(screen: Screen):
-    app.storage.client["counter"] = 123
+    app.storage.client['counter'] = 123
 
     def increment():
-        app.storage.client["counter"] = app.storage.client["counter"] + 1
+        app.storage.client['counter'] = app.storage.client['counter'] + 1
 
-    ui.button("Increment").on_click(increment)
-    ui.label().bind_text(app.storage.client, "counter")
+    ui.button('Increment').on_click(increment)
+    ui.label().bind_text(app.storage.client, 'counter')
 
     screen.open('/')
     screen.should_contain('123')
     screen.click('Increment')
     screen.wait_for('124')
+
+
+def test_clear(screen: Screen):
+    app.storage.client['counter'] = 123
+    app.storage.client.clear()
+    assert 'counter' not in app.storage.client
