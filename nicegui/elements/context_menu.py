4b8c670bf372553c0d98b95589158f1e9cbfc217
Paula Kammler <paula@zauberzeug.com>
2024-04-08 14:38:36 +0200

    add auto_close parameter to context menu

--- nicegui/elements/context_menu.py
+++ nicegui/elements/context_menu.py
@@ -3,16 +3,20 @@ from ..element import Element
 
 class ContextMenu(Element):
 
-    def __init__(self) -> None:
+    def __init__(self, *, auto_close: bool = False) -> None:
         """Context Menu
 
         Creates a context menu based on Quasar's `QMenu <https://quasar.dev/vue-components/menu>`_ component.
         The context menu should be placed inside the element where it should be shown.
         It is automatically opened when the user right-clicks on the element and appears at the mouse position.
+
+        :param auto_close: whether the context menu should be closed after a click on one of its items (default: `False`)
         """
         super().__init__('q-menu')
         self._props['context-menu'] = True
         self._props['touch-position'] = True
+        if auto_close:
+            self._props['auto-close'] = True
 
     def open(self) -> None:
         """Open the context menu."""
