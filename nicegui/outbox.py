ccca3c5fcea10e421dbc38ed30d1c05b8cb2289e
Aaron Fuller <159853563+afullerx@users.noreply.github.com>
2024-04-10 19:55:29 -0400

    clean up timeout argument

--- nicegui/outbox.py
+++ nicegui/outbox.py
@@ -60,7 +60,7 @@ class Outbox:
                 await asyncio.sleep(0.005)
                 
                 if not self.client.has_socket_connection:
-                    await self.client.connected(timeout=99999999)
+                    await self.client.connected(timeout=2**30)
                 
                 self._enqueue_event.clear()
                 coros = []
