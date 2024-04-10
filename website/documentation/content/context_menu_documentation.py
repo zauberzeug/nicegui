e988b412ec330fef8f84bce81d811c01c82c21a9
Paula Kammler <paula@zauberzeug.com>
2024-04-10 11:48:57 +0200

    update tests and documentation to use the menu_item again

--- website/documentation/content/context_menu_documentation.py
+++ website/documentation/content/context_menu_documentation.py
@@ -6,11 +6,12 @@ from . import doc
 @doc.demo(ui.context_menu)
 def main_demo() -> None:
     with ui.image('https://picsum.photos/id/377/640/360'):
-        with ui.context_menu(auto_close=True):
-            ui.item('Flip horizontally').props('clickable')
-            ui.item('Flip vertically').props('clickable')
-            ui.separator()
-            ui.item('Reset').props('clickable')
+        with ui.context_menu():
+            with ui.list():
+                ui.menu_item('Flip horizontally').props('clickable')
+                ui.menu_item('Flip vertically').props('clickable')
+                ui.separator()
+                ui.menu_item('Reset', auto_close=False).props('clickable')
 
 
 doc.reference(ui.context_menu)
--- website/documentation/content/menu_documentation.py
+++ website/documentation/content/menu_documentation.py
@@ -9,11 +9,13 @@ def main_demo() -> None:
         result = ui.label().classes('mr-auto')
         with ui.button(icon='menu'):
             with ui.menu() as menu:
-                ui.item('Menu item 1', on_click=lambda: result.set_text('Selected item 1'))
-                ui.item('Menu item 2', on_click=lambda: result.set_text('Selected item 2'))
-                ui.item('Menu item 3', on_click=lambda: result.set_text('Selected item 3'))
-                ui.separator()
-                ui.item('Close', on_click=menu.close)
+                with ui.list():
+                    ui.menu_item('Menu item 1', on_click=lambda: result.set_text('Selected item 1'))
+                    ui.menu_item('Menu item 2', on_click=lambda: result.set_text('Selected item 2'))
+                    ui.menu_item('Menu item 3 (keep open)',
+                                 on_click=lambda: result.set_text('Selected item 3'), auto_close=False)
+                    ui.separator()
+                    ui.menu_item('Close', on_click=menu.close)
 
 
 doc.reference(ui.menu)
