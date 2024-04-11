6e61721a3a440217a401d32c380896c3de09f600
Paula Kammler <paula@zauberzeug.com>
2024-04-11 11:11:56 +0200

    refactor list and menu items (#2855)
    * move item, item section and item label to own file and add text to item
    
    * add auto_close to menu and deprecate menu item
    
    * add auto_close parameter to context menu
    
    * replace menu_item with item in tests
    
    * use new item in documentation
    
    * use new item in header
    
    * code review
    
    * don't deprecate menu_item, but inherit from item and rework auto_close
    
    * warn if the auto_close parameter of the menu and its items differs
    
    * update tests and documentation to use the menu_item again
    
    * remove auto_close parameter from menus
    
    * simplify tests and documentation
    
    * more cleanup
    
    * remove ui.list where it's not needed
    
    ---------
    
    Co-authored-by: Falko Schindler <falko@zauberzeug.com>

--- tests/test_context_menu.py
+++ tests/test_context_menu.py
@@ -5,12 +5,12 @@ from nicegui.testing import Screen
 def test_context_menu(screen: Screen):
     with ui.label('Right-click me'):
         with ui.context_menu():
-            ui.menu_item('Item 1', on_click=lambda: ui.label('Item 1 clicked'))
+            ui.menu_item('Item 1', on_click=lambda: ui.notify('You clicked'))
             ui.menu_item('Item 2')
 
     screen.open('/')
     screen.context_click('Right-click me')
     screen.click('Item 1')
-    screen.should_contain('Item 1 clicked')
+    screen.should_contain('You clicked')
     screen.wait(0.5)
-    screen.should_not_contain('Item 2')
+    screen.should_not_contain('Item 1')
