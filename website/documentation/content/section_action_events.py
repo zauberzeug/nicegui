fc5bd3cd2933aeff65eef131a072b2efda65acf0
Rodja Trappe <rodja@zauberzeug.com>
2024-04-16 06:41:01 +0200

    note about run.cpu_bound problem with classes

--- website/documentation/content/section_action_events.py
+++ website/documentation/content/section_action_events.py
@@ -71,6 +71,9 @@ doc.intro(generic_events_documentation)
     NiceGUI provides a `cpu_bound` function for running CPU-bound tasks in a separate process.
     This is useful for long-running computations that would otherwise block the event loop and make the UI unresponsive.
     The function returns a future that can be awaited.
+    NOTE: The function needs to transfer the whole state of the passed function to the process (which is done with pickle).
+    It is encouraged to create static methods (or free functions) which get all the data as simple parameters (eg. no class/ui logic)
+    and return the result (instead of writing it in class properties or global variables).
 ''')
 def cpu_bound_demo():
     import time
