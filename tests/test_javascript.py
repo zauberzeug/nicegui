4a3724e0c6f3cdd5c418250e4b10fca3c0234850
Falko Schindler <falko@zauberzeug.com>
2024-04-29 14:16:46 +0200

    fix other failing tests (most of them)

--- tests/test_javascript.py
+++ tests/test_javascript.py
@@ -45,11 +45,13 @@ def test_run_javascript_before_client_connected(screen: Screen):
 
 
 def test_response_from_javascript(screen: Screen):
-    async def compute() -> None:
-        response = await ui.run_javascript('1 + 41')
-        ui.label(response)
+    @ui.page('/')
+    def page():
+        async def compute() -> None:
+            response = await ui.run_javascript('1 + 41')
+            ui.label(response)
 
-    ui.button('compute', on_click=compute)
+        ui.button('compute', on_click=compute)
 
     screen.open('/')
     screen.click('compute')
@@ -57,25 +59,33 @@ def test_response_from_javascript(screen: Screen):
 
 
 def test_async_javascript(screen: Screen):
-    async def run():
-        result = await ui.run_javascript('await new Promise(r => setTimeout(r, 100)); return 42')
-        ui.label(result)
-    ui.button('run', on_click=run)
+    @ui.page('/')
+    def page():
+        async def run():
+            result = await ui.run_javascript('await new Promise(r => setTimeout(r, 100)); return 42')
+            ui.label(result)
+
+        ui.button('run', on_click=run)
+
     screen.open('/')
     screen.click('run')
     screen.should_contain('42')
 
 
 def test_simultaneous_async_javascript(screen: Screen):
-    async def runA():
-        result = await ui.run_javascript('await new Promise(r => setTimeout(r, 500)); return 1')
-        ui.label(f'A: {result}')
+    @ui.page('/')
+    def page():
+        async def runA():
+            result = await ui.run_javascript('await new Promise(r => setTimeout(r, 500)); return 1')
+            ui.label(f'A: {result}')
 
-    async def runB():
-        result = await ui.run_javascript('await new Promise(r => setTimeout(r, 250)); return 2')
-        ui.label(f'B: {result}')
-    ui.button('runA', on_click=runA)
-    ui.button('runB', on_click=runB)
+        async def runB():
+            result = await ui.run_javascript('await new Promise(r => setTimeout(r, 250)); return 2')
+            ui.label(f'B: {result}')
+
+        ui.button('runA', on_click=runA)
+        ui.button('runB', on_click=runB)
+
     screen.open('/')
     screen.click('runA')
     screen.click('runB')
