4050e6a034d33c087378b8403a1d93c2152381a6
Falko Schindler <falko@zauberzeug.com>
2024-04-29 15:25:46 +0200

    simplify demos a bit

--- website/documentation/content/chip_documentation.py
+++ website/documentation/content/chip_documentation.py
@@ -7,9 +7,8 @@ from . import doc
 def main_demo() -> None:
     with ui.row().classes('gap-1'):
         ui.chip('Click me', icon='ads_click', on_click=lambda: ui.notify('Clicked'))
-        ui.chip('Selectable', selectable=True, icon='bookmark',
-                color='orange').on_selection_change(lambda e: ui.notify(e.sender.selected))
-        ui.chip('Removable', removable=True, icon='label', color='indigo')
+        ui.chip('Selectable', selectable=True, icon='bookmark', color='orange')
+        ui.chip('Removable', removable=True, icon='label', color='indigo-3')
         ui.chip('Styled', icon='star', color='green').props('outline square')
         ui.chip('Disabled', icon='block', color='red').set_enabled(False)
 
@@ -17,32 +16,24 @@ def main_demo() -> None:
 @doc.demo('Dynamic chip elements as labels/tags', '''
     This demo shows how to implement a dynamic list of chips as labels or tags.
     You can add new chips by typing a label and pressing Enter or pressing the plus button.
-    Removed chips still exist, but their value is set to False.
+    Removed chips still exist, but their value is set to `False`.
 ''')
 def labels():
-    def on_chip_change():
-        ui.notify('Labels: ' + ', '.join(chip.text for chip in label_chips.default_slot.children if chip.value))
-
     def add_chip():
-        if not (val := label.value.strip()):
-            return
-        with label_chips:
-            ui.chip(val, icon='label', color='gray', removable=True, on_value_change=on_chip_change)
-            on_chip_change()
-        label.value = ''
+        with chips:
+            ui.chip(label_input.value, icon='label', color='silver', removable=True)
+        label_input.value = ''
 
-    with ui.input('Add label').props('filled') as label:
-        with label.add_slot('prepend'):
-            ui.icon('label', color='primary')
-        with label.add_slot('append'):
-            ui.button(icon='add', on_click=add_chip).props('round dense flat')
+    label_input = ui.input('Add label').on('keydown.enter', add_chip)
+    with label_input.add_slot('append'):
+        ui.button(icon='add', on_click=add_chip).props('round dense flat')
 
-    with ui.row().classes('gap-0') as label_chips:
-        ui.chip('Label 1', icon='label', color='gray', removable=True, on_value_change=on_chip_change)
+    with ui.row().classes('gap-0') as chips:
+        ui.chip('Label 1', icon='label', color='silver', removable=True)
 
-    label.on('keydown.enter', add_chip)
-
-    ui.button('Restore', on_click=lambda: [chip.set_value(True) for chip in label_chips.default_slot.children])
+    ui.button('Restore removed chips', icon='unarchive',
+              on_click=lambda: [chip.set_value(True) for chip in chips]) \
+        .props('flat')
 
 
 doc.reference(ui.chip)
