6792c22a73724fd2d5c1296962387a773b6c4a89
Falko Schindler <falko@zauberzeug.com>
2024-04-29 15:05:12 +0200

    improve pytests

--- tests/test_chip.py
+++ tests/test_chip.py
@@ -2,30 +2,26 @@ from nicegui import ui
 from nicegui.testing import Screen
 
 
-def test_hello_world(screen: Screen):
-    text = 'Hello, world'
-    chip = ui.chip(text, removable=True)
+def test_removable_chip(screen: Screen):
+    chip = ui.chip('Chip', removable=True)
 
     screen.open('/')
-    screen.should_contain(text)
+    screen.should_contain('Chip')
 
     chip.set_value(False)
     screen.wait(0.5)
-    screen.should_not_contain(text)
+    screen.should_not_contain('Chip')
 
 
-def test_selectable(screen: Screen):
-    text = 'Selectable'
-    chip = ui.chip(text, selectable=True)
-    assert not chip.selected
+def test_selectable_chip(screen: Screen):
+    chip = ui.chip('Chip', selectable=True)
+    ui.label().bind_text_from(chip, 'selected', lambda s: f'Selected: {s}')
 
     screen.open('/')
-    screen.should_contain(text)
+    screen.should_contain('Selected: False')
 
-    screen.click(text)
-    screen.should_contain(text)
-    assert chip.selected
+    screen.click('Chip')
+    screen.should_contain('Selected: True')
 
-    screen.click(text)
-    screen.should_contain(text)
-    assert not chip.selected
+    screen.click('Chip')
+    screen.should_contain('Selected: False')
