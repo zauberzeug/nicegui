539887a2cd99971fbfe85d3256159a84d228d737
CrystalWindSnake <568166495@qq.com>
2024-06-01 02:59:17 +0800

    docs

--- nicegui/elements/teleport.py
+++ nicegui/elements/teleport.py
@@ -5,14 +5,16 @@ class Teleport(Element, component="teleport.js"):
     def __init__(self, to: str) -> None:
         """Teleport
 
-        An element enables us to transmit the content from within a component to any location on the page.
+        An element that allows us to transmit the content from within a component to any location on the page.
 
-        :param to: the selector of the element to which we want to teleport the content
+        :param to: the css selector of the element to which we want to teleport the content
         """
         super().__init__()
         self._props["to"] = to
 
     def force_update(self):
-        """Force update.This method can be used to force the update of the content in the teleported element.
+        """This method forces the internal content to be retransmitted to the specified location.
+
+        It is usually called after the target container is rebuilt.
         """
         self.run_method("forceUpdate")
