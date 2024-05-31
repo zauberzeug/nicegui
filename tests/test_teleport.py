d317865ffa5eaf4a3b5f1c9b1d5c1024dc9fc90e
CrystalWindSnake <568166495@qq.com>
2024-06-01 01:56:19 +0800

    testing

--- tests/test_teleport.py
+++ tests/test_teleport.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from nicegui import ui
 from nicegui.testing import Screen
 
@@ -16,6 +18,31 @@ def test_should_work(screen: Screen):
     assert screen.find_by_css('.src > div').text == 'Hello'
 
 
-def test_update(screen: Screen):
-    pass
+def test_force_update(screen: Screen):
+    tp:Optional[ui.teleport] = None
+
+    box = ui.row().classes('src')
+
+    def create_teleport() :
+        nonlocal tp
+        with ui.teleport('.src') as tp:
+            ui.label('Hello')
+
+    ui.button('create',on_click=create_teleport)
+
+
+    def rebuild_box():
+        box.delete()
+        ui.row().classes('src')
+        tp.force_update() # type: ignore
+
+    ui.button('rebuild box',on_click=rebuild_box)
+
+
+    screen.open('/')
+    screen.click('create')
+    screen.should_contain('Hello')
+    screen.click('rebuild box')
+    assert screen.find_by_css('.src > div').text == 'Hello'
+
 
