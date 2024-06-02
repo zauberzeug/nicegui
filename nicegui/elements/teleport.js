d8f95db40062136dc7a63d136da1c4b15bd5fca3
CrystalWindSnake <568166495@qq.com>
2024-06-03 00:01:05 +0800

    fix disappear on auto-index page

--- nicegui/elements/teleport.js
+++ nicegui/elements/teleport.js
@@ -1,11 +1,15 @@
 export default {
-  template: `<Teleport :to="to" :key="key"><slot></slot></Teleport>`,
+  template: `<Teleport v-if="isLoaded" :to="to" :key="key"><slot></slot></Teleport>`,
   props: {
     to: String,
+  },
+  mounted() {
+    this.isLoaded = true;
   },
   data() {
     return {
       key: 0,
+      isLoaded: false,
     };
   },
   methods: {
