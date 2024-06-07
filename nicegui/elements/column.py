c69f2a76d4bfb1946bf75d96a48cb38c4c9dcc1f
Falko Schindler <falko@zauberzeug.com>
2024-06-07 09:53:15 +0200

    Introduce `align_items` parameter for `ui.row`, `ui.column` and `ui.card` (#3175)
    * tiny fix
    
    * introduce `align_items` parameter for row, column and card

--- nicegui/elements/column.py
+++ nicegui/elements/column.py
@@ -1,17 +1,25 @@
+from typing import Literal, Optional
+
 from ..element import Element
 
 
 class Column(Element):
 
-    def __init__(self, *, wrap: bool = False) -> None:
+    def __init__(self, *,
+                 wrap: bool = False,
+                 align_items: Optional[Literal['start', 'end', 'center', 'baseline', 'stretch']] = None,
+                 ) -> None:
         """Column Element
 
         Provides a container which arranges its child in a column.
 
         :param wrap: whether to wrap the content (default: `False`)
+        :param align_items: alignment of the items in the column (default: `None`)
         """
         super().__init__('div')
         self._classes.append('nicegui-column')
+        if align_items:
+            self._classes.append(f'items-{align_items}')
 
         if wrap:
             self._style['flex-wrap'] = 'wrap'
--- nicegui/elements/row.py
+++ nicegui/elements/row.py
@@ -1,17 +1,26 @@
+from typing import Literal, Optional
+
 from ..element import Element
 
 
 class Row(Element):
 
-    def __init__(self, *, wrap: bool = True) -> None:
+    def __init__(self, *,
+                 wrap: bool = True,
+                 align_items: Optional[Literal['start', 'end', 'center', 'baseline', 'stretch']] = None,
+                 ) -> None:
         """Row Element
 
         Provides a container which arranges its child in a row.
 
         :param wrap: whether to wrap the content (default: `True`)
+        :param align_items: alignment of the items in the row (default: `None`)
         """
         super().__init__('div')
-        self._classes.append('nicegui-row row')  # NOTE: 'row' class for compatibility with Quasar's col-* classes
+        self._classes.append('nicegui-row')
+        self._classes.append('row')  # NOTE: for compatibility with Quasar's col-* classes
+        if align_items:
+            self._classes.append(f'items-{align_items}')
 
         if not wrap:
             self._style['flex-wrap'] = 'nowrap'
