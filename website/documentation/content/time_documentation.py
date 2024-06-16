aef58d3c4ed554d18603c2915683940bf0c1ada5
Falko Schindler <falko@zauberzeug.com>
2024-06-16 15:15:30 +0200

    use "Close" button instead of auto-close prop

--- website/documentation/content/time_documentation.py
+++ website/documentation/content/time_documentation.py
@@ -13,9 +13,9 @@ def main_demo() -> None:
     This demo shows how to implement a time picker with an input element.
     We place an icon in the input element's append slot.
     When the icon is clicked, we open a menu with a time picker.
-    [QMenu](https://quasar.dev/vue-components/menu)'s "no-parent-event" prop can be used
-    to prevent a click within the text input itself to open the menu.
-    As the menu doesn't come with a "Close" button by default, we can add one for convenience.
+    [QMenu](https://quasar.dev/vue-components/menu)'s "no-parent-event" prop is used
+    to prevent opening the menu when clicking into the input field.
+    As the menu doesn't come with a "Close" button by default, we add one for convenience.
 
     The time is bound to the input element's value.
     So both the input element and the time picker will stay in sync whenever the time is changed.
@@ -23,9 +23,9 @@ def main_demo() -> None:
 def time_picker_demo():
     with ui.input('Time') as time:
         with ui.menu().props('no-parent-event') as menu:
-            with ui.time().props('format24h').bind_value(time):
+            with ui.time().bind_value(time):
                 with ui.row().classes('justify-end'):
-                    ui.button('Close', on_click=menu.close)
+                    ui.button('Close', on_click=menu.close).props('flat')
         with time.add_slot('append'):
             ui.icon('access_time').on('click', menu.open).classes('cursor-pointer')
 
