f08f2eca948c7d73fcd6ef06b7bd67249b571c05
frankvp11 <frankvanpaassen3@gmail.com>
2024-07-03 05:46:07 -0400

    added example

--- website/documentation/content/teleport_documentation.py
+++ website/documentation/content/teleport_documentation.py
@@ -29,4 +29,27 @@ def arbitrary_content():
     ui.label().bind_text_from(radio, 'value')
 
 
+@doc.demo('Injecting a graph into a table cell', '''
+          You can inject a graph into a table cell by using the right CSS selector.
+''')
+def graph_in_table():
+    columns = [
+        {'name': 'name', 'label': 'Name', 'field': 'name', 'required': True, 'align': 'left'},
+        {'name': 'age', 'label': 'Age', 'field': 'age', 'sortable': True},
+    ]
+    rows = [
+        {'name': 'Alice', 'age': 18},
+        {'name': 'Bob', 'age': 21},
+        {'name': 'Carol'},
+    ]
+    table = ui.table(columns=columns, rows=rows, row_key='name')
+    with ui.teleport(f'#c{table.id} > .q-table__middle > .q-table > tbody > tr:nth-child(3) > td:nth-child(2)'):
+        ui.echart({
+            'xAxis': {'type': 'category'},
+            'yAxis': {'type': 'value'},
+            'series': [{'type': 'line', 'data': [20, 10, 30, 50, 40, 30]}],
+        }, on_point_click=ui.notify)
+
+ui.run()
+
 doc.reference(ui.teleport)
