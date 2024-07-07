b46ca9b2c5490ef472f1d842880442779dbb4f3f
Rodja Trappe <rodja@zauberzeug.com>
2024-07-07 07:56:13 +0200

    improve check if we are running as pytest

--- nicegui/core.py
+++ nicegui/core.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import asyncio
-import sys
+import os
 from typing import TYPE_CHECKING, Optional
 
 from socketio import AsyncServer
@@ -14,4 +14,4 @@ app: App
 sio: AsyncServer
 loop: Optional[asyncio.AbstractEventLoop] = None
 air: Optional[Air] = None
-is_pytest: bool = 'pytest' in sys.modules
+is_pytest: bool = 'PYTEST_CURRENT_TEST' in os.environ
