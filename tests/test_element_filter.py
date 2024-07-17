5e82e4679263cd8c121c667614e417bae3f8ede3
Falko Schindler <falko@zauberzeug.com>
2024-07-17 17:27:01 +0200

    update element filter tests to use User instead of Screen

--- tests/test_element_filter.py
+++ tests/test_element_filter.py
@@ -1,32 +1,35 @@
+from typing import List
+
+import pytest
+
 from nicegui import ElementFilter, ui
 from nicegui.elements.mixins.text_element import TextElement
-from nicegui.testing import Screen
+from nicegui.testing import User
+
+pytestmark = pytest.mark.usefixtures('user')
 
 
-def test_get_all(screen: Screen):
+def test_find_all():
     ui.button('button A')
     ui.label('label A')
     with ui.row():
         ui.button('button B')
         ui.label('label B')
 
-    elements = list(ElementFilter())
+    elements: List[ui.element] = list(ElementFilter())
 
-    screen.open('/')
     assert len(elements) == 8
     assert elements[0].tag == 'q-page-container'
     assert elements[1].tag == 'q-page'
     assert elements[2]._classes == ['nicegui-content']  # pylint: disable=protected-access
-    assert elements[3].text == 'button A'
-    assert elements[4].text == 'label A'
+    assert elements[3].text == 'button A'  # type: ignore
+    assert elements[4].text == 'label A'  # type: ignore
     assert elements[5].__class__ == ui.row
-    assert elements[6].text == 'button B'
-    assert elements[7].text == 'label B'
+    assert elements[6].text == 'button B'  # type: ignore
+    assert elements[7].text == 'label B'  # type: ignore
 
 
-def test_get_by_text_element(screen: Screen):
-    from nicegui.elements.mixins.text_element import TextElement
-
+def test_find_by_text_element():
     ui.button('button A')
     ui.label('label A')
     ui.icon('home')
@@ -35,11 +38,10 @@ def test_get_by_text_element(screen: Screen):
 
     result = [element.text for element in ElementFilter(kind=TextElement)]
 
-    screen.open('/')
     assert result == ['button A', 'label A', 'button B', 'label B']
 
 
-def test_get_by_type(screen: Screen):
+def test_find_by_type():
     ui.button('button A')
     ui.label('label A')
     ui.button('button B')
@@ -47,11 +49,10 @@ def test_get_by_type(screen: Screen):
 
     result = [element.text for element in ElementFilter(kind=ui.button)]
 
-    screen.open('/')
     assert result == ['button A', 'button B']
 
 
-def test_get_by_containing_text(screen: Screen):
+def test_find_by_containing_text():
     ui.button('button A')
     ui.label('label A')
     ui.button('button B')
@@ -59,11 +60,10 @@ def test_get_by_containing_text(screen: Screen):
 
     result = [element.text for element in ElementFilter(content='A')]
 
-    screen.open('/')
     assert result == ['button A', 'label A']
 
 
-def test_get_by_containing_texts(screen: Screen):
+def test_find_by_containing_texts():
     ui.button('button A')
     ui.label('label A')
     ui.button('button B')
@@ -71,54 +71,19 @@ def test_get_by_containing_texts(screen: Screen):
 
     result = [element.text for element in ElementFilter(content=['A', 'tt'])]
 
-    screen.open('/')
     assert result == ['button A']
 
 
-def test_setting_classes(screen: Screen):
-    ui.label('label A')
-    ui.label('label B')
-
-    ElementFilter(kind=ui.label).classes('text-2xl')
-
-    screen.open('/')
-    assert screen.find('label A').get_attribute('class') == 'text-2xl'
-    assert screen.find('label B').get_attribute('class') == 'text-2xl'
-
-
-def test_setting_style(screen: Screen):
-    ui.label('label A')
-    ui.label('label B')
-
-    ElementFilter(kind=ui.label).style('color: red')
-
-    screen.open('/')
-    assert screen.find('label A').get_attribute('style') == 'color: red;'
-    assert screen.find('label B').get_attribute('style') == 'color: red;'
-
-
-def test_setting_props(screen: Screen):
-    ui.button('A')
-    ui.button('B')
-
-    ElementFilter(kind=ui.button).props('flat')
-
-    screen.open('/')
-    for e in screen.find_all_by_tag('button'):
-        assert 'q-btn--flat' in e.get_attribute('class')
-
-
-def test_get_by_marker(screen: Screen):
+def test_find_by_marker():
     ui.button('button A')
     ui.button('button B').mark('important')
 
     result = [element.text for element in ElementFilter(marker='important')]
 
-    screen.open('/')
     assert result == ['button B']
 
 
-def test_get_by_specific_marker(screen: Screen):
+def test_find_by_specific_marker():
     ui.button('button A').mark('test')
     ui.button('button B').mark('important ', 'test')
     ui.button('button C').mark(' important test')
@@ -126,23 +91,22 @@ def test_get_by_specific_marker(screen: Screen):
     test = [element.text for element in ElementFilter(marker='test')]
     important = [element.text for element in ElementFilter(marker='important')]
 
-    screen.open('/')
     assert test == ['button A', 'button B', 'button C']
     assert important == ['button B', 'button C']
 
 
-def test_get_by_multiple_markers(screen: Screen):
+def test_find_by_multiple_markers():
     ui.button('button A').mark('test')
     ui.button('button B').mark('important ', 'test')
     ui.button('button C').mark(' important test')
 
     search = ElementFilter(kind=ui.button, marker='test important')
     result = [element.text for element in search]
-    screen.open('/')
+
     assert result == ['button B', 'button C']
 
 
-def test_get_within_type(screen: Screen):
+def test_find_within_type():
     ui.button('button A')
     ui.label('label A')
     with ui.row():
@@ -151,11 +115,10 @@ def test_get_within_type(screen: Screen):
 
     result = [element.text for element in ElementFilter(kind=ui.button).within(kind=ui.row)]
 
-    screen.open('/')
     assert result == ['button B']
 
 
-def test_get_within_marker(screen: Screen):
+def test_find_within_marker():
     ui.button('button A')
     ui.label('label A')
     with ui.row().mark('horizontal'):
@@ -164,11 +127,10 @@ def test_get_within_marker(screen: Screen):
 
     result = [element.text for element in ElementFilter().within(marker='horizontal')]
 
-    screen.open('/')
     assert result == ['button B', 'label B']
 
 
-def test_get_within_element(screen: Screen):
+def test_find_within_element():
     ui.button('button A')
     ui.label('label A')
     with ui.row() as r:
@@ -177,11 +139,10 @@ def test_get_within_element(screen: Screen):
 
     result = [element.text for element in ElementFilter().within(instance=r)]
 
-    screen.open('/')
     assert result == ['button B', 'label B']
 
 
-def test_get_within_elements(screen: Screen):
+def test_find_within_elements():
     with ui.row() as row1:
         ui.button('button A')
         ui.label('label A')
@@ -191,11 +152,10 @@ def test_get_within_elements(screen: Screen):
 
     result = [element.text for element in ElementFilter().within(instance=[row1, row2])]
 
-    screen.open('/')
     assert result == ['button A', 'label A', 'button B', 'label B']
 
 
-def test_get_with_excluding_type(screen: Screen):
+def test_find_with_excluding_type():
     ui.button('button A')
     ui.label('label A')
     ui.button('button B')
@@ -203,11 +163,10 @@ def test_get_with_excluding_type(screen: Screen):
 
     result = [element.text for element in ElementFilter(content='A').exclude(kind=ui.label)]
 
-    screen.open('/')
     assert result == ['button A']
 
 
-def test_get_with_excluding_marker(screen: Screen):
+def test_find_with_excluding_marker():
     ui.button('button A').mark('normal')
     ui.label('label A').mark('important')
     ui.button('button B')
@@ -215,11 +174,10 @@ def test_get_with_excluding_marker(screen: Screen):
 
     result = [element.text for element in ElementFilter(kind=TextElement).exclude(marker='normal')]
 
-    screen.open('/')
     assert result == ['label A', 'button B']
 
 
-def test_get_with_excluding_text(screen: Screen):
+def test_find_with_excluding_text():
     ui.button('button A')
     ui.label('label A')
     ui.button('button B')
@@ -227,11 +185,10 @@ def test_get_with_excluding_text(screen: Screen):
 
     result = [element.text for element in ElementFilter(kind=ui.button).exclude(content='A')]
 
-    screen.open('/')
     assert result == ['button B']
 
 
-def test_get_not_within_type(screen: Screen):
+def test_find_not_within_type():
     ui.button('button A')
     ui.label('label A')
     with ui.row():
@@ -240,11 +197,10 @@ def test_get_not_within_type(screen: Screen):
 
     result = [element.text for element in ElementFilter(kind=ui.button).not_within(kind=ui.row)]
 
-    screen.open('/')
     assert result == ['button A']
 
 
-def test_get_not_within_marker(screen: Screen):
+def test_find_not_within_marker():
     ui.button('button A')
     ui.label('label A')
     with ui.row().mark('horizontal'):
@@ -253,11 +209,10 @@ def test_get_not_within_marker(screen: Screen):
 
     result = [element.text for element in ElementFilter(kind=ui.button).not_within(marker='horizontal')]
 
-    screen.open('/')
     assert result == ['button A']
 
 
-def test_get_not_within_element(screen: Screen):
+def test_find_not_within_element():
     ui.button('button A')
     ui.label('label A')
     with ui.row() as r:
@@ -266,11 +221,10 @@ def test_get_not_within_element(screen: Screen):
 
     result = [element.text for element in ElementFilter(kind=ui.button).not_within(instance=r)]
 
-    screen.open('/')
     assert result == ['button A']
 
 
-def test_get_in_local_scope(screen: Screen):
+def test_find_in_local_scope():
     ui.button('button A')
     ui.label('label A')
     with ui.row():
@@ -278,5 +232,37 @@ def test_get_in_local_scope(screen: Screen):
         ui.label('label B')
         result = [element.text for element in ElementFilter(local_scope=True)]
 
-    screen.open('/')
     assert result == ['button B', 'label B']
+
+
+async def test_setting_classes(user: User):
+    ui.label('label A')
+    ui.label('label B')
+
+    ElementFilter(kind=ui.label).classes('text-2xl')
+
+    await user.open('/')
+    for label in user.find('label').elements:
+        assert label._classes == ['text-2xl']  # pylint: disable=protected-access
+
+
+async def test_setting_style(user: User):
+    ui.label('label A')
+    ui.label('label B')
+
+    ElementFilter(kind=ui.label).style('color: red')
+
+    await user.open('/')
+    for label in user.find('label').elements:
+        assert label._style['color'] == 'red'  # pylint: disable=protected-access
+
+
+async def test_setting_props(user: User):
+    ui.button('button A')
+    ui.button('button B')
+
+    ElementFilter(kind=ui.button).props('flat')
+
+    await user.open('/')
+    for button in user.find('button').elements:
+        assert button._props['flat']  # pylint: disable=protected-access
