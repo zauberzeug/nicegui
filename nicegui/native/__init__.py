ee6728d5043a93083f24cd8b183959abed6ea2ac
Jacob Danell <jacob@emberlight.se>
2024-07-21 13:40:55 +0200

    Add drop_queue to native's __init__.py

--- nicegui/native/__init__.py
+++ nicegui/native/__init__.py
@@ -1,9 +1,10 @@
-from .native import WindowProxy, method_queue, response_queue
+from .native import WindowProxy, drop_queue, method_queue, response_queue
 from .native_config import NativeConfig
 from .native_mode import activate, find_open_port
 
 __all__ = [
     'activate',
+    'drop_queue',
     'find_open_port',
     'method_queue',
     'NativeConfig',
