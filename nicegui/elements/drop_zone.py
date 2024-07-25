c5172483cf9f3794a8746ef37894005285499437
Jacob Danell <jacob@emberlight.se>
2024-07-25 14:27:45 +0200

    Add shutdown support

--- nicegui/elements/drop_zone.py
+++ nicegui/elements/drop_zone.py
@@ -1,10 +1,11 @@
-from queue import Queue
-from threading import Thread
+from queue import Empty, Queue
+from threading import Event, Thread
 from typing import Any
 
-from .. import native
+from .. import core, native
 from ..element import Element
 from ..events import GenericEventArguments
+from ..server import Server
 
 
 class DropZone(Element, component="drop_zone.js"):
@@ -12,13 +13,20 @@ class DropZone(Element, component="drop_zone.js"):
         super().__init__()
 
         self.drop_info = Queue()
-        Thread(target=self.wait_for_file_info, args=(self.drop_info,), name=f"drop_zone_{self.id}").start()
+        self.stop_thread = Event()
+        self.thread = Thread(target=self.wait_for_file_info, args=(self.drop_info,), name=f"drop_zone_{self.id}")
+        self.thread.start()
         self.on("__file-dropped", handler=self.file_dropped_handler)
 
     def wait_for_file_info(self, drop_info: Queue[Any]):
         while True:
-            data = native.drop_queue.get()
-            drop_info.put(data)
+            try:
+                data = native.drop_queue.get(timeout=0.1)
+                if data:
+                    drop_info.put(data)
+            except Empty:
+                if core.app.is_stopped or core.app.is_stopping or Server.instance.should_exit:
+                    return
 
     def file_dropped_handler(self, event: GenericEventArguments):
         data = self.drop_info.get()
