7da736e69a4258179ea58662ba4342d176aea12c
Paula Kammler <paula@zauberzeug.com>
2024-07-27 11:58:12 +0200

    add conftest with renaming to pytests example

---
+++ examples/pytests/tests/conftest.py
@@ -0,0 +1,15 @@
+from typing import AsyncGenerator, Generator
+
+import pytest
+
+from nicegui.testing import Screen, User
+
+
+@pytest.fixture
+def screen(nicegui_screen: Screen) -> Generator[Screen, None, None]:
+    yield nicegui_screen
+
+
+@pytest.fixture
+async def user(nicegui_user: User) -> AsyncGenerator[User, None]:
+    yield nicegui_user
