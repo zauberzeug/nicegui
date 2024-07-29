7afcc7c4dac4af727641b8bead425dc3009e0c31
Paula Kammler <paula@zauberzeug.com>
2024-07-29 12:19:56 +0200

    double quotes

--- examples/pytests/tests/conftest.py
+++ examples/pytests/tests/conftest.py
@@ -1,5 +1,5 @@
-'''
+"""
 NOTE: This is uncommented because pytest doesn't allow for conftest.py files in subdirectories.
       If you want to use this example as a template, you have to uncomment the next line
-'''
+"""
 # pytest_plugins = ['nicegui.testing.fixtures']
