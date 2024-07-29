661f50e4faa3a60b4c27463327fa93e93eaf1e82
Paula Kammler <paula@zauberzeug.com>
2024-07-29 11:52:11 +0200

    remove renaming

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,1 @@
-from typing import AsyncGenerator, Generator
-
-import pytest
-
-from nicegui.testing import Screen, User
-
-
-@pytest.fixture
-def screen(nicegui_screen: Screen) -> Generator[Screen, None, None]:
-    yield nicegui_screen
-
-
-@pytest.fixture
-async def user(nicegui_user: User) -> AsyncGenerator[User, None]:
-    yield nicegui_user
+pytest_plugins = ['nicegui.testing.fixtures']
