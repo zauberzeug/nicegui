927766ef026933d69d38195b8155ed5a6f4e6b20
Falko Schindler <falko@zauberzeug.com>
2024-08-07 07:15:56 +0200

    fix is_even example

--- examples/is_even/main.py
+++ examples/is_even/main.py
@@ -3,14 +3,17 @@ from number_checker import NumberChecker
 
 from nicegui import ui
 
-number_checker = NumberChecker()
-number = ui.number(value=42)
 
+@ui.page('/')
+def page():
+    number_checker = NumberChecker()
+    number = ui.number(value=42.0)
 
-async def check():
-    even = await number_checker.is_even(number.value)
-    ui.notify(f'{number.value} is {"even" if even else "odd"}')
+    async def check():
+        even = await number_checker.is_even(number.value)
+        ui.notify(f'{number.value} is {"even" if even else "odd"}')
 
-ui.button('Check', on_click=check)
+    ui.button('Check', on_click=check)
+
 
 ui.run()
