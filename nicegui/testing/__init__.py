14a7f756da8f25e96ab1fc49e9b29b9b4992db3d
Rodja Trappe <rodja@zauberzeug.com>
2024-08-24 14:37:15 +0200

    allow nicegui.test to be used without selenium dependencies

--- nicegui/testing/__init__.py
+++ nicegui/testing/__init__.py
@@ -1,4 +1,9 @@
-from .screen import Screen
+try:
+    from .screen import Screen
+except ImportError:
+    # we simply define Screen as None if selenium is not installed
+    # this allows simpler dependency management when only using the "User" fixture
+    Screen = None  # type: ignore
 from .user import User
 from .user_interaction import UserInteraction
 
