5d6a2365676e049b079fa852cfd1f2c559895563
Rodja Trappe <rodja@zauberzeug.com>
2024-08-26 07:08:22 +0200

    re-raise original error

--- nicegui/testing/__init__.py
+++ nicegui/testing/__init__.py
@@ -1,14 +1,16 @@
 try:
     from .screen import Screen
-except ImportError:
+except ImportError as e:
     # we simply define Screen as None if selenium is not installed
     # this allows simpler dependency management when only using the "User" fixture
     class Screen:  # type: ignore
+        error = e
+
         def __init__(self, *args, **kwargs):
-            raise ImportError('Selenium dependencies are missing. Please install selenium to use the Screen class.')
+            raise RuntimeError('Screen is not available') from self.error
 
         def __getattr__(self, name):
-            raise ImportError('Selenium dependencies are missing. Please install selenium to use the Screen class.')
+            raise RuntimeError('Screen is not available') from self.error
 
 from .user import User
 from .user_interaction import UserInteraction
