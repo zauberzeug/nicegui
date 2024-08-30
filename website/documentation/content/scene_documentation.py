6b1766c7c5f96e107cc5fa3bfd1a324be902e5eb
Falko Schindler <falko@zauberzeug.com>
2024-08-30 15:48:44 +0200

    add missing page function for scene.initialized demo

--- website/documentation/content/scene_documentation.py
+++ website/documentation/content/scene_documentation.py
@@ -167,10 +167,13 @@ def point_clouds() -> None:
     This demo animates a camera movement after the scene has been fully loaded.
 ''')
 async def wait_for_init() -> None:
-    with ui.scene(width=285, height=220) as scene:
-        scene.sphere()
-        await scene.initialized()
-        scene.move_camera(x=1, y=-1, z=1.5, duration=2)
+    # @ui.page('/')
+    # async def page():
+    with ui.column():  # HIDE
+        with ui.scene(width=285, height=220) as scene:
+            scene.sphere()
+            await scene.initialized()
+            scene.move_camera(x=1, y=-1, z=1.5, duration=2)
 
 
 @doc.demo(ui.scene_view)
