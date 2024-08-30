ce0e5875eae82517f23743231edd65e8c8c98ebe
Rodja Trappe <rodja@zauberzeug.com>
2024-08-30 06:04:39 +0200

    remove timer objects from hierarchy

--- nicegui/elements/timer.py
+++ nicegui/elements/timer.py
@@ -125,3 +125,5 @@ class Timer(Element, component='timer.js'):
 
     def _cleanup(self) -> None:
         self.callback = None
+        assert self.parent_slot
+        self.parent_slot.parent.remove(self)
