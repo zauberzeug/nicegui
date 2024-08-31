ac684e6beca8064b3e19d019d0a830a544dacc23
Lucas Bourneuf <lucas@bourneuf.net>
2024-08-31 16:28:26 +0200

    trying  falkoschindler alternative

--- nicegui/elements/zeitline.js
+++ nicegui/elements/zeitline.js
@@ -1,3 +1,4 @@
+import { loadResource } from "../../static/utils/resources.js";
 export default {
   template: `
     <div></div>
@@ -25,9 +26,13 @@ export default {
   },
   async mounted() {
     await this.$nextTick(); // NOTE: wait for window.path_prefix to be set
-    this.ZL = await import(window.path_prefix + `${this.resource_path}/zeitline.bundle.min.js`);
-    const ZL = this.ZL;
-    this.line = new ZL.Timeline(this.conf);
+    // this.ZL = await import(window.path_prefix + `${this.resource_path}/zeitline.bundle.min.js`);
+    // const ZL = this.ZL;
+    // this.line = new ZL.Timeline(this.conf);
+    // alternative proposed by falkoschindler using same syntax as nicegui/examples/fullcalendar/fullcalendar.js
+    await loadResource(window.path_prefix + `${this.resource_path}/zeitline.bundle.min.js`);
+    // use the loaded package
+    this.line = new Zeitline.Timeline(this.conf);
     this.line.render();
     this.resolveLine(this.line);
   },
