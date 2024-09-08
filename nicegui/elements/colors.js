6f84b3dc24883c929e12e0de46ff85af9ba7ab67
PythonPan <2292551082@qq.com>
2024-09-08 22:02:51 +0800

    Add dark_page props.

--- nicegui/elements/colors.js
+++ nicegui/elements/colors.js
@@ -3,12 +3,14 @@ export default {
     for (let name in this.$props) {
       document.body.style.setProperty("--q-" + name, this.$props[name]);
     }
+    document.body.style.setProperty("--q-dark-page", this.$props['dark_page']);
   },
   props: {
     primary: String,
     secondary: String,
     accent: String,
     dark: String,
+    dark_page: String,
     positive: String,
     negative: String,
     info: String,
