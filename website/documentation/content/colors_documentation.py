cc1bdcc444a971a59df3584899d1a0b367242d98
Ciro García Belmonte <info@cirogarcia.dev>
2024-09-10 08:48:19 +0200

    docs for custom colors

--- website/documentation/content/colors_documentation.py
+++ website/documentation/content/colors_documentation.py
@@ -12,4 +12,22 @@ def main_demo() -> None:
     b2 = ui.button('Gray', on_click=lambda: [b.classes(replace='!bg-[#555]') for b in [b1, b2]])
 
 
+@doc.demo('Custom colors', '''
+    You can add custom color definitions for branding. In this case, `ui.colors` must be called before the
+    custom color is ever used.
+''')
+def custom_color_demo() -> None:
+    import random
+
+    def random_color():
+        return f'#{random.randint(0, 0xffffff):06x}'
+
+    ui.colors(my_custom_color=random_color())
+    ui.button(
+        'Random color',
+        color='my-custom-color',
+        on_click=lambda: ui.colors(my_custom_color=random_color())
+    )
+
+
 doc.reference(ui.colors)
