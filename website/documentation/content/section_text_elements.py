4e07df214038ab41347db638711eecdcb68893a6
Rodja Trappe <rodja@zauberzeug.com>
2024-10-25 06:15:49 +0200

    provide info and demo on how to set props

--- website/documentation/content/section_text_elements.py
+++ website/documentation/content/section_text_elements.py
@@ -25,7 +25,9 @@ doc.intro(html_documentation)
 @doc.demo('Other HTML Elements', '''
     There is an `html` module that allows you to insert other HTML elements like `<span>`, `<div>`, `<p>`, etc.
     It is equivalent to using the `ui.element` method with the `tag` argument.
+
     Like any other element, you can add classes, style, props, tooltips and events.
+    One convenience is that the kwargs are automatically added to the element's `props` dictionary.
 ''')
 def other_html_elements():
     from nicegui import html, ui
@@ -36,3 +38,6 @@ def other_html_elements():
             .on('click', lambda: ui.notify('Bold!'))
         html.hr()
         html.em('This is italic.').tooltip('Nice!')
+        with ui.row():
+            html.img().props('src=https://placehold.co/60')
+            html.img(src='https://placehold.co/60')
