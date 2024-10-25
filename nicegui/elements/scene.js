zauberzeug/nicegui

commit d136bfaac2ba751ca4c0b4b359703e07f6f3730a

Fix orbit controls of `ui.scene` after changing the camera's up vector (#3544)
* Remove hardcoded z-up for scene orbit controls

* Revert "Remove hardcoded z-up for scene orbit controls"

This reverts commit 8c80453c1f9b90b8ba74f0253bb23ebad0864ae2.

* Reset OrbitControls after camera up changed

---------

Co-authored-by: Andreas Voigt <andreas.voigt@optonic.com>

nicegui/elements/scene.js
@@ -401,6 +401,7 @@ export default {
     },
     move_camera(x, y, z, look_at_x, look_at_y, look_at_z, up_x, up_y, up_z, duration) {
       if (this.camera_tween) this.camera_tween.stop();
+      const camera_up_changed = up_x !== null || up_y !== null || up_z !== null;
       this.camera_tween = new TWEEN.Tween([
         this.camera.position.x,
         this.camera.position.y,
@@ -432,6 +433,12 @@ export default {
           this.look_at.set(p[6], p[7], p[8]);
           this.camera.lookAt(p[6], p[7], p[8]);
           this.controls.target.set(p[6], p[7], p[8]);
+        })
+        .onComplete(() => {
+          if (camera_up_changed) {
+            this.controls.dispose();
+            this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+          }
         })
         .start();
     },

commit 5f9b3ff9a9d6efab1c29c669cea6f2fdd264ebfd

Fix camera looking back to the origin after calling `move_camera()` (#3920)
This PR fixes a problem introduced by PR #3544: The camera kept looking
at the origin when calling `move_camera()` with given `look_at_*`
coordinates.

The fix can be verified with the following snippet. Moving back and
forth between the spheres should not point the camera back to the
origin, an the orbit controls should orbit around the sphere which is
currently looked at:
```py
with ui.scene() as scene:
    scene.sphere().move(1, 2, 0).material('blue')
    scene.sphere().move(4, 5, 0).material('red')

ui.button('blue', on_click=lambda: scene.move_camera(*(1, 2, 3), *(1, 2, 0), *(0, 1, 0), duration=1))
ui.button('red', on_click=lambda: scene.move_camera(*(4, 5, 6), *(4, 5, 0), *(0, 1, 0), duration=0))
```

nicegui/elements/scene.js
@@ -452,6 +452,8 @@ export default {
           if (camera_up_changed) {
             this.controls.dispose();
             this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+            this.controls.target.copy(this.look_at);
+            this.camera.lookAt(this.look_at);
           }
         })
         .start();