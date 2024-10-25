3e65af2827880fd89410b576a3ef2a422b24eaa9
Falko Schindler <falko@zauberzeug.com>
2024-10-25 17:02:56 +0200

    Introduce an `html` module with plain HTML elements (#3900)
    Following up on
    https://github.com/zauberzeug/nicegui/discussions/572#discussioncomment-10476200,
    this PR introduces an `html` module for creating plain HTML elements.
    They are basically shortcuts for `ui.element(<tag>)` and allow passing
    inner HTML as optional positional argument and arbitrary attributes as
    keyword arguments. Nesting, classes, styling and event subscriptions
    work like with regular NiceGUI elements. Here's a quick demo:
    
    ```py
    from nicegui import html
    
    with html.p():
        html.a('Link', href='https://google.com').classes('text-red-500')
        html.hr()
        html.button('Button').on('click', lambda: print('Hello World'))
        html.img(src='https://picsum.photos/400/300', width='200')
    ```
    
    Open tasks:
    
    - [x] Should we exclude "unusable" elements like `html`, `head`, and
    `body`? This might not be obvious in all cases.
    - [x] Add it to the documentation.
    - [x] Add pytests for some elements like `button`?
    
    ---------
    
    Co-authored-by: Rodja Trappe <rodja@zauberzeug.com>

--- nicegui/__init__.py
+++ nicegui/__init__.py
@@ -1,4 +1,4 @@
-from . import elements, run, ui
+from . import elements, html, run, ui
 from .api_router import APIRouter
 from .app.app import App
 from .client import Client
@@ -16,6 +16,7 @@ __all__ = [
     'context',
     'ElementFilter',
     'elements',
+    'html',
     'run',
     'Tailwind',
     'ui',
--- website/documentation/content/section_text_elements.py
+++ website/documentation/content/section_text_elements.py
@@ -20,3 +20,24 @@ doc.intro(markdown_documentation)
 doc.intro(restructured_text_documentation)
 doc.intro(mermaid_documentation)
 doc.intro(html_documentation)
+
+
+@doc.demo('Other HTML Elements', '''
+    There is an `html` module that allows you to insert other HTML elements like `<span>`, `<div>`, `<p>`, etc.
+    It is equivalent to using the `ui.element` method with the `tag` argument.
+
+    Like with any other element, you can add classes, style, props, tooltips and events.
+    One convenience is that the keyword arguments are automatically added to the element's `props` dictionary.
+''')
+def other_html_elements():
+    from nicegui import html, ui
+
+    with html.section().style('font-size: 120%'):
+        html.strong('This is bold.') \
+            .classes('cursor-pointer') \
+            .on('click', lambda: ui.notify('Bold!'))
+        html.hr()
+        html.em('This is italic.').tooltip('Nice!')
+        with ui.row():
+            html.img().props('src=https://placehold.co/60')
+            html.img(src='https://placehold.co/60')
