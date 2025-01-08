f43a3cfb2abc4777ce237a0a67e9487779c9d2ab
Daniel Kramer <drkspace@gmail.com>
2025-01-08 18:59:20 +0000

    Added documentation for disabling line_plot auto limit

--- website/documentation/content/line_plot_documentation.py
+++ website/documentation/content/line_plot_documentation.py
@@ -31,5 +31,39 @@ def main_demo() -> None:
             ui.timer(10.0, turn_off, once=True)
     line_checkbox.on('update:model-value', handle_change, args=[None])
 
+@doc.demo("Setting Custom Limits",
+          """
+        By default, the x and y limits are calculated and changed based on new data points.
+        You can disable this behovior by passing `update_x_lims = True` and/or `update_y_lims = True` to `push`.
+""")
+def lims_demo() -> None:
+    import math
+    from datetime import datetime
+    import matplotlib.pyplot as plt
+
+    line_plot = ui.line_plot(n=1, limit=20, figsize=(3, 2), update_every=5) \
+        .with_legend(['sin'], loc='upper center', ncol=1)
+
+    with line_plot:
+        plt.ylim([0,1])
+
+    def update_line_plot() -> None:
+        now = datetime.now()
+        x = now.timestamp()
+        y1 = math.sin(x)
+        line_plot.push([now], [[y1]], update_y_lims=False)
+
+    line_updates = ui.timer(0.1, update_line_plot, active=False)
+    line_checkbox = ui.checkbox('active').bind_value(line_updates, 'active')
+
+    # END OF DEMO
+    def handle_change(e: events.GenericEventArguments) -> None:
+        def turn_off() -> None:
+            line_checkbox.set_value(False)
+            ui.notify('Turning off that line plot to save resources on our live demo server. 😎')
+        line_checkbox.value = e.args
+        if line_checkbox.value:
+            ui.timer(10.0, turn_off, once=True)
+    line_checkbox.on('update:model-value', handle_change, args=[None])
 
 doc.reference(ui.line_plot)
