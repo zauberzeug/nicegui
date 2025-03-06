d35182c6566bd714135e9c9f5a2f35f4e7cec85f
Falko Schindler <falko@zauberzeug.com>
2025-03-06 19:28:37 +0100

    disable matplotlib

--- profile_imports.py
+++ profile_imports.py
@@ -2,9 +2,12 @@
 #
 # kernprof -l -v profile_imports.py
 
+import os
 import sys
 
 from line_profiler import LineProfiler
+
+os.environ['MATPLOTLIB'] = 'false'
 
 profile = LineProfiler()
 sys.modules['__main__'].__dict__['profile'] = profile
