557c71424fb393762a77f26e6375e2c9e661259b
Falko Schindler <falko@zauberzeug.com>
2025-03-07 09:06:26 +0100

    try mocking FastAPI with a WhateverModule

--- profile_imports.py
+++ profile_imports.py
@@ -4,6 +4,7 @@
 
 import os
 import sys
+from types import ModuleType
 
 from line_profiler import LineProfiler
 
@@ -11,6 +12,33 @@ os.environ['MATPLOTLIB'] = 'false'
 
 profile = LineProfiler()
 sys.modules['__main__'].__dict__['profile'] = profile
+
+
+class WhateverModule(ModuleType):
+
+    def __getattr__(self, name):
+        return Whatever()
+
+
+class Whatever:
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __getattr__(self, name):
+        return Whatever()
+
+    def __call__(self, *args, **kwargs):
+        return Whatever()
+
+    def __mro_entries__(self, *args, **kwargs):
+        return (Whatever, )
+
+    def __add__(self, other):
+        return Whatever()
+
+
+sys.modules['fastapi'] = WhateverModule('fastapi')
 
 if True:
     import nicegui
