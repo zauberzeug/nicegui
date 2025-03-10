2b066ebedf354132edd23f0cb25c7fb256cb6187
Falko Schindler <falko@zauberzeug.com>
2025-03-10 14:55:05 +0100

    review

--- examples/google_oauth2/main.py
+++ examples/google_oauth2/main.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 import os
+from typing import Optional
 
 from authlib.integrations.starlette_client import OAuth, OAuthError
 from dotenv import load_dotenv
@@ -9,24 +11,23 @@ from nicegui import app, ui
 
 # Get the credentials from the Google Cloud Console
 # https://developers.google.com/identity/gsi/web/guides/get-google-api-clientid#get_your_google_api_client_id
-# and pass them as environment variables (or write it to an .env file)
+# and pass them as environment variables (or write it to an .env file).
 load_dotenv()
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
 
 oauth = OAuth()
-
 oauth.register(
     name='google',
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_id=GOOGLE_CLIENT_ID,
     client_secret=GOOGLE_CLIENT_SECRET,
-    client_kwargs={'scope': 'openid email profile', },
+    client_kwargs={'scope': 'openid email profile'},
 )
 
 
 @app.get('/auth')
-async def google_oauth(request: Request):
+async def google_oauth(request: Request) -> RedirectResponse:
     try:
         user_data = await oauth.google.authorize_access_token(request)
     except OAuthError as e:
@@ -36,22 +37,21 @@ async def google_oauth(request: Request):
     return RedirectResponse('/')
 
 
-def logout():
+def logout() -> None:
     del app.storage.user['user_data']
     ui.navigate.to('/')
 
 
 @ui.page('/')
-async def main(request: Request):
-
+async def main(request: Request) -> Optional[RedirectResponse]:
     user_data = app.storage.user.get('user_data', None)
     if user_data:
-        ui.label(f'Welcome {user_data.get('userinfo', {}).get('name', '')}!')
+        ui.label(f'Welcome {user_data.get("userinfo", {}).get("name", "")}!')
         ui.button('Logout', on_click=logout)
     else:
         url = request.url_for('google_oauth')
         if url.hostname != 'localhost':  # NOTE this ensures we use the localhost URL configured in Google Console
-            return RedirectResponse(next(app.urls))
+            return RedirectResponse(next(iter(app.urls)))
         return await oauth.google.authorize_redirect(request, url)
 
 ui.run(storage_secret='random secret goes here')
