bdbca2011645830608af79539e7da1ccead2efdb
Falko Schindler <falko@zauberzeug.com>
2025-03-10 16:08:44 +0100

    remove dotenv dependency for simplicity

--- examples/google_oauth2/main.py
+++ examples/google_oauth2/main.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-import os
 from typing import Optional
 
 from authlib.integrations.starlette_client import OAuth, OAuthError
-from dotenv import load_dotenv
 from fastapi import Request
 from starlette.responses import RedirectResponse
 
@@ -11,10 +9,8 @@ from nicegui import app, ui
 
 # Get the credentials from the Google Cloud Console
 # https://developers.google.com/identity/gsi/web/guides/get-google-api-clientid#get_your_google_api_client_id
-# and pass them as environment variables (or write it to an .env file).
-load_dotenv()
-GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
-GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+GOOGLE_CLIENT_ID = '...'
+GOOGLE_CLIENT_SECRET = '...'
 
 oauth = OAuth()
 oauth.register(
@@ -48,10 +44,9 @@ async def main(request: Request) -> Optional[RedirectResponse]:
     if user_data:
         ui.label(f'Welcome {user_data.get("userinfo", {}).get("name", "")}!')
         ui.button('Logout', on_click=logout)
+        return None
     else:
         url = request.url_for('google_oauth')
-        if url.hostname != 'localhost':  # NOTE this ensures we use the localhost URL configured in Google Console
-            return RedirectResponse(next(iter(app.urls)))
         return await oauth.google.authorize_redirect(request, url)
 
 ui.run(storage_secret='random secret goes here')
