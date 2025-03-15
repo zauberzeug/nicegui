9ee1e3cfd4441cff6b89dda75657bfb648eaeeb5
evnchn <evanchan040511@gmail.com>
2025-03-15 04:11:37 +0100

    better filtering logic for the traceback to show only the things we wanted

--- nicegui/error.py
+++ nicegui/error.py
@@ -4,12 +4,15 @@ from typing import Union
 from .elements.column import Column as column
 from .elements.html import Html as html
 from .elements.label import Label as label
+from .elements.textarea import Textarea as textarea
 from .elements.button import Button as button
+from .elements.mixins.name_element import Element as element
 from .elements.code import Code as code
+
 
 import traceback
 
-SAD_FACE_SVG = (Path(__file__).parent / 'static' / 'sad_face.svg').read_text(encoding='utf-8')
+SAD_FACE_SVG = (Path(__file__).parent / 'static' / 'sad_face.svg').read_text()
 
 
 def error_content(status_code: int, exception: Union[str, Exception] = '') -> None:
@@ -30,11 +33,12 @@ def error_content(status_code: int, exception: Union[str, Exception] = '') -> No
         message2 = ''
     else:
         message = exception.__class__.__name__
+        message2 = traceback.format_exc(chain=False)
         if str(exception):
             message += ': ' + str(exception)
-        message2 = traceback.format_exc(chain=False)
-        # remove the last line from message2, because it's the same as message
-        message2 = '\n'.join(message2.split('\n')[:-1])
+            message2 = message2.rpartition(message)[0].strip()
+        # drop lines in traceback if they belong to NiceGUI
+        message2 = '\n'.join(message2.split('\n')[:1] + message2.split('\n')[4:])
 
     with column().style('width: 100%; padding: 5rem 0; align-items: center; gap: 0'):
         html(SAD_FACE_SVG).style('width: 8rem; padding: 1.25rem 0')
