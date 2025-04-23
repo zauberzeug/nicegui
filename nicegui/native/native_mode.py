d8f1c88aaf0a6e1d4fb0db052cb47cac567a0f82
Rodja Trappe <rodja@zauberzeug.com>
2025-04-23 07:49:10 +0200

    use existing multiprocessing import

--- nicegui/native/native_mode.py
+++ nicegui/native/native_mode.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import _thread
-import multiprocessing
 import multiprocessing as mp
 import queue
 import socket
@@ -93,8 +92,8 @@ def _start_window_method_executor(window: webview.Window,
             except Exception:
                 log.exception(f'error in window.{method_name}')
         # NOTE: we need to cleanup the multiprocessing queues, see https://github.com/zauberzeug/nicegui/issues/4131
-        if hasattr(multiprocessing.util, '_exit_function'):  # to make mypy happy
-            multiprocessing.util._exit_function()  # pylint: disable=protected-access
+        if hasattr(mp.util, '_exit_function'):  # to make mypy happy
+            mp.util._exit_function()  # pylint: disable=protected-access
 
     Thread(target=window_method_executor).start()
 
