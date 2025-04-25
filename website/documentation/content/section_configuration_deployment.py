b48ba1ab4f2266dda2d97885ffd265280ef3b644
Rodja Trappe <rodja@zauberzeug.com>
2025-04-25 06:06:25 +0200

    write backup with aiofiles

--- website/documentation/content/section_configuration_deployment.py
+++ website/documentation/content/section_configuration_deployment.py
@@ -90,6 +90,7 @@ def env_var_demo():
 def background_tasks_demo():
     from nicegui import background_tasks
     import asyncio
+    import aiofiles
 
     results = {'answer': '?'}
 
@@ -100,7 +101,9 @@ def background_tasks_demo():
     @background_tasks.await_on_shutdown
     async def backup() -> None:
         await asyncio.sleep(1)
-        # pathlib.Path('backup.json').write_text(f'{results["answer"]}')
+        # async with aiofiles.open('backup.json', 'w') as f:
+        #     await f.write(f'{results["answer"]}')
+        # print('backup.json written', flush=True)
 
     ui.label().bind_text_from(results, 'answer', lambda x: f'answer: {x}')
     ui.button('Compute', on_click=lambda: background_tasks.create(compute()))
