b950a31d4bfe2956f75417cf0893a9560deef482
Falko Schindler <falko@zauberzeug.com>
2025-04-28 18:43:50 +0200

    simplify change set processing for ui.codemirror

--- tests/test_codemirror.py
+++ tests/test_codemirror.py
@@ -1,6 +1,7 @@
 from typing import Dict, List
 
 from nicegui import ui
+from nicegui.elements.codemirror import _apply_change_set
 from nicegui.testing import Screen
 
 
@@ -31,3 +32,13 @@ def test_supported_values(screen: Screen):
     screen.wait_for('Done')
     assert values['languages'] == values['supported_languages']
     assert values['themes'] == values['supported_themes']
+
+
+def test_change_set():
+    assert _apply_change_set('', [0, 1], [['A']]) == 'A'
+    assert _apply_change_set('', [0, 2], [['AB']]) == 'AB'
+    assert _apply_change_set('X', [1, 2], [['AB']]) == 'AB'
+    assert _apply_change_set('X', [1, -1], []) == 'X'
+    assert _apply_change_set('X', [1, -1, 0, 1], [[], ['Y']]) == 'XY'
+    assert _apply_change_set('Hello', [5, -1, 0, 8], [[], [', world!']]) == 'Hello, world!'
+    assert _apply_change_set('Hello, world!', [5, -1, 7, 0, 1, -1], []) == 'Hello!'
