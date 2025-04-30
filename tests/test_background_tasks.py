9116acd9832710057d81f62a2fced06fe97a844d
Falko Schindler <falko@zauberzeug.com>
2025-04-30 10:59:53 +0200

    simplify pytest

--- tests/test_background_tasks.py
+++ tests/test_background_tasks.py
@@ -11,24 +11,21 @@ from nicegui.testing import User
 # NOTE: click handlers, and system events used to wrap background_task in a background_task (see https://github.com/zauberzeug/nicegui/pull/4641#issuecomment-2837448265)
 @pytest.mark.parametrize('strategy', ['direct', 'click', 'system'])
 async def test_awaiting_background_tasks_on_shutdown(user: User, strategy: str):
+    cancelled = set()
 
     async def one():
-        nonlocal cancelled_one
         try:
             await asyncio.sleep(1)
         except asyncio.CancelledError:
-            cancelled_one = True
+            cancelled.add('one')
 
     @background_tasks.await_on_shutdown
     async def two():
-        nonlocal cancelled_two
         try:
             await asyncio.sleep(1)
         except asyncio.CancelledError:
-            cancelled_two = True
+            cancelled.add('two')
 
-    cancelled_one = False
-    cancelled_two = False
     ui.button('One', on_click=lambda: background_tasks.create(one(), name='one'))
     ui.button('Two', on_click=lambda: background_tasks.create(two(), name='two'))
 
@@ -49,5 +46,4 @@ async def test_awaiting_background_tasks_on_shutdown(user: User, strategy: str):
 
     # NOTE: teardown is called on shutdown; here we call it directly to test the teardown logic while test is still running
     await background_tasks.teardown()
-    assert cancelled_one
-    assert not cancelled_two
+    assert cancelled == {'one'}
