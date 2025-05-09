3c6f6c6d3e68ef31ddc5bd782888fc55ae4e0d1e
evnchn <evanchan040511@gmail.com>
2025-05-09 09:15:30 +0200

    Dear mermaid: we'll call API ourselves thankyouverymuch AGAIN

--- nicegui/elements/markdown.js
+++ nicegui/elements/markdown.js
@@ -7,6 +7,7 @@ export default {
     await loadResource(window.path_prefix + this.codehilite_css_url);
     if (this.use_mermaid) {
       this.mermaid = (await import("mermaid")).default;
+      this.mermaid.initialize({ startOnLoad: false });
       this.renderMermaid();
     }
   },
@@ -21,9 +22,32 @@ export default {
   methods: {
     renderMermaid() {
       this.$el.querySelectorAll(".mermaid-pre").forEach(async (pre, i) => {
-        await this.mermaid.run({ nodes: [pre.children[0]] });
+        try {
+          if (pre.children[0].innerText != this.lastContent) {
+            const { svg, bindFunctions } = await this.mermaid.render(this.$el.id + "_mermaid_" + i, pre.children[0].innerText);
+            this.lastSvg = svg;
+            this.lastBindFunctions = bindFunctions;
+            this.lastContent = pre.children[0].innerText;
+          }
+          this.addMermaidToElement(pre, this.lastSvg, this.lastBindFunctions);
+        } catch (error) {
+          const { svg, bindFunctions } = await this.mermaid.render(this.$el.id + "_mermaid_" + i, "error");
+          this.addMermaidToElement(pre, svg, bindFunctions);
+          const mermaidErrorFormat = { str: error.message, message: error.message, hash: error.name, error };
+          console.error(mermaidErrorFormat);
+        }
       });
     },
+    addMermaidToElement(element, svg, bindFunctions) {
+      const svgElement = document.createElement("div")
+      svgElement.classList.add("mermaid-svg");
+      svgElement.innerHTML = svg;
+      bindFunctions?.(svgElement);
+      element.querySelectorAll(".mermaid-svg").forEach((svg) => {
+        svg.remove();
+      });
+      element.appendChild(svgElement);
+    }
   },
   props: {
     codehilite_css_url: String,
