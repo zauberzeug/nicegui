d445eb8a0e932b822f4fd87d9325ae9a30e872e6
Falko Schindler <falko@zauberzeug.com>
2025-05-13 11:26:41 +0200

    remove unused `all_registry_keys`

--- website/documentation/tree.py
+++ website/documentation/tree.py
@@ -10,15 +10,11 @@ tree_format_list: List[Node] = []
 
 def build_tree_format_list() -> None:
     """Build tree format list."""
-    all_registry_keys = list(registry)
-    all_registry_keys.remove('')
-
     # First build the adjacency list
     adjacency_list: List[tuple[str, str, str]] = []
     for module, _ in tiles:
         name = module.__name__.split('.')[-1]
         adjacency_list.append(('', name, registry[name].title))
-        all_registry_keys.remove(name)
 
     i = 0
     while i < len(adjacency_list):
@@ -33,7 +29,6 @@ def build_tree_format_list() -> None:
             for part in registry_entry.parts:
                 if part.link:
                     adjacency_list.append((v, part.link, part.title))
-                    all_registry_keys.remove(part.link)
                 elif part.link_target:
                     adjacency_list.append((v, f'{v}#{part.link_target}', part.title))
         i += 1
