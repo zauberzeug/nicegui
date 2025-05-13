151670b352ee2e167ad654da5920f89292ee5518
Falko Schindler <falko@zauberzeug.com>
2025-05-13 15:59:07 +0200

    major refactoring of `build_tree_format_list`

--- website/documentation/tree.py
+++ website/documentation/tree.py
@@ -1,56 +1,37 @@
-from typing import Any, Dict, List, Tuple
+from __future__ import annotations
 
-from .content import registry
+from typing import Any, Dict, List, Optional
+
+from .content import DocumentationPage, registry
 from .content.overview import tiles
 
-Node = Dict[str, Any]
-
-tree_format_list: List[Node] = []
+tree_format_list: List[Dict[str, Any]] = []
 
 
 def build_tree_format_list() -> None:
     """Build tree format list."""
-    # First build the adjacency list
-    adjacency_list: List[Tuple[str, str, str]] = []
-    for module, _ in tiles:
-        name = module.__name__.rsplit('.', 1)[-1]
-        adjacency_list.append(('', name, registry[name].title or ''))
-
-    i = 0
-    while i < len(adjacency_list):
-        if i > 1000000:
-            break  # no way
-        _, v, _ = adjacency_list[i]
-        if '#' in v:
-            i += 1
-            continue
-        registry_entry = registry.get(v)
-        if registry_entry and registry_entry.parts:
-            for part in registry_entry.parts:
-                if part.link:
-                    adjacency_list.append((v, part.link, part.title or ''))
-                elif part.link_target:
-                    adjacency_list.append((v, f'{v}#{part.link_target}', part.title or ''))
-        i += 1
-
-    def add_to_tree(tree: List[Node], parent_id: str, child_id: str, title: str) -> bool:
-        for node in tree:
-            if node['id'] == parent_id:
-                node['children'].append({'id': child_id, 'children': [], 'title': title})
-                return True
-            # Recursively search in the children
-            if add_to_tree(node['children'], parent_id, child_id, title):
-                return True
-        return False
-
-    adjacency_list = [(k, v, t.replace('*', '')) for k, v, t in adjacency_list]
-
-    # Build the tree from adjacency list
     tree_format_list.clear()
-    for key, v, title in adjacency_list:
-        if key == '':
-            tree_format_list.append({'id': v, 'children': [], 'title': title})
-        elif not add_to_tree(tree_format_list, key, v, title):
-            # Try to add the child to the correct parent in the tree
-            # If the parent is not found, create a new top-level node
-            tree_format_list.append({'id': key, 'children': [{'id': v, 'children': [], 'title': title}]})
+    for module, _ in tiles:
+        page = registry[module.__name__.rsplit('.', 1)[-1]]
+        tree_format_list.append({
+            'id': page.name,
+            'title': _plain(page.title),
+            'children': _children(page),
+        })
+
+
+def _children(page: DocumentationPage) -> List[Dict[str, Any]]:
+    return [
+        {
+            'id': part.link if part.link else f'{page.name}#{part.link_target}',
+            'title': _plain(part.title),
+            'children': _children(registry[part.link]) if part.link else [],
+        }
+        for part in page.parts
+        if part.link or part.link_target
+    ]
+
+
+def _plain(string: Optional[str]) -> str:
+    assert string is not None
+    return string.replace('*', '')
