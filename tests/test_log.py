10533dd2a3a7588e29a269782789f4ecdc49e639
evnchn <evanchan040511@gmail.com>
2025-05-17 23:44:18 +0200

    pytest `ui.log` against default classes/style/props

--- tests/test_log.py
+++ tests/test_log.py
@@ -53,3 +53,31 @@ def test_special_characters(screen: Screen):
     screen.should_contain('50%')
     screen.click('push')
     screen.should_contain('100%')
+
+
+def test_log_against_defaults(screen: Screen):
+
+    ui.label.default_classes('text-gray-500')
+    ui.label.default_style('font-size: 2rem')
+    ui.label.default_props('my_label_props')
+
+    log = ui.log()
+    log.push('This is how a log label looks like',
+             classes='text-red-500',
+             style='font-size: 1rem',
+             props='my_log_props',
+             )
+
+    screen.open('/')
+
+    # for all divs, check if the default classes, style and props are applied, if so, fail
+    for div in screen.find_all_by_tag('div'):
+        props = div.get_attribute('props')
+        if props is not None and 'my_label_props' in props:
+            raise AssertionError(f'Default props should not be applied to log label: {div}')
+        classes = div.get_attribute('class')
+        if classes is not None and 'text-gray-500' in classes:
+            raise AssertionError(f'Default classes should not be applied to log label: {div}')
+        style = div.get_attribute('style')
+        if style is not None and 'font-size: 2rem' in style:
+            raise AssertionError(f'Default style should not be applied to log label: {div}')
