ad89528dc70e7de8c002e31b0945b6b8b034cf8a
Rodja Trappe <rodja@zauberzeug.com>
2025-05-19 10:32:10 +0200

    Remove Prometheus analytics

--- main.py
+++ main.py
@@ -6,14 +6,10 @@ from typing import Optional
 from fastapi import HTTPException, Request
 from fastapi.responses import RedirectResponse
 from starlette.middleware.sessions import SessionMiddleware
-
-import prometheus
 from nicegui import app, ui
 from website import anti_scroll_hack, documentation, fly, main_page, svg
 
-prometheus.start_monitor(app)
-
-# session middleware is required for demo in documentation and prometheus
+# session middleware is required for demo in documentation
 app.add_middleware(SessionMiddleware, secret_key=os.environ.get('NICEGUI_SECRET_KEY', ''))
 
 on_fly = fly.setup()
