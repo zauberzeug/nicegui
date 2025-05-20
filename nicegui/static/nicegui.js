1ba3512a6d6a25670a0e6c0db9cb608c80d3d459
Alex Pilon <alex.pilon@shopify.com>
2025-05-20 10:29:52 -0400

    avoid potential mounted_app is undefined gap by using created() (and assign mount in there) instead of beforeCreate

--- nicegui/static/nicegui.js
+++ nicegui/static/nicegui.js
@@ -329,7 +329,8 @@ function createApp(elements, options) {
     render() {
       return renderRecursively(this.elements, 0);
     },
-    beforeCreate() {
+    created() {
+      mounted_app = this;
       window.documentId = createRandomUUID();
       window.clientId = options.query.client_id;
       const url = window.location.protocol === "https:" ? "wss://" : "ws://" + window.location.host;
@@ -432,9 +433,6 @@ function createApp(elements, options) {
           }
         });
       }
-    },
-    mounted() {
-      mounted_app = this;
     },
   }).use(Quasar, {
     config: options.quasarConfig,
