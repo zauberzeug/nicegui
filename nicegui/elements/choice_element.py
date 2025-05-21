f22d03709197a79552f2aaa48c7ae474e99d19b5
jdoiro3 <joseph.doiron@trillianthealth.com>
2025-05-21 10:56:56 -0400

    extending ui.select so users can utilze more of the underlying quasar component features

--- nicegui/elements/choice_element.py
+++ nicegui/elements/choice_element.py
@@ -1,20 +1,28 @@
-from typing import Any, Optional, Union
+from typing import Any, Collection, Dict, Optional, TypedDict
 
 from ..events import Handler, ValueChangeEventArguments
 from .mixins.value_element import ValueElement
+
+
+class Option(TypedDict):
+    label: str
+    value: Any
 
 
 class ChoiceElement(ValueElement):
 
     def __init__(self, *,
                  tag: Optional[str] = None,
-                 options: Union[list, dict],
+                 options: Collection[Option],
                  value: Any,
                  on_change: Optional[Handler[ValueChangeEventArguments]] = None,
                  ) -> None:
         self.options = options
         self._values: list[str] = []
         self._labels: list[str] = []
+        self._values_to_option: dict[str, Option] = {
+            o["value"]: o for o in self.options
+        }
         self._update_values_and_labels()
         if not isinstance(value, list) and value is not None and value not in self._values:
             raise ValueError(f'Invalid value: {value}')
@@ -22,12 +30,15 @@ class ChoiceElement(ValueElement):
         self._update_options()
 
     def _update_values_and_labels(self) -> None:
-        self._values = self.options if isinstance(self.options, list) else list(self.options.keys())
-        self._labels = self.options if isinstance(self.options, list) else list(self.options.values())
+        self._values = [o["value"] for o in self.options]
+        self._labels = [o["label"] for o in self.options]
+        self._values_to_option: Dict[str, Option] = {
+            o["value"]: o for o in self.options
+        }
 
     def _update_options(self) -> None:
         before_value = self.value
-        self._props['options'] = [{'value': index, 'label': option} for index, option in enumerate(self._labels)]
+        self._props['options'] = self.options
         self._props[self.VALUE_PROP] = self._value_to_model_value(before_value)
         if not isinstance(before_value, list):  # NOTE: no need to update value in case of multi-select
             self.value = before_value if before_value in self._values else None
@@ -38,7 +49,7 @@ class ChoiceElement(ValueElement):
             self._update_options()
         super().update()
 
-    def set_options(self, options: Union[list, dict], *, value: Any = ...) -> None:
+    def set_options(self, options: Collection[Option], *, value: Any = ...) -> None:
         """Set the options of this choice element.
 
         :param options: The new options.
--- nicegui/elements/select.py
+++ nicegui/elements/select.py
@@ -1,18 +1,20 @@
 from collections.abc import Generator, Iterable, Iterator
 from copy import deepcopy
-from typing import Any, Callable, Literal, Optional, Union
+from typing import (Any, Callable, Collection, Iterator, List, Literal,
+                    Optional, Union)
 
 from ..events import GenericEventArguments, Handler, ValueChangeEventArguments
-from .choice_element import ChoiceElement
+from .choice_element import ChoiceElement, Option
 from .mixins.disableable_element import DisableableElement
 from .mixins.label_element import LabelElement
-from .mixins.validation_element import ValidationDict, ValidationElement, ValidationFunction
+from .mixins.validation_element import (ValidationDict, ValidationElement,
+                                        ValidationFunction)
 
 
 class Select(LabelElement, ValidationElement, ChoiceElement, DisableableElement, component='select.js'):
 
     def __init__(self,
-                 options: Union[list, dict], *,
+                 options: Collection[Option], *,
                  label: Optional[str] = None,
                  value: Any = None,
                  on_change: Optional[Handler[ValueChangeEventArguments]] = None,
@@ -101,7 +103,7 @@ class Select(LabelElement, ValidationElement, ChoiceElement, DisableableElement,
                             if arg1 == arg2['label']:
                                 e.args.remove(arg1)
                                 break
-                args = [self._values[arg['value']] if isinstance(arg, dict) else arg for arg in e.args]
+                args = [arg['value'] if isinstance(arg, dict) else arg for arg in e.args]
                 for arg in e.args:
                     if isinstance(arg, str):
                         self._handle_new_value(arg)
@@ -114,25 +116,14 @@ class Select(LabelElement, ValidationElement, ChoiceElement, DisableableElement,
                     new_value = self._handle_new_value(e.args)
                     return new_value if new_value in self._values else None
                 else:
-                    return self._values[e.args['value']]
+                    return e.args['value']
 
-    def _value_to_model_value(self, value: Any) -> Any:
+    def _value_to_model_value(self, value: Any) -> List[Option] | Option:
         # pylint: disable=no-else-return
         if self.multiple:
-            result = []
-            for item in value or []:
-                try:
-                    index = self._values.index(item)
-                    result.append({'value': index, 'label': self._labels[index]})
-                except ValueError:
-                    pass
-            return result
+            return [self._values_to_option[v] for v in value or []]
         else:
-            try:
-                index = self._values.index(value)
-                return {'value': index, 'label': self._labels[index]}
-            except ValueError:
-                return None
+            return self._values_to_option.get(value)
 
     def _generate_key(self, value: str) -> Any:
         if isinstance(self.key_generator, Generator):
