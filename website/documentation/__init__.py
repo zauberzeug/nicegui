699672de690e09c3083025dcb775135e79edfcb9
Rodja Trappe <rodja@zauberzeug.com>
2025-05-24 07:53:57 +0200

    fix issues with storage and context

--- website/documentation/__init__.py
+++ website/documentation/__init__.py
@@ -1,6 +1,11 @@
 import asyncio
 
-from nicegui import Client, ui
+from fastapi import Request, Response
+
+from nicegui import Client, app, ui
+from nicegui.logging import log
+from nicegui.page import page
+from nicegui.storage import RequestTrackingMiddleware
 
 from .content import overview, redirects, registry
 from .custom_restructured_text import CustomRestructuredText
@@ -13,7 +18,14 @@ from .windows import bash_window, browser_window, python_window
 
 async def preload_pages() -> None:
     """Execute demo functions once to register all page routes."""
-    with Client.auto_index_client:
+    async def call_next(_):
+        return Response(status_code=200)
+
+    request = Request(scope={'type': 'http', 'method': 'GET', 'path': '/', 'session': {}})
+    await RequestTrackingMiddleware(app).dispatch(request, call_next)
+    with Client(page(''), request=request) as client:
+        client.tab_id = '1'
+        await app.storage._create_tab_storage(client.tab_id)  # pylint: disable=protected-access
         for documentation in registry.values():
             for part in documentation.parts:
                 if part.demo is not None:
@@ -21,11 +33,13 @@ async def preload_pages() -> None:
                         try:
                             result = part.demo.function()
                             if asyncio.iscoroutine(result):
-                                await result
-                        except Exception:
+                                # NOTE: we are not using helpers.wait_for because it messes up the context and does not need to be cancelled
+                                await asyncio.wait_for(result, timeout=1)
+                        except TimeoutError:
                             pass
+                        except Exception:
+                            log.exception('Error in demo function %s in "%s"', part.demo.function.__name__, part.title)
                     container.delete()
-
 
 __all__ = [
     'CustomRestructuredText',
