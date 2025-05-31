aff175e7146c1a9e37b76b060a584afb2d9532c7
evnchn <evanchan040511@gmail.com>
2025-05-31 20:33:00 +0200

    add back the comment

--- nicegui/elements/image.js
+++ nicegui/elements/image.js
@@ -25,7 +25,7 @@ export default {
     };
   },
   mounted() {
-    setTimeout(() => this.compute_src(), 0);
+    setTimeout(() => this.compute_src(), 0); // NOTE: wait for window.path_prefix to be set in app.mounted()
   },
   updated() {
     this.compute_src();
