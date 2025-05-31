1d4dba11a1109ebc5cdf6a3eec9d31c004f27e0b
evnchn <evanchan040511@gmail.com>
2025-05-31 18:08:32 +0200

    Feat: `ui.page_scroller`

--- nicegui/page_layout.py
+++ nicegui/page_layout.py
@@ -307,6 +307,48 @@ class PageSticky(Element):
             self._props['expand'] = True
 
 
+class PageScroller(Element):
+
+    def __init__(self,
+                 position: PageStickyPositions = 'bottom-right',
+                 x_offset: float = 0,
+                 y_offset: float = 0,
+                 *,
+                 expand: bool = False,
+                 scroll_offset: int = 1000,
+                 duration: int = 300,
+                 reverse: bool = False,
+                 ) -> None:
+        """Page scroller
+
+        This element is based on Quasar's `QPageScroller <https://quasar.dev/layout/page-scroller>`_ component.
+
+        It is very similar to ``ui.page_sticky``, sharing ``position``, ``x_offset``, and ``y_offset`` parameters.
+
+        However, ``ui.page_sticky`` is always visible, and ``ui.page_scroller`` only appears after the ``scroll-offset`` is reached.
+
+        Once visible, the user can click on it to quickly get back to the top of the page in ``duration`` ms.
+
+        :param position: position on the screen (default: "bottom-right")
+        :param x_offset: horizontal offset (default: 0)
+        :param y_offset: vertical offset (default: 0)
+        :param expand: whether to fully expand instead of shrinking to fit the content (default: ``False``)
+        :param scroll_offset: the vertical offset in pixels at which the scroller becomes visible (default: 1000)
+        :param duration: the duration in milliseconds for the scroll animation (default: 300)
+        :param reverse: if True, the scroller will work in reverse, showing when at the top of the page, and scrolls to bottom when triggered (default: ``False``)
+        """
+        super().__init__('q-page-scroller')
+        self._props['position'] = position
+        self._props['offset'] = [x_offset, y_offset]
+        if expand:
+            self._props['expand'] = True
+
+        self._props['scroll-offset'] = scroll_offset
+        self._props['duration'] = duration
+        if reverse:
+            self.props('reverse')
+
+
 def _check_current_slot(element: Element) -> None:
     parent = context.slot.parent
     if parent != parent.client.content:
