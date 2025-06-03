3c2d98a30eeff2903f47eddd5ff550f18dac8971
Evan Chan <37951241+evnchn@users.noreply.github.com>
2025-06-03 15:50:40 +0800

    Support ui.run(cache_control_directives=) (#4540)
    https://github.com/zauberzeug/nicegui/discussions/4532#discussioncomment-12643633
    
    TL-DR: 
    
    * Cache the static assets more aggressively, but we don't know how much
    is enough, and afraid overdo it.
    * Expose `cache_control_directives` argument in `ui.run` and
    `ui.run_with`, with default being long 1-year cache*
    * Better than original solution because: 
      * Centralized management, as @falkoschindler pointed out
    * Ability to set other directives such as `immutable` (not available for
    Chrome), `stale-while-revalidate` (available for Chrome)
    
    Concerns: 
    
    * Is `ui.run` and `ui.run_with` the best place to add the parameter? 
    * *Should we set the default to "public, max-age=3600" to match the
    default behaviour from before the PR (maximize compatibility), or let
    the better (?) 1-year-cache be the default (maximize rollout of
    performance increase)?
    
    Testing: 
    
    ```py
    ui.run()
    ```
    
    <img width="689" alt="{28E98406-CA70-42CE-94FA-8C894D1E6E5C}"
    src="https://github.com/user-attachments/assets/de133236-966a-4fdf-9b22-df442db9962c"
    />
    
    ```py
    ui.run(cache_control_directives='no-store')
    ```
    
    <img width="586" alt="{40FAC44F-07B0-44B8-910D-F0A31875726A}"
    src="https://github.com/user-attachments/assets/7590b019-f9ab-47df-8883-251a56de91c0"
    />
    
    ---------
    
    Co-authored-by: Falko Schindler <falko@zauberzeug.com>

--- nicegui/middlewares.py
+++ nicegui/middlewares.py
@@ -2,6 +2,9 @@ from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoin
 from starlette.requests import Request
 from starlette.responses import Response
 
+from . import core
+from .version import __version__
+
 
 class RedirectWithPrefixMiddleware(BaseHTTPMiddleware):
 
@@ -11,4 +14,14 @@ class RedirectWithPrefixMiddleware(BaseHTTPMiddleware):
         if 'Location' in response.headers and response.headers['Location'].startswith('/'):
             new_location = prefix + response.headers['Location']
             response.headers['Location'] = new_location
+        return response
+
+
+class SetCacheControlMiddleware(BaseHTTPMiddleware):
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        response = await call_next(request)
+        if request.url.path.startswith(f'/_nicegui/{__version__}/') \
+                and not request.url.path.startswith(f'/_nicegui/{__version__}/dynamic_resources/'):
+            response.headers['Cache-Control'] = core.app.config.cache_control_directives
         return response
