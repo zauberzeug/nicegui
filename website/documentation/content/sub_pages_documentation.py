9d2226e817c27abcb89d4bbd4a9943dca6abc570
Rodja Trappe <rodja@zauberzeug.com>
2025-06-11 06:08:06 +0200

    add demo for later adding sub pages

--- website/documentation/content/sub_pages_documentation.py
+++ website/documentation/content/sub_pages_documentation.py
@@ -38,11 +38,12 @@ def main_demo() -> None:
     ui.sub_pages({'/': main, '/other': other}, root_path=f'/documentation/{location}')
 
 
-@doc.demo('Passing Parameters', '''
+@doc.demo('Passing Parameters to Sub Page', '''
     If a sub page needs to modify content from it's parent, the object can simply be passed via lambda.
 ''')
 def parameters_demo():
     # @ui.page('/')
+    # @ui.page('/{_:path}') # NOTE: our page should catch all paths
     # def index():
     #     with ui.row():
     #         ui.label('Title:')
@@ -72,10 +73,11 @@ def parameters_demo():
 
 
 @doc.demo('Async Sub Pages', '''
-    `ui.sub_pages` also work for async builder functions.
+    `ui.sub_pages` also work with async builder functions.
 ''')
 def async_demo():
     # @ui.page('/')
+    # @ui.page('/{_:path}') # NOTE: our page should catch all paths
     # def index():
     #     title = ui.label()
     #     ui.sub_pages({'/': lambda: main(title)})
@@ -97,6 +99,39 @@ def async_demo():
     ui.sub_pages({'/': lambda: main(2), '/other': other}, root_path='/documentation/sub_pages')
     ui.toggle({'': 'main', 'other': 'other'}, value='',
               on_change=lambda e: ui.navigate.to(f'/documentation/sub_pages/{e.value}'))
+
+
+@doc.demo('Adding Sub Pages', '''
+    Sometimes not all routes are known when creating the `ui.sub_pages` element.
+    In such cases, the `add` method can be used to add routes after the element has been created.
+    This can also be used to pass elements which should be placed below the `ui.sub_pages` container.
+''')
+def adding_sub_pages_demo() -> None:
+    # @ui.page('/')
+    # @ui.page('/{_:path}') # NOTE: our page should catch all paths
+    # def index():
+    #     pages = ui.sub_pages()
+    #     ui.separator()
+    #     footer = ui.label()
+    #     pages.add('/', lambda: main(footer))
+    #     pages.add('/other', lambda: other(footer))
+
+    def main(footer: ui.label):
+        footer.set_text('normal footer')
+        # ui.link('Go to other page', '/other')
+        ui.link('Go to other page', '/documentation/sub_pages/other')  # HIDE
+
+    def other(footer: ui.label):
+        footer.set_text('other footer')
+        # ui.link('Go to main page', '/')
+        ui.link('Go to main page', '/documentation/sub_pages')  # HIDE
+
+    # END OF DEMO
+    pages = ui.sub_pages(root_path='/documentation/sub_pages')
+    ui.separator()
+    footer = ui.label()
+    pages.add('/', lambda: main(footer))
+    pages.add('/other', lambda: other(footer))
 
 
 doc.reference(ui.sub_pages)
