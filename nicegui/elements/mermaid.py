aa0cc3f24bf2d6d4ad52496f687a840a57df9102
evnchn <evanchan040511@gmail.com>
2025-06-13 00:04:29 +0800

    Mermaid `on_node_click`, with `NICEGUI_HANDLER` drop-in approach

--- nicegui/elements/mermaid.py
+++ nicegui/elements/mermaid.py
@@ -1,5 +1,7 @@
+import uuid
 from typing import Dict, Optional
 
+from ..events import GenericEventArguments, Handler
 from .mixins.content_element import ContentElement
 
 
@@ -11,7 +13,9 @@ class Mermaid(ContentElement,
               ]):
     CONTENT_PROP = 'content'
 
-    def __init__(self, content: str, config: Optional[Dict] = None) -> None:
+    def __init__(self, content: str, config: Optional[Dict] = None, *,
+                 on_node_click: Optional[Handler[GenericEventArguments]] = None,
+                 function_placeholder: str = 'NICEGUI_HANDLER') -> None:
         """Mermaid Diagrams
 
         Renders diagrams and charts written in the Markdown-inspired `Mermaid <https://mermaid.js.org/>`_ language.
@@ -25,11 +29,19 @@ class Mermaid(ContentElement,
 
         Refer to the Mermaid documentation for the ``mermaid.initialize()`` method for a full list of options.
 
-        :param content: the Mermaid content to be displayed
+        :param content: the Mermaid content to be displayed. If ``on_node_click`` is passed, expect lines such as ``click A NICEGUI_HANDLER()``, where ``NICEGUI_HANDLER`` is a placeholder for the JavaScript function defined by NiceGUI to handle node clicks.
         :param config: configuration dictionary to be passed to ``mermaid.initialize()``
+        :param on_node_click: a callback function that will be called when a node is clicked. The function should accept an event argument
+        :param function_placeholder: the placeholder string to be used in the content for the click handler function. Defaults to "NICEGUI_HANDLER". Customizing it allows you to avoid conflicts with your Mermaid diagram content.
         """
         super().__init__(content=content)
-        self._props['config'] = config
+        self._props['config'] = config if config is not None else {}
+
+        if on_node_click is not None:
+            self.on('nodeClicked', on_node_click)
+            self._props['config']['securityLevel'] = 'loose'
+            self._props['function_name'] = f'mermaid_click_handler_{uuid.uuid4().hex}' if on_node_click else None
+            self._props['function_placeholder'] = function_placeholder
 
     def _handle_content_change(self, content: str) -> None:
         self._props[self.CONTENT_PROP] = content.strip()
