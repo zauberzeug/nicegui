fcc4841015821c34e3d48071295f7676766cd023
evnchn <evanchan040511@gmail.com>
2025-06-16 23:19:19 +0800

    Hotfix: reset the `app.config.vue_config` for test reset

--- nicegui/app/app.py
+++ nicegui/app/app.py
@@ -18,7 +18,7 @@ from ..observables import ObservableSet
 from ..server import Server
 from ..staticfiles import CacheControlledStaticFiles
 from ..storage import Storage
-from .app_config import AppConfig
+from .app_config import DEFAULT_VUE_CONFIG, AppConfig
 from .range_response import get_range_response
 
 
@@ -285,6 +285,7 @@ class App(FastAPI):
         self._connect_handlers.clear()
         self._disconnect_handlers.clear()
         self._exception_handlers[:] = [log.exception]
+        self.config.vue_config = DEFAULT_VUE_CONFIG
 
     @staticmethod
     def clients(path: str) -> Iterator[Client]:
