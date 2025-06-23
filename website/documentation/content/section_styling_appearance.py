7a09d526aa1101dfbea430c96f617b8b81097b74
Falko Schindler <falko@zauberzeug.com>
2025-06-23 18:24:26 +0200

    simplify demo

--- website/documentation/content/section_styling_appearance.py
+++ website/documentation/content/section_styling_appearance.py
@@ -205,7 +205,6 @@ doc.intro(dark_mode_documentation)
 doc.intro(add_style_documentation)
 
 
-@doc.auto_execute
 @doc.demo('Using other Vue UI frameworks', '''
     **This is strictly a beta feature! Many NiceGUI elements are likely to break, and the API is not yet stable.**
 
@@ -214,7 +213,8 @@ doc.intro(add_style_documentation)
     To do so, you need to add the framework's JavaScript and CSS file to the head of your HTML document and configure NiceGUI accordingly with `app.config.vue_config_script`.
 ''')
 def other_vue_ui_frameworks_demo():
-    # from nicegui import app, ui
+    from nicegui import app
+
     # ui.add_body_html('''
     #     <link rel="stylesheet" href="//unpkg.com/element-plus/dist/index.css" />
     #     <script defer src="https://unpkg.com/element-plus"></script>
@@ -222,17 +222,41 @@ def other_vue_ui_frameworks_demo():
     # app.config.vue_config_script = '''
     #     app.use(ElementPlus);
     # '''
-    # with ui.element('el-button'):
-    #     ui.html('I am a button from Element Plus')
-    # ui.run()
+
+    with ui.element('el-button'):
+        ui.html('I am a button from Element Plus')
+
     # END OF DEMO
-    @ui.page('/element_plus')
-    def element_plus_page():
-        ui.add_body_html('''
-            <link rel="stylesheet" href="/static/element-plus.css" />
-            <script defer src="/static/element-plus.js"></script>
-        ''')
-        ui.add_head_html('<script>USE_ELEMENT_PLUS = true;</script>')
-        with ui.element('el-button'):
-            ui.html('I am a button from Element Plus')
-    ui.link('Element Plus', '/element_plus')
+    ui.add_css('''
+        el-button {
+            border: 1px solid #dcdfe6;
+            border-radius: 4px;
+            color: #606266;
+            cursor: pointer;
+            font-weight: 500;
+            font-size: 14px;
+            height: 32px;
+            line-height: 1;
+            padding: 8px 15px;
+        }
+        el-button:hover {
+            background-color: rgb(235.9,245.3,255);
+            border-color: rgb(197.7,225.9,255);
+            color: rgb(64,158,255);
+        }
+        el-button:active {
+            border-color: rgb(64,158,255);
+        }
+        body.dark el-button {
+            border-color: #4c4d4f;
+            color: #cfd3dc;
+        }
+        body.dark el-button:hover {
+            background-color: rgb(24.4, 33.8, 43.5);
+            border-color: rgb(33.2, 61.4, 90.5);
+            color: rgb(64, 158, 255);
+        }
+        body.dark el-button:active {
+            border-color: rgb(64, 158, 255);
+        }
+    ''')
