83133176430a27c2dc219e21bd07cd9069aa366e
Rodja Trappe <rodja@zauberzeug.com>
2025-06-28 08:46:11 +0200

    update parameter passing demo and add new one on how to use PageArgs

--- website/documentation/content/sub_pages_documentation.py
+++ website/documentation/content/sub_pages_documentation.py
@@ -38,7 +38,8 @@ def main_demo() -> None:
 
 
 @doc.demo('Passing Parameters to Sub Page', '''
-    If a sub page needs to modify content from its parent, the object can simply be passed in a lambda function.
+    If a sub page needs data from its parent, a dictionary can be passed to the `ui.sub_pages` element.
+    The data will be available as a keyword argument in the sub page function or as `PageArgs` object.
 ''')
 def parameters_demo():
     # @ui.page('/')
@@ -49,9 +50,9 @@ def parameters_demo():
     #         title = ui.label()
     #     ui.separator()
     #     ui.sub_pages({
-    #        '/': lambda: main(title),
-    #        '/other': lambda: other(title),
-    #     })
+    #        '/': main,
+    #        '/other': other,
+    #     }, data={'title': title})
 
     def main(title: ui.label):
         title.text = 'Main page content'
@@ -68,7 +69,7 @@ def parameters_demo():
         ui.label('Title:')
         title = ui.label()
     ui.separator()
-    ui.sub_pages({'/': lambda: main(title), '/other': lambda: other(title)}, root_path='/documentation/sub_pages')
+    ui.sub_pages({'/': main, '/other': other}, root_path='/documentation/sub_pages', data={'title': title})
 
 
 @doc.demo('Async Sub Pages', '''
@@ -159,4 +160,24 @@ def binding_to_sub_pages_demo():
     ui.label().bind_text_from(pages, 'path')
 
 
-doc.reference(ui.sub_pages)
+@doc.demo('Using PageArgs', '''
+          By type-hinting the parameter as `PageArgs`, the sub page builder function gets unified access to
+          query parameters, path parameters, and more.
+''')
+def page_args_demo():
+    from nicegui import PageArgs
+
+    # @ui.page('/')
+    # @ui.page('/{_:path}')
+    # def index():
+    #     ui.link('msg=hello', '/documentation/sub_pages?msg=hello')
+    #     ui.link('msg=world', '/documentation/sub_pages?msg=world')
+    #     ui.sub_pages({'/': main})
+
+    def main(args: PageArgs):
+        ui.label(args.query_parameters.get('msg', 'no message'))
+
+    # END OF DEMO
+    ui.link('msg=hello', '/documentation/sub_pages?msg=hello')
+    ui.link('msg=world', '/documentation/sub_pages?msg=world')
+    ui.sub_pages({'/': main}, root_path='/documentation/sub_pages')
