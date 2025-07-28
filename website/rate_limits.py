e7a5d14594e1a1b7de459d084a9f979a62bf7a41
Falko Schindler <falko@zauberzeug.com>
2025-07-28 16:27:48 +0200

    code review

--- website/rate_limits.py
+++ website/rate_limits.py
@@ -1,17 +1,17 @@
+from nicegui import app
+
 try:
     from slowapi import Limiter, _rate_limit_exceeded_handler
     from slowapi.errors import RateLimitExceeded
     from slowapi.middleware import SlowAPIMiddleware
     from slowapi.util import get_remote_address
 
-    from nicegui import app
-
     HAS_SLOWAPI = True
 except ImportError:
     HAS_SLOWAPI = False
 
 
-def setup():
+def setup() -> None:
     if not HAS_SLOWAPI:
         return
     limiter = Limiter(key_func=get_remote_address, default_limits=['50/minute', '5/second'])
