205f334e9aaf8e346c1f2b53685ce7c4601dd6a0
Rodja Trappe <rodja@zauberzeug.com>
2025-07-29 05:45:07 +0200

    simplify fastapi example

--- examples/fastapi/main.py
+++ examples/fastapi/main.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
-import frontend
+import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from nicegui import app, ui
+
+# This example deliberately creates a separate FastAPI app and run nicegui on top of it by using ui.run_with.
+# Please note that the `app` object from NiceGUI also is a FastAPI app.
+# Often it is easier to stick to ui.run and use the `@app.get` etc. decorators to add normal FastAPI endpoints.
+fastapi_app = FastAPI()
 
 
-@app.get('/')
+@fastapi_app.get('/')
 def read_root():
-    return {'Hello': 'World'}
+    return {'message': 'Hello, FastAPI! Browse to /gui to see the NiceGUI app.'}
 
 
-frontend.init(app)
+@ui.page('/')
+def show():
+    ui.label('Hello, NiceGUI!')
+
+    # NOTE dark mode will be persistent for each user across tabs and server restarts
+    ui.dark_mode().bind_value(app.storage.user, 'dark_mode')
+    ui.checkbox('dark mode').bind_value(app.storage.user, 'dark_mode')
+
+
+ui.run_with(
+    fastapi_app,
+    mount_path='/gui',  # NOTE this can be omitted if you want the paths passed to @ui.page to be at the root
+    storage_secret='pick your private secret here',  # NOTE setting a secret is optional but allows for persistent storage per user
+)
 
 if __name__ == '__main__':
-    print('Please start the app with the "uvicorn" command as shown in the start.sh script')
+    uvicorn.run('main:fastapi_app', log_level='info', reload=True)
