4991003fe672220ff4a50832f29bfb9cf7d2f4f5
git2212 <p32gxy@gmail.com>
2025-08-12 10:52:49 -0400

    Update middlewares.py

--- nicegui/middlewares.py
+++ nicegui/middlewares.py
@@ -5,6 +5,9 @@ from starlette.responses import Response
 from . import core
 from .version import __version__
 
+import re
+from starlette.middleware import Middleware
+from starlette.types import ASGIApp, Receive, Scope, Send
 
 class RedirectWithPrefixMiddleware(BaseHTTPMiddleware):
 
@@ -25,3 +28,36 @@ class SetCacheControlMiddleware(BaseHTTPMiddleware):
                 and not request.url.path.startswith(f'/_nicegui/{__version__}/dynamic_resources/'):
             response.headers['Cache-Control'] = core.app.config.cache_control_directives
         return response
+
+class RedirectWithPrefixMiddlewareSubApp(Middleware):
+    def __init__(self, app: ASGIApp, root_path: str = '', mount_path: str = '/'):
+        super().__init__(app)
+        self._app=app
+        self._path_prefix = root_path.rstrip('/')
+        self._mount_path = mount_path.rstrip('/') or '/'
+        # Patterns for static files that shouldn't get path modification
+        self._static_patterns = [
+            re.compile(r'^/_nicegui/'),
+            re.compile(r'^/_static/'),
+            re.compile(r'^/_favicon\.ico$'),
+            re.compile(r'^/_favicon\.png$')
+        ]
+        # Pattern for file extensions
+        self._file_extension_pattern = re.compile(r'\.[a-z]{2,5}$')
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send):
+        if scope['type'] == 'websocket':
+            # Handle WebSocket connections
+            if scope['path'].startswith(self._mount_path):
+                new_path = f"{self._path_prefix}{scope['path']}"
+                if not new_path.endswith('/'):
+                    new_path += '/'
+                scope['path'] = new_path
+
+        elif scope['type'] == 'http':
+            if scope['path'].startswith(self._mount_path):
+                old_path = scope['path']
+                new_path = f"{self._path_prefix}{old_path}"
+                if not new_path.endswith('/') and not self._file_extension_pattern.search(new_path.rstrip('/')):
+                    new_path += '/'
+                scope['path'] = new_path
