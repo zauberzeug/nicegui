87bb667237c4204b4af9f564e5032b6e1b522ae7
Falko Schindler <falko@zauberzeug.com>
2025-08-19 15:13:15 +0200

    update card test

--- tests/test_card.py
+++ tests/test_card.py
@@ -4,10 +4,10 @@ from nicegui.testing import Screen
 
 def test_preserve_borders(screen: Screen):
     with ui.card():
-        ui.label('A').classes('border shadow')
+        a = ui.table(rows=[]).props('bordered flat')
     with ui.card().tight():
-        ui.label('B').classes('border shadow')
+        b = ui.table(rows=[]).props('bordered flat')
 
     screen.open('/')
-    assert screen.find('A').value_of_css_property('border') == '1px solid rgb(229, 231, 235)'
-    assert screen.find('B').value_of_css_property('border') == '0px none rgb(0, 0, 0)'
+    assert screen.find_element(a).value_of_css_property('border') == '1px solid rgba(0, 0, 0, 0.12)'
+    assert screen.find_element(b).value_of_css_property('border') == '0px none rgb(0, 0, 0)'
