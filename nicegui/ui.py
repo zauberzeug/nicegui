c896d6e9cfd1b386ca049bd7d6c9285819a400aa
Andy Zhu <azhu@tower-research.com>
2025-09-13 18:57:30 +0800

    Add nicegui.ui.altair wrapper
    Altair is one of the more popular plotting libraries,
    so add a dedicated wrapper for ease of reference,
    although the implementation is just a very thin
    wrapper over anywidget.
    
    Also add an example of altair usage.

--- nicegui/ui.py
+++ nicegui/ui.py
@@ -5,6 +5,7 @@ __all__ = [
     'add_sass',
     'add_scss',
     'aggrid',
+    'altair',
     'anywidget',
     'audio',
     'avatar',
@@ -135,6 +136,7 @@ __all__ = [
 from .context import context
 from .element import Element as element
 from .elements.aggrid import AgGrid as aggrid
+from .elements.altair import Altair as altair
 from .elements.anywidget import AnyWidget as anywidget
 from .elements.audio import Audio as audio
 from .elements.avatar import Avatar as avatar
