ec2f770e6abd2a9fc74d0ef2614e0b83a2fe164a
evnchn <evanchan040511@gmail.com>
2025-09-24 13:25:33 +0000

    Segregate Quasar's important rules in Python-side

---
+++ quasar_segregator.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import tinycss2
+from tinycss2 import ast
+
+ROOT = Path(__file__).parent
+STATIC = ROOT / 'nicegui' / 'static'
+
+rules = tinycss2.parse_stylesheet(
+    (STATIC / 'quasar.css').read_text(), skip_whitespace=True, skip_comments=True)
+
+rules_unimportant_only = []
+rules_important_only = []
+rules_mixed = []
+
+for rule in rules:
+    assert isinstance(rule, (ast.QualifiedRule, ast.AtRule)), f'Unexpected {rule}. Script needs update.'
+    declarations = tinycss2.parse_blocks_contents(rule.content or '')
+    has_important = False
+    has_unimportant = False
+    for declaration in declarations:
+        if isinstance(declaration, ast.Declaration) and declaration.important:
+            has_important = True
+        if isinstance(declaration, ast.Declaration) and not declaration.important:
+            has_unimportant = True
+    if has_important and has_unimportant:
+        rules_mixed.append(rule)
+    elif has_important:
+        rules_important_only.append(rule)
+    else:
+        rules_unimportant_only.append(rule)
+
+print(f'Found {len(rules_unimportant_only)} unimportant-only rules, '
+      f'{len(rules_important_only)} important-only rules, '
+      f'{len(rules_mixed)} mixed rules.')
+
+# serialize them all
+(STATIC / 'quasar_unimportant.css').write_text(
+    tinycss2.serialize(rules_unimportant_only))
+(STATIC / 'quasar_important.css').write_text(
+    tinycss2.serialize(rules_important_only))
+(STATIC / 'quasar_mixed.css').write_text(
+    tinycss2.serialize(rules_mixed))
