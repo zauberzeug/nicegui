a9b7d8a8a4b0bd08d261a483a3364bbef4ec9667
Falko Schindler <falko@zauberzeug.com>
2025-09-26 17:07:58 +0200

    simplify pytest

--- tests/test_button.py
+++ tests/test_button.py
@@ -1,37 +1,24 @@
-import pytest
-
 from nicegui import ui
 from nicegui.testing import Screen
 
 
-@pytest.mark.parametrize('extra_classes', ['', '!bg-[#00ffff]'])
-def test_quasar_colors(screen: Screen, extra_classes: str):
-    b1 = b2 = b3 = b4 = b5 = None
-
+def test_quasar_colors(screen: Screen):
     @ui.page('/')
     def page():
-        nonlocal b1, b2, b3, b4, b5
-        b1 = ui.button().classes(extra_classes)
-        b2 = ui.button(color=None).classes(extra_classes)
-        b3 = ui.button(color='red-5').classes(extra_classes)
-        b4 = ui.button(color='red-500').classes(extra_classes)
-        b5 = ui.button(color='#ff0000').classes(extra_classes)
+        ui.button()
+        ui.button(color=None)
+        ui.button(color='red-5')
+        ui.button(color='red-500')
+        ui.button(color='#ff0000')
+        ui.button().classes('!bg-[#00ffff]')
 
     screen.open('/')
-
-    if extra_classes == '!bg-[#00ffff]':
-        expected = ['rgba(0, 255, 255, 1)'] * 5
-    else:
-        expected = [
-            'rgba(88, 152, 212, 1)',
-            'rgba(0, 0, 0, 0)',
-            'rgba(239, 83, 80, 1)',
-            'oklch(0.637 0.237 25.331)',
-            'rgba(255, 0, 0, 1)',
-        ]
-
-    for b, exp in zip((b1, b2, b3, b4, b5), expected):
-        assert screen.find_element(b).value_of_css_property('background-color') == exp
+    assert screen.find_all_by_tag('button')[0].value_of_css_property('background-color') == 'rgba(88, 152, 212, 1)'
+    assert screen.find_all_by_tag('button')[1].value_of_css_property('background-color') == 'rgba(0, 0, 0, 0)'
+    assert screen.find_all_by_tag('button')[2].value_of_css_property('background-color') == 'rgba(239, 83, 80, 1)'
+    assert screen.find_all_by_tag('button')[3].value_of_css_property('background-color') == 'oklch(0.637 0.237 25.331)'
+    assert screen.find_all_by_tag('button')[4].value_of_css_property('background-color') == 'rgba(255, 0, 0, 1)'
+    assert screen.find_all_by_tag('button')[5].value_of_css_property('background-color') == 'rgba(0, 255, 255, 1)'
 
 
 def test_enable_disable(screen: Screen):
