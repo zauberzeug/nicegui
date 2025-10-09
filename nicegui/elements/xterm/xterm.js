2a2f4548635508c6a1a94a36f1bf2f2f1aebe5a8
Falko Schindler <falko@zauberzeug.com>
2025-10-09 18:00:12 +0200

    update package management to new NiceGUI 3.0 style

--- nicegui/elements/xterm/xterm.js
+++ nicegui/elements/xterm/xterm.js
@@ -1,19 +1,16 @@
-import "xterm";
-import "addon-fit";
-import "addon-web-links";
+import { Terminal, FitAddon, WebLinksAddon } from "nicegui-xterm";
 import { loadResource } from "../../static/utils/resources.js";
 
 export default {
   template: "<div></div>",
   async mounted() {
     await this.$nextTick(); // NOTE: wait for window.path_prefix to be set
-    await loadResource(window.path_prefix + `${this.resource_path}/xterm/css/xterm.css`);
+    await loadResource(window.path_prefix + `${this.resource_path}/xterm.css`);
 
     // Create terminal with addons
     this.terminal = new Terminal(this.options);
-    this.fit_addon = new FitAddon.FitAddon();
-    this.terminal.loadAddon(this.fit_addon);
-    this.terminal.loadAddon(new WebLinksAddon.WebLinksAddon());
+    this.terminal.loadAddon(new FitAddon());
+    this.terminal.loadAddon(new WebLinksAddon());
     this.terminal.open(this.$el);
 
     // Register events that are re-emitted by the vue component
--- nicegui/elements/xterm/xterm.py
+++ nicegui/elements/xterm/xterm.py
@@ -1,22 +1,17 @@
+from __future__ import annotations
+
 from pathlib import Path
-from typing import Dict, Optional, Union
 
 from typing_extensions import Self
 
-from ..awaitable_response import AwaitableResponse
-from ..element import Element
-from ..events import GenericEventArguments, Handler, XtermBellEventArguments, XtermDataEventArguments, handle_event
+from ...awaitable_response import AwaitableResponse
+from ...element import Element
+from ...events import GenericEventArguments, Handler, XtermBellEventArguments, XtermDataEventArguments, handle_event
 
 
-class Xterm(Element,
-            component='xterm.js',
-            dependencies=[
-                'lib/xterm/xterm/lib/xterm.js',
-                'lib/xterm/xterm-addon-fit/lib/addon-fit.js',
-                'lib/xterm/xterm-addon-web-links/lib/addon-web-links.js',
-            ]):
+class Xterm(Element, component='xterm.js', esm={'nicegui-xterm': 'dist'}):
 
-    def __init__(self, options: Optional[Dict] = None) -> None:
+    def __init__(self, options: dict | None = None) -> None:
         """Xterm
 
         This element is a wrapper around `xterm.js <https://github.com/xtermjs/xterm.js>`_ to emulate a terminal.
@@ -26,7 +21,7 @@ class Xterm(Element,
                         `xterm.js documentation <https://xtermjs.org/docs/api/terminal/classes/terminal/#constructor>`_.
         """
         super().__init__()
-        self.add_resource(Path(__file__).parent / 'lib' / 'xterm')
+        self.add_resource(Path(__file__).parent / 'dist')
 
         self._props['options'] = options or {}
 
@@ -44,8 +39,8 @@ class Xterm(Element,
         This method only resizes the terminal, making the number of rows and columns match the size of the container.
         Note that you might also need to resize the backing pty to match the new size of the terminal.
 
-        :return: AwaitableResponse that, if awaited, returns `None` once the method call is finished. Otherwise, the
-                 method is executed without waiting for it to finish.
+        :return: AwaitableResponse that, if awaited, returns ``None`` once the method call is finished.
+                 Otherwise, the method is executed without waiting for it to finish.
         """
         return self.run_method('fit')
 
@@ -67,26 +62,28 @@ class Xterm(Element,
     def input(self, data: str, was_user_input: bool = True) -> AwaitableResponse:
         """Input data to application side.
 
-        The data is treated the same way input typed into the terminal would (ie. the `data` event will fire). To write
-        data onto the terminal (e.g. data coming from a pty), use the `write` method instead.
+        The data is treated the same way input typed into the terminal would (i.e. the ``data`` event will fire).
+        To write data onto the terminal (e.g. data coming from a pty), use the ``write`` method instead.
 
         :param data: The data to forward to the application.
-        :param was_user_input: Whether the input is genuine user input. This triggers additional behavior like focus or
-                               selection clearing. Set this to false if the data sent should not be treated like user
-                               input would, for example passing an escape sequence to the application.
+        :param was_user_input: Whether the input is genuine user input.
+                               This triggers additional behavior like focus or selection clearing.
+                               Set this to ``False`` if the data sent should not be treated like user input would,
+                               for example passing an escape sequence to the application.
 
-        :return: AwaitableResponse that, if awaited, returns `None` once the method call is finished. Otherwise, the
-                 method is executed without waiting for it to finish.
+        :return: AwaitableResponse that, if awaited, returns ``None`` once the method call is finished.
+                 Otherwise, the method is executed without waiting for it to finish.
         """
         return self.run_method('input', data, was_user_input)
 
-    def write(self, data: Union[bytes, str]) -> AwaitableResponse:
+    def write(self, data: bytes | str) -> AwaitableResponse:
         """Write data to the terminal.
 
-        :param data: The data to write to the terminal. This can either be UTF-8 encoded bytes or a string.
+        :param data: The data to write to the terminal.
+                     This can either be UTF-8 encoded bytes or a string.
 
-        :return: AwaitableResponse that, if awaited, returns `None` once the method call is finished. Otherwise, the
-                 method is executed without waiting for it to finish.
+        :return: AwaitableResponse that, if awaited, returns ``None`` once the method call is finished.
+                 Otherwise, the method is executed without waiting for it to finish.
         """
         if isinstance(data, bytes):
             # Xterm.js accepts an `Uint8Array`, which we can get by converting `bytes` to a `list`
@@ -94,13 +91,14 @@ class Xterm(Element,
         else:
             return self.run_method('write', data)
 
-    def writeln(self, data: Union[bytes, str]) -> AwaitableResponse:
-        """Write data to the terminal, followed by a break line character (\\n).
+    def writeln(self, data: bytes | str) -> AwaitableResponse:
+        """Write data to the terminal, followed by a break line character (``\\n``).
 
-        :param data: The data to write to the terminal. This can either be UTF-8 encoded bytes or a string.
+        :param data: The data to write to the terminal.
+                     This can either be UTF-8 encoded bytes or a string.
 
-        :return: AwaitableResponse that, if awaited, returns `None` once the method call is finished. Otherwise, the
-                 method is executed without waiting for it to finish.
+        :return: AwaitableResponse that, if awaited, returns ``None`` once the method call is finished.
+                 Otherwise, the method is executed without waiting for it to finish.
         """
         if isinstance(data, bytes):
             # Xterm.js accepts an `Uint8Array`, which we can get by converting `bytes` to a `list`
@@ -111,8 +109,8 @@ class Xterm(Element,
     def run_terminal_method(self, name: str, *args, timeout: float = 1) -> AwaitableResponse:
         """Run a method of the Xterm.js terminal instance.
 
-        Refer to the `Xterm.js documentation <https://xtermjs.org/docs/api/terminal/classes/terminal/#methods>`_ for a
-        list of methods.
+        Refer to the `Xterm.js documentation <https://xtermjs.org/docs/api/terminal/classes/terminal/#methods>`_
+        for a list of methods.
 
         If the function is awaited, the result of the method call is returned.
         Otherwise, the method is executed without waiting for a response.
