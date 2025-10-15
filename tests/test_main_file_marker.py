bf00c7d18714703085a17e56412c6187accf82c1
Falko Schindler <falko@zauberzeug.com>
2025-10-15 16:09:47 +0200

    skip new main_file_marker tests for now

--- tests/test_main_file_marker.py
+++ tests/test_main_file_marker.py
@@ -3,12 +3,14 @@ import pytest
 from nicegui.testing import Screen, User
 
 
+@pytest.mark.skip('This test breaks all following tests')
 @pytest.mark.xfail(raises=FileNotFoundError, strict=True)
 @pytest.mark.nicegui_main_file('non_existent_file.py')
 async def test_marker_injects_main_file_for_user_plugin(user: User):
     await user.open('/')
 
 
+@pytest.mark.skip('This test breaks all following tests')
 @pytest.mark.xfail(raises=FileNotFoundError, strict=True)
 @pytest.mark.nicegui_main_file('non_existent_file.py')
 def test_marker_injects_main_file_for_screen_plugin(screen: Screen):
