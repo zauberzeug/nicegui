77b3d2a6d732717a66953462175282661337b2db
Falko Schindler <falko@zauberzeug.com>
2025-10-15 14:49:39 +0200

    make sure the test fails if the marker isn't working

--- tests/test_main_file_marker.py
+++ tests/test_main_file_marker.py
@@ -3,13 +3,13 @@ import pytest
 from nicegui.testing import Screen, User
 
 
-@pytest.mark.xfail(raises=FileNotFoundError)
+@pytest.mark.xfail(raises=FileNotFoundError, strict=True)
 @pytest.mark.nicegui_main_file('non_existent_file.py')
 async def test_marker_injects_main_file_for_user_plugin(user: User):
     await user.open('/')
 
 
-@pytest.mark.xfail(raises=FileNotFoundError)
+@pytest.mark.xfail(raises=FileNotFoundError, strict=True)
 @pytest.mark.nicegui_main_file('non_existent_file.py')
 def test_marker_injects_main_file_for_screen_plugin(screen: Screen):
     screen.open('/')
