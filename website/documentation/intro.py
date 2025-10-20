809253ede2fe2ffe111a4d5e6492ba4163dcf02d
Falko Schindler <falko@zauberzeug.com>
2025-10-20 11:57:11 +0200

    review

--- website/documentation/intro.py
+++ website/documentation/intro.py
@@ -9,12 +9,14 @@ from .demo import demo
 
 
 def create_intro() -> None:
-    @_main_page_demo('Single Page App', '''
-        Build applications with fast client-side routing using [`ui.sub_pages`](/documentation/sub_pages) and a `root` function as single entry point.
+    @_main_page_demo('Single-Page Applications', '''
+        Build applications with fast client-side routing using [`ui.sub_pages`](/documentation/sub_pages)
+        and a `root` function as single entry point.
         For each visitor, the `root` function is executed and generates the interface.
         [`ui.link`](/documentation/link) and [`ui.navigate`](/documentation/navigate) can be used to navigate to other sub pages.
 
-        If you do not want a Single Page App, you can also use [`@ui.page('/your/path')`](/documentation/page) to define standalone content available at a specific path.
+        If you do not want a single-page application, you can also use [`@ui.page('/your/path')`](/documentation/page)
+        to define standalone content available at a specific path.
     ''')
     def spa_demo():
         def root():
@@ -44,13 +46,13 @@ def create_intro() -> None:
     @_main_page_demo('Reactive Transformations', '''
         Create real-time interfaces with automatic updates.
         Type and watch text flow in both directions.
-        When input changes, the [binding](/documentation/section_binding_properties) transforms the text with a custom python function and updates the label.
+        When input changes, the [binding](/documentation/section_binding_properties) transforms the text
+        with a custom Python function and updates the label.
     ''')
     def binding_demo():
         def root():
-            with ui.column():
-                user_input = ui.input(value='Hello')
-                ui.label().bind_text_from(user_input, 'value', reverse)
+            user_input = ui.input(value='Hello')
+            ui.label().bind_text_from(user_input, 'value', reverse)
 
         def reverse(text: str) -> str:
             return text[::-1]
@@ -59,17 +61,18 @@ def create_intro() -> None:
 
     @_main_page_demo('Event System', '''
         Use an [Event](/documentation/event) to trigger actions and pass data.
-        Here we have an IoT temperature sensor submitting its readings to a [FastAPI endpoint](/documentation/section_pages_routing#api_responses) with path `/sensor`.
-        If a new value arrives, it emits an event for the chart to be updated.
+        Here we have an IoT temperature sensor submitting its readings
+        to a [FastAPI endpoint](/documentation/section_pages_routing#api_responses) with path "/sensor".
+        When a new value arrives, it emits an event for the chart to be updated.
     ''')
     def event_system_demo():
         from datetime import datetime
 
-        from nicegui import Event, app, ui
+        from nicegui import Event, app
 
         sensor = Event[float]()
 
-        # @app.get('/sensor')
+        # @app.post('/sensor')
         def sensor_webhook(temperature: float):
             sensor.emit(temperature)
 
@@ -79,17 +82,17 @@ def create_intro() -> None:
                 'yAxis': {'type': 'value', 'min': 'dataMin'},
                 'series': [{'type': 'line', 'data': [], 'smooth': True}],
             })
-            data = chart.options['series'][0]['data']
 
             def update_chart(temperature: float):
+                data = chart.options['series'][0]['data']
                 data.append([datetime.now().timestamp() * 1000, temperature])
                 if len(data) > 10:
                     data.pop(0)
 
             sensor.subscribe(update_chart)
-            #
             # END OF DEMO
 
+            data = chart.options['series'][0]['data']
             data.append([datetime.now().timestamp() * 1000, 24.0])
             ui.timer(1.0, lambda: update_chart(round(data[-1][1] + (random.random() - 0.5), 1)), immediate=False)
         app  # noqa: B018 to avoid unused import warning
