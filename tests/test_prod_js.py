bb93e5c485bec24935714494f9844f593cf7c92e
evnchn <evanchan040511@gmail.com>
2025-10-26 12:50:34 +0000

    Simply Use Vue ESM

--- tests/test_prod_js.py
+++ tests/test_prod_js.py
@@ -12,7 +12,7 @@ def test_dev_mode(screen: Screen) -> None:
         ui.label('Hello, world!')
 
     screen.open('/')
-    screen.selenium.find_element(By.XPATH, f'//script[@src="/_nicegui/{__version__}/static/vue.global.js"]')
+    screen.selenium.find_element(By.XPATH, f'//script[@src="/_nicegui/{__version__}/static/vue.esm-browser.js"]')
     screen.selenium.find_element(By.XPATH, f'//script[@src="/_nicegui/{__version__}/static/quasar.umd.js"]')
 
 
@@ -24,5 +24,5 @@ def test_prod_mode(screen: Screen):
         ui.label('Hello, world!')
 
     screen.open('/')
-    screen.selenium.find_element(By.XPATH, f'//script[@src="/_nicegui/{__version__}/static/vue.global.prod.js"]')
+    screen.selenium.find_element(By.XPATH, f'//script[@src="/_nicegui/{__version__}/static/vue.esm-browser.prod.js"]')
     screen.selenium.find_element(By.XPATH, f'//script[@src="/_nicegui/{__version__}/static/quasar.umd.prod.js"]')
