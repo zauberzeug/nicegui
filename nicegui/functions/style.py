e17acb4e83cc348ba84465ec5e06cf73a8e37552
Falko Schindler <falko@zauberzeug.com>
2025-11-17 18:06:11 +0100

    deprecate the use of `add_sass` and `add_scss`

--- nicegui/functions/style.py
+++ nicegui/functions/style.py
@@ -20,12 +20,14 @@ def add_css(content: Union[str, Path], *, shared: bool = False) -> None:
     add_head_html(f'<style>{content}</style>', shared=shared)
 
 
-def add_scss(content: Union[str, Path], *, indented: bool = False, shared: bool = False) -> None:
-    """Add SCSS style definitions to the page.
+def add_scss(content: Union[str, Path], *, indented: bool = False, shared: bool = False) -> None:  # DEPRECATED
+    """Add SCSS style definitions to the page (deprecated).
 
     This function can be used to add SCSS style definitions to the head of the HTML page.
 
     *Added in version 2.0.0*
+
+    **Note: This function is deprecated and will be removed in NiceGUI 4.0. Use add_css instead.**
 
     :param content: SCSS content (string or file path)
     :param indented: whether the content is indented (SASS) or not (SCSS) (default: `False`)
@@ -43,12 +45,14 @@ def add_scss(content: Union[str, Path], *, indented: bool = False, shared: bool 
     ''', shared=shared)
 
 
-def add_sass(content: Union[str, Path], *, shared: bool = False) -> None:
-    """Add SASS style definitions to the page.
+def add_sass(content: Union[str, Path], *, shared: bool = False) -> None:  # DEPRECATED
+    """Add SASS style definitions to the page (deprecated).
 
     This function can be used to add SASS style definitions to the head of the HTML page.
 
     *Added in version 2.0.0*
+
+    **Note: This function is deprecated and will be removed in NiceGUI 4.0. Use add_css instead.**
 
     :param content: SASS content (string or file path)
     :param shared: whether to add the code to all pages (default: ``False``, *added in version 2.14.0*)
