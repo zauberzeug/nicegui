f59494aeabc5debb75740c3c58a137b1d2c4bb70
Evan Chan <37951241+evnchn@users.noreply.github.com>
2025-11-18 13:48:53 +0800

    Put SASS and SCSS compiling in the browser (#5449)
    ### Motivation
    
    - Annoying to have to install a library to use SASS or SCSS
    - LibSass is deprecated, and that comes with it
    https://github.com/sass/libsass-python
    - No plans for a Dart Sass Python binding replacement:
    https://github.com/sass/dart-sass/issues/2166
    - **We will die on November 30, 2025 if nothing gets done**:
    https://github.com/zauberzeug/nicegui/discussions/5321#discussioncomment-14960698
    
    ### Implementation
    
    - Let the importmap be before the head HTML for it to import stuff
    (should have been the case since day1 but anyways...)
    - Follow
    https://github.com/sass/dart-sass/blob/main/README.md#dart-sass-in-the-browser
    - Drop check for installed Python library
    - Deprecate `indented` since it doesn't matter any more (cannot
    configure)
    - `immutable.es.js` had trailing whitespaces. Patch that. 
    
    ### Progress
    
    - [x] I chose a meaningful title that completes the sentence: "If
    applied, this PR will..."
    - [x] The implementation is complete.
    - [x] Pytests already have.
    - [x] Documentation is not necessary (self-explanatory).
    
    ### Final notes
    
    
    https://github.com/zauberzeug/nicegui/discussions/5321#discussioncomment-14961222
    lasted 33 minutes before it was outdate 🚀
    
    ---------
    
    Co-authored-by: Falko Schindler <falko@zauberzeug.com>

--- extract_core_libraries.py
+++ extract_core_libraries.py
@@ -5,6 +5,7 @@ import copy
 import difflib
 import re
 import shutil
+import subprocess
 from pathlib import Path
 
 import cssbeautifier
@@ -54,6 +55,11 @@ def _extract_headwind_css(quasar_css_path: Path) -> None:
     (STATIC / 'headwind.css').write_text(headwind_css)
 
 
+def _minify_js(input_path: Path, output_path: Path) -> None:
+    subprocess.run(['npx', '--yes', 'terser', str(input_path), '--compress', '--mangle', '--output', str(output_path)],
+                   capture_output=True, text=True, check=True)
+
+
 shutil.copy2(NODE_MODULES / 'vue' / 'dist' / 'vue.esm-browser.js', STATIC / 'vue.esm-browser.js')
 shutil.copy2(NODE_MODULES / 'vue' / 'dist' / 'vue.esm-browser.prod.js', STATIC / 'vue.esm-browser.prod.js')
 
@@ -70,3 +76,7 @@ shutil.copy2(NODE_MODULES / 'socket.io' / 'client-dist' / 'socket.io.min.js', ST
 shutil.copy2(NODE_MODULES / 'socket.io' / 'client-dist' / 'socket.io.min.js.map', STATIC / 'socket.io.min.js.map')
 
 shutil.copy2(NODE_MODULES / 'es-module-shims' / 'dist' / 'es-module-shims.js', STATIC / 'es-module-shims.js')
+
+_minify_js(NODE_MODULES / 'sass' / 'sass.default.js', STATIC / 'sass.default.js')
+_minify_js(NODE_MODULES / 'sass' / 'sass.dart.js', STATIC / 'sass.dart.js')
+_minify_js(NODE_MODULES / 'immutable' / 'dist' / 'immutable.es.js', STATIC / 'immutable.es.js')
