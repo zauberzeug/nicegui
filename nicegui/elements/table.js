1e56696278db8f79a9670e947b36b594b6961597
Luis Riegger <lri@me.com>
2025-11-21 12:02:13 +0100

    Enable tooltips on table component

--- nicegui/elements/table.js
+++ nicegui/elements/table.js
@@ -1,12 +1,16 @@
 import { convertDynamicProperties } from "../../static/utils/dynamic_properties.js";
 
 export default {
+  inheritAttrs: false,
   template: `
-    <q-table ref="qRef" :columns="convertedColumns">
-      <template v-for="(_, slot) in $slots" v-slot:[slot]="slotProps">
-        <slot :name="slot" v-bind="slotProps || {}" />
-      </template>
-    </q-table>
+    <div>
+      <q-table ref="qRef" v-bind="$attrs" :columns="convertedColumns">
+        <template v-for="(_, slot) in forwardedSlots" v-slot:[slot]="slotProps">
+          <slot :name="slot" v-bind="slotProps || {}" />
+        </template>
+      </q-table>
+      <slot />
+    </div>
   `,
   props: {
     columns: Array,
@@ -15,6 +19,10 @@ export default {
     convertedColumns() {
       this.columns.forEach((column) => convertDynamicProperties(column, false));
       return this.columns;
+    },
+    forwardedSlots() {
+      const { default: _default, ...nonDefaultSlots } = this.$slots;
+      return nonDefaultSlots;
     },
   },
 };
