1dea497f8397b27f51b00ca8f8349d429651862f
evnchn <evanchan040511@gmail.com>
2025-11-21 12:33:50 +0000

    test the tooltip method

---
+++ tests/test_tooltip.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium.webdriver import ActionChains
+
+from nicegui import ui
+from nicegui.element import Element
+from nicegui.testing.screen import Screen
+
+
+@pytest.mark.parametrize('element', [ui.label, ui.button, ui.markdown])
+def test_tooltip_method(screen: Screen, element: Element):
+    @ui.page('/')
+    def page():
+        element('Hover').tooltip('OK')
+
+    screen.open('/')
+    ActionChains(screen.selenium).move_to_element(screen.find('Hover')).perform()
+    screen.should_contain('OK')
