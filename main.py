af58ad7accf85e0f50dc447bf9d8298c0791dd79
evnchn <evanchan040511@gmail.com>
2025-11-25 05:53:22 +0000

    Cache control middleware (impl inspired from library code)

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ import os
 from pathlib import Path
 
 from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.middleware.sessions import SessionMiddleware
+from starlette.responses import Response
 
-from nicegui import app, ui
+from nicegui import app, core, ui
 from nicegui.page_arguments import RouteMatch
 from website import documentation, fly, header, imprint_privacy, main_page, rate_limits, svg
+
+
+class DocsSetCacheControlMiddleware(BaseHTTPMiddleware):
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        response = await call_next(request)
+        if request.url.path.startswith('/fonts/') \
+                or request.url.path.startswith('/static/'):
+            response.headers['Cache-Control'] = core.app.config.cache_control_directives
+        return response
+
+
+app.add_middleware(DocsSetCacheControlMiddleware)
+
 
 # session middleware is required for demo in documentation
 app.add_middleware(SessionMiddleware, secret_key=os.environ.get('NICEGUI_SECRET_KEY', ''))
