c8d99290eb2090a93799fe4aaa35816d9f70fea9
Falko Schindler <falko@zauberzeug.com>
2025-12-03 16:47:28 +0100

    fix typing

--- tests/test_tooltip.py
+++ tests/test_tooltip.py
@@ -2,12 +2,11 @@ import pytest
 from selenium.webdriver import ActionChains
 
 from nicegui import ui
-from nicegui.element import Element
 from nicegui.testing.screen import Screen
 
 
 @pytest.mark.parametrize('element', [ui.label, ui.button, ui.markdown])
-def test_tooltip_method(screen: Screen, element: Element):
+def test_tooltip_method(screen: Screen, element: type[ui.element]):
     @ui.page('/')
     def page():
         element('Hover').tooltip('OK')
