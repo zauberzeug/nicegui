1c0d937ce8b57369525b2245bcc78fd7d6820cee
Falko Schindler <falko@zauberzeug.com>
2025-12-19 12:06:32 +0100

    review altair_documentation.py

--- website/documentation/content/altair_documentation.py
+++ website/documentation/content/altair_documentation.py
@@ -9,12 +9,7 @@ def main_demo() -> None:
     import numpy as np
     import pandas as pd
 
-    rand = np.random.RandomState(42)
-
-    df = pd.DataFrame({
-        'xval': range(100),
-        'yval': rand.randn(100).cumsum()
-    })
+    df = pd.DataFrame({'xval': range(100), 'yval': np.random.randn(100).cumsum()})
 
     slider = alt.binding_range(min=0, max=100, step=1)
     cutoff = alt.param(name='cutoff', bind=slider, value=50)
@@ -22,17 +17,10 @@ def main_demo() -> None:
     chart = alt.Chart(df).mark_point().encode(
         x='xval',
         y='yval',
-        color=alt.condition(
-            alt.datum.xval < cutoff,
-            alt.value('red'), alt.value('blue')
-        )
-    ).add_params(
-        cutoff
-    )
+        color=alt.condition(alt.datum.xval < cutoff, alt.value('red'), alt.value('blue')),
+    ).add_params(cutoff)
 
-    ui.altair(chart, throttle=0.5)
-
-    ui.run()
+    ui.altair(chart)
 
 
 doc.reference(ui.altair)
