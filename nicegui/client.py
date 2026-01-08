6c52eb2c90c4b67387c025b29646b4bc1578eb83
Daniel Yudelevich <4537920+yudelevi@users.noreply.github.com>
2026-01-08 00:49:08 -0800

    Merge commit from fork
    * fix: close Redis connections on tab/user storage cleanup
    
    Redis listener loop was leaking connections due to missing cleanups:
    - Save reference to listener task to cancel it on close
    - added _should_listen flag in the listen loop for clean exit
    - Store tab_id before clearing to close its storage on disconnect
    - another issue that was raised during fixing : added check if already
    deleted before invoking delete handlers
    
    * fix type annotations for Python 3.9
    
    * Update nicegui/client.py
    
    Co-authored-by: Falko Schindler <falko@zauberzeug.com>
    
    * requested fixes
    
    * create a `_listener_task` in both cases
    
    * remove the `_should_listen` flag and rely on the task being cancelled during `close`
    
    * replace the try-finally block by awaiting the listener task
    
    * introduce `storage.close_tab`; handle PersistentDict and other storages (no-op)
    
    ---------
    
    Co-authored-by: Daniel Yudelevich <yudelevi@users.noreply.github.com>
    Co-authored-by: Falko Schindler <falko@zauberzeug.com>

--- nicegui/client.py
+++ nicegui/client.py
@@ -304,6 +304,7 @@ class Client:
         document_id = self._socket_to_document_id.pop(socket_id)
         self._cancel_delete_task(document_id)
         self._num_connections[document_id] -= 1
+        tab_id_to_close = self.tab_id
         self.tab_id = None
 
         for t in self.disconnect_handlers:
@@ -316,6 +317,7 @@ class Client:
             if self._num_connections[document_id] == 0:
                 self._num_connections.pop(document_id)
                 self._delete_tasks.pop(document_id)
+                await core.app.storage.close_tab(tab_id_to_close)
                 self.delete()
         self._delete_tasks[document_id] = \
             background_tasks.create(delete_content(), name=f'delete content {document_id}')
