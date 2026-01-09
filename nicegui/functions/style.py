zauberzeug/nicegui

commit a8fd25b7d5e23afb1952d0f60a1940e18b5f1ca8

Merge commit from fork
* Use Base64 encoding for add_css, add_scss and add_sass to prevent XSS by closing the script/style tag

* remove `safe` parameter

* remove base64 approach from add_scss

* reduce whitespace in head HTML

* cleanup

* add pytests

* use a unicode replacement for "<" instead of base64

* reduce payload

* create safe CSS content without JavaScript

* use same strategy for SCSS

* keep larger payload for simplicity of the deprecated `add_scss`

* switch back to the JavaScript-based solution

* reduce payload be defining an addStyle function in index.html

---------

Co-authored-by: Falko Schindler <falko@zauberzeug.com>

nicegui/functions/style.py
@@ -17,7 +17,8 @@ def add_css(content: Union[str, Path], *, shared: bool = False) -> None:
     """
     if helpers.is_file(content):
         content = Path(content).read_text(encoding='utf-8')
-    add_head_html(f'<style>{content}</style>', shared=shared)
+    safe_content = json.dumps(content).replace('<', r'\u003c')
+    add_head_html(f'<script>addStyle({safe_content});</script>', shared=shared)
 
 
 def add_scss(content: Union[str, Path], *, indented: bool = False, shared: bool = False) -> None:  # DEPRECATED
@@ -35,12 +36,11 @@ def add_scss(content: Union[str, Path], *, indented: bool = False, shared: bool 
     """
     content = Path(content).read_text(encoding='utf-8') if helpers.is_file(content) else str(content).strip()
     syntax = 'indented' if indented else 'scss'
+    safe_content = json.dumps(content).replace('<', r'\u003c')
     add_head_html(f'''
         <script type="module">
             import * as sass from "sass";
-            const style = document.createElement("style");
-            style.textContent = sass.compileString({json.dumps(content)}, {{syntax: "{syntax}"}}).css;
-            document.head.appendChild(style);
+            addStyle(sass.compileString({safe_content}, {{syntax: "{syntax}"}}).css);
         </script>
     ''', shared=shared)
 

tests/test_add_html.py
@@ -63,3 +63,26 @@ def test_sass(screen: Screen):
 
     screen.open('/')
     assert screen.find('This is purple on yellow with SASS.').value_of_css_property('color') == 'rgba(128, 0, 128, 1)'
+
+
+def test_add_css_with_script(screen: Screen):
+    @ui.page('/')
+    def page():
+        ui.add_css("</style></script><script>document.write('XSS');</script>")
+        ui.label('Hello, World!')
+
+    screen.open('/')
+    screen.should_contain('Hello, World!')
+    screen.should_not_contain('XSS')
+
+
+def test_add_scss_with_script(screen: Screen):
+    @ui.page('/')
+    def page():
+        ui.add_scss("</style></script><script>document.write('XSS');</script>")
+        ui.label('Hello, World!')
+
+    screen.allowed_js_errors.append('static/sass.dart.js')
+    screen.open('/')
+    screen.should_contain('Hello, World!')
+    screen.should_not_contain('XSS')

nicegui/templates/index.html
@@ -34,6 +34,9 @@
     <script type="importmap">
       {"imports": {{ imports | safe }}}
     </script>
+    <script>
+      addStyle = (c) => document.head.append(Object.assign(document.createElement("style"), { textContent: c }));
+    </script>
     {{ head_html | safe }}
     <!-- prevent Prettier from removing this line -->
     {% for url in js_imports_urls %}

commit 58ff288a5456d54d6303e25138bdd1f13a128dbd

Fix CSS not being added correctly after client connected (#5628)
### Motivation

In #5624 we noticed that there has been a regression in
a8fd25b7d5e23afb1952d0f60a1940e18b5f1ca8 (in the context of
https://github.com/advisories/GHSA-72qc-wxch-74mg). Now CSS isn't
applied if `add_css` is called after a client connected:
```py
@ui.page('/')
async def page():
    await ui.context.client.connected()
    ui.add_css('.red { color: red } ')
    ui.label('This should be red.').classes('red')
```

### Implementation

This PR refactors the `add_*` methods in style.py to call
`ui.run_javascript` if `client.has_socket_connection`.
It also extends the corresponding tests and resets the shared head and
body HTML in `nicegui_reset_globals`.

### Progress

- [x] I chose a meaningful title that completes the sentence: "If
applied, this PR will..."
- [x] The implementation is complete.
- [x] If this PR addresses a security issue, it has been coordinated via
the [security
advisory](https://github.com/zauberzeug/nicegui/security/advisories/new)
process.
- [x] Pytests have been extended.
- [x] Documentation is not necessary.

nicegui/functions/style.py
@@ -2,7 +2,8 @@ from pathlib import Path
 from typing import Union
 
 from .. import helpers, json
-from .html import add_head_html
+from ..client import Client
+from ..context import context
 
 
 def add_css(content: Union[str, Path], *, shared: bool = False) -> None:
@@ -18,7 +19,7 @@ def add_css(content: Union[str, Path], *, shared: bool = False) -> None:
     if helpers.is_file(content):
         content = Path(content).read_text(encoding='utf-8')
     safe_content = json.dumps(content).replace('<', r'\u003c')
-    add_head_html(f'<script>addStyle({safe_content});</script>', shared=shared)
+    _add_javascript(f'addStyle({safe_content});', shared=shared)
 
 
 def add_scss(content: Union[str, Path], *, indented: bool = False, shared: bool = False) -> None:  # DEPRECATED
@@ -37,11 +38,8 @@ def add_scss(content: Union[str, Path], *, indented: bool = False, shared: bool 
     content = Path(content).read_text(encoding='utf-8') if helpers.is_file(content) else str(content).strip()
     syntax = 'indented' if indented else 'scss'
     safe_content = json.dumps(content).replace('<', r'\u003c')
-    add_head_html(f'''
-        <script type="module">
-            import * as sass from "sass";
-            addStyle(sass.compileString({safe_content}, {{syntax: "{syntax}"}}).css);
-        </script>
+    _add_javascript(f'''
+        import("sass").then(sass => addStyle(sass.compileString({safe_content}, {{syntax: "{syntax}"}}).css));
     ''', shared=shared)
 
 
@@ -58,3 +56,14 @@ def add_sass(content: Union[str, Path], *, shared: bool = False) -> None:  # DEP
     :param shared: whether to add the code to all pages (default: ``False``, *added in version 2.14.0*)
     """
     add_scss(content, indented=True, shared=shared)
+
+
+def _add_javascript(code: str, *, shared: bool = False) -> None:
+    script_html = f'<script>{code}</script>'
+    client = context.client
+    if shared:
+        Client.shared_head_html += script_html + '\n'
+    else:
+        client._head_html += script_html + '\n'
+    if client.has_socket_connection:
+        client.run_javascript(code)

nicegui/testing/general.py
@@ -51,6 +51,8 @@ def nicegui_reset_globals():
 
     Client.instances.clear()
     Client.page_routes.clear()
+    Client.shared_head_html = ''
+    Client.shared_body_html = ''
     app.reset()
     binding.reset()
 

tests/test_add_html.py
@@ -1,3 +1,5 @@
+import pytest
+
 from nicegui import ui
 from nicegui.testing import Screen
 
@@ -17,23 +19,33 @@ def test_add_head_html(screen: Screen) -> None:
     assert screen.find('Label').value_of_css_property('background-color') == 'rgba(0, 255, 0, 1)'
 
 
-def test_css(screen: Screen):
+@pytest.mark.parametrize('shared', [False, True])
+@pytest.mark.parametrize('delayed', [False, True])
+def test_css(screen: Screen, shared: bool, delayed: bool):
     @ui.page('/')
-    def page():
+    async def page():
+        if delayed:
+            await ui.context.client.connected()
         ui.add_css('''
             .red {
                 color: red;
             }
-        ''')
+        ''', shared=shared)
         ui.label('This is red with CSS.').classes('red')
 
     screen.open('/')
-    assert screen.find('This is red with CSS.').value_of_css_property('color') == 'rgba(255, 0, 0, 1)'
+    label = screen.find('This is red with CSS.')
+    screen.wait(0.5)
+    assert label.value_of_css_property('color') == 'rgba(255, 0, 0, 1)'
 
 
-def test_scss(screen: Screen):
+@pytest.mark.parametrize('shared', [False, True])
+@pytest.mark.parametrize('delayed', [False, True])
+def test_scss(screen: Screen, shared: bool, delayed: bool):
     @ui.page('/')
-    def page():
+    async def page():
+        if delayed:
+            await ui.context.client.connected()
         ui.add_scss('''
             .green {
                 background-color: lightgreen;
@@ -41,28 +53,36 @@ def test_scss(screen: Screen):
                     color: blue;
                 }
             }
-        ''')
+        ''', shared=shared)
         with ui.element().classes('green'):
             ui.label('This is blue on green with SCSS.').classes('blue')
 
     screen.open('/')
-    assert screen.find('This is blue on green with SCSS.').value_of_css_property('color') == 'rgba(0, 0, 255, 1)'
+    label = screen.find('This is blue on green with SCSS.')
+    screen.wait(0.5)
+    assert label.value_of_css_property('color') == 'rgba(0, 0, 255, 1)'
 
 
-def test_sass(screen: Screen):
+@pytest.mark.parametrize('shared', [False, True])
+@pytest.mark.parametrize('delayed', [False, True])
+def test_sass(screen: Screen, shared: bool, delayed: bool):
     @ui.page('/')
-    def page():
+    async def page():
+        if delayed:
+            await ui.context.client.connected()
         ui.add_sass('''
             .yellow
                 background-color: yellow
                 .purple
                     color: purple
-        ''')
+        ''', shared=shared)
         with ui.element().classes('yellow'):
             ui.label('This is purple on yellow with SASS.').classes('purple')
 
     screen.open('/')
-    assert screen.find('This is purple on yellow with SASS.').value_of_css_property('color') == 'rgba(128, 0, 128, 1)'
+    label = screen.find('This is purple on yellow with SASS.')
+    screen.wait(0.5)
+    assert label.value_of_css_property('color') == 'rgba(128, 0, 128, 1)'
 
 
 def test_add_css_with_script(screen: Screen):