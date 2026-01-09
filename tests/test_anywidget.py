c7996b0296035dd9cf3cd30c51ff0f9097d341a8
Sofia Meza Salas <smeza@tower-research.com>
2026-01-09 10:52:59 +0800

    Add regression test for nested state changes

--- tests/test_anywidget.py
+++ tests/test_anywidget.py
@@ -35,3 +35,38 @@ def test_anywidget(screen: Screen):
     screen.click('anywidget: 42')
     screen.click('NiceGUI: 43')
     screen.should_contain('anywidget: 44')
+
+def test_nested_update(screen: Screen):
+    class UpdateWidget(anywidget.AnyWidget):  # pylint: disable=abstract-method
+        _esm = '''
+        function render({model, el}) {
+            const div = document.createElement("div");
+            div.innerText = "nothing";
+            el.appendChild(div);
+
+            model.on("change:b", () => {
+                const b = model.get("b");
+                div.innerText = `b=${b}`;
+            });
+
+            model.set("a", 1);
+            model.save_changes();
+        }
+
+        export default { render };
+        '''
+        a = traitlets.Int(0).tag(sync=True)
+        b = traitlets.Int(0).tag(sync=True)
+
+    @ui.page('/')
+    def page():
+        uw = UpdateWidget()
+        def change_b(change):
+            assert change['name'] == 'a'
+            assert change['new'] == 1
+            uw.b = 1
+        uw.observe(change_b, names=['a'])
+        ui.anywidget(uw)
+
+    screen.open('/')
+    screen.should_contain('b=1')
