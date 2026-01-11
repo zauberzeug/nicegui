64edca667c9743374b7808b2e61f760559f440fb
evnchn <evanchan040511@gmail.com>
2026-01-11 15:37:26 +0000

    ui.log scroll-to-bottom working in ui.tab_panel using onScroll

--- nicegui/elements/log.js
+++ nicegui/elements/log.js
@@ -1,18 +1,19 @@
 export default {
-  template: `<q-scroll-area ref="qRef"><slot></slot></q-scroll-area>`,
+  template: `<q-scroll-area ref="qRef" @scroll="onScroll"><slot></slot></q-scroll-area>`,
   data() {
     return {
       shouldScroll: true,
     };
   },
-  beforeUpdate() {
-    if (this.$refs.qRef) {
-      this.shouldScroll = this.$refs.qRef.getScroll().verticalPercentage == 1.0;
-    }
-  },
   updated() {
     if (this.$refs.qRef && this.shouldScroll) {
       this.$nextTick(() => this.$refs.qRef.setScrollPosition("vertical", Number.MAX_SAFE_INTEGER));
     }
+  },
+  methods: {
+    onScroll() {
+      if (!this.$refs.qRef.$el.childNodes[0].clientHeight) return;
+      this.shouldScroll = this.$refs.qRef.getScroll().verticalPercentage == 1.0;
+    },
   },
 };
