22ebaad55932c8c940da88f9b15fab4bd553d775
evnchn <evanchan040511@gmail.com>
2026-01-11 16:18:20 +0000

    Do not run `update_method` for not-yet-mounted elements

--- nicegui/static/nicegui.js
+++ nicegui/static/nicegui.js
@@ -410,7 +410,7 @@ function createApp(elements, options) {
 
           await this.$nextTick();
           for (const [id, element] of Object.entries(msg)) {
-            if (element?.update_method) {
+            if (getElement(id) && element?.update_method) {
               getElement(id)[element.update_method]();
             }
           }
