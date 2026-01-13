4f39a0d0ecc9b4881d96c1e60d0305ae3bcedba8
evnchn <evanchan040511@gmail.com>
2026-01-13 02:23:26 +0000

    Enhancement on `test_with_tab_objects`

--- tests/test_tabs.py
+++ tests/test_tabs.py
@@ -22,17 +22,22 @@ def test_with_strings(screen: Screen):
 
 
 def test_with_tab_objects(screen: Screen):
+    event_tabs = []
+    event_tab_panels = []
+
     @ui.page('/')
     def page():
-        with ui.tabs() as tabs:
+        with ui.tabs(on_change=lambda e: event_tabs.append(e.value)) as tabs:
             tab1 = ui.tab('One')
             tab2 = ui.tab('Two')
 
-        with ui.tab_panels(tabs, value=tab2):
+        with ui.tab_panels(tabs, value=tab2, on_change=lambda e: event_tab_panels.append(e.value)) as tab_panels:
             with ui.tab_panel(tab1):
                 ui.label('First tab')
             with ui.tab_panel(tab2):
                 ui.label('Second tab')
+
+        ui.button('Change tab_panels to Two', on_click=lambda: tab_panels.set_value(tab2))
 
     screen.open('/')
     screen.should_contain('One')
@@ -40,3 +45,7 @@ def test_with_tab_objects(screen: Screen):
     screen.should_contain('Second tab')
     screen.click('One')
     screen.should_contain('First tab')
+    screen.click('Change tab_panels to Two')
+    screen.should_contain('Second tab')
+    assert event_tabs == ['Two', 'One', 'Two']
+    assert event_tab_panels == ['One', 'Two']
