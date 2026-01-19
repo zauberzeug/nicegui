ff13a588a5d26f946bce4dd1a896296406b71bd6
Falko Schindler <falko@zauberzeug.com>
2026-01-19 16:58:10 +0100

    include a custom color in pytest

--- tests/test_colors.py
+++ tests/test_colors.py
@@ -3,14 +3,16 @@ from nicegui.testing import Screen
 
 
 def test_appwide_colors(screen: Screen):
-    app.colors(primary='#00ffff')
+    app.colors(primary='#ff0000', brand='#00ff00')
 
     @ui.page('/')
     def page():
         ui.button('Test Button')
+        ui.button('Brand Button', color='brand')
 
     screen.open('/')
-    assert screen.find_by_tag('button').value_of_css_property('background-color') == 'rgba(0, 255, 255, 1)'
+    assert screen.find_all_by_tag('button')[0].value_of_css_property('background-color') == 'rgba(255, 0, 0, 1)'
+    assert screen.find_all_by_tag('button')[1].value_of_css_property('background-color') == 'rgba(0, 255, 0, 1)'
 
 
 def test_replace_colors(screen: Screen):
