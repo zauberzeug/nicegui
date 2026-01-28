04a7ae4fb3a30ab9ee2335ae915d130dbe554068
Falko Schindler <falko@zauberzeug.com>
2026-01-28 16:43:01 +0100

    remove unused return value from `_on_connect`

--- nicegui/nicegui.py
+++ nicegui/nicegui.py
@@ -188,11 +188,10 @@ async def _exception_handler_500(request: Request, exception: Exception) -> Resp
 
 
 @sio.on('connect')
-async def _on_connect(sid: str, data: dict[str, Any], _=None) -> bool:
+async def _on_connect(sid: str, data: dict[str, Any], _=None) -> None:
     query = {k: v[0] for k, v in urllib.parse.parse_qs(data.get('QUERY_STRING', '')).items()}
     if query.get('implicit_handshake', '') == 'true' and not await _on_handshake(sid, query):
         raise socketio.exceptions.ConnectionRefusedError('Implicit handshake failed')
-    return True
 
 
 @sio.on('handshake')
