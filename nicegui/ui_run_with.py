c8314fe0fbddfd8e823336d40879172a38964ead
Christian Hattemer <11576107+KrilleGH@users.noreply.github.com>
2026-03-12 15:09:57 +0100

    update the documentation for the storage (#5874)
    ### Motivation
    
    In
    https://github.com/zauberzeug/nicegui/commit/ab6aeae5c4618753b098d62dba180255c2e5f303
    `app.storage.individual` was renamed to `app.storage.user`. Leftover
    references to the old name in the docs should be updated.
    
    ### Implementation
    
    Simple update.
    
    ### Progress
    
    - [x] I chose a meaningful title that completes the sentence: "If
    applied, this PR will..."
    - [x] The implementation is complete.
    - [x] If this PR addresses a security issue, it has been coordinated via
    the [security
    advisory](https://github.com/zauberzeug/nicegui/security/advisories/new)
    process.
    - [x] Pytests have been added (or are not necessary).
    - [x] Documentation has been added (or is not necessary).
    
    Co-authored-by: Christian Hattemer <christian.hattemer@lst.at>

--- nicegui/ui_run_with.py
+++ nicegui/ui_run_with.py
@@ -59,7 +59,7 @@ def run_with(
     :param tailwind: whether to use Tailwind CSS (experimental, default: `True`)
     :param unocss: use UnoCSS with the specified preset instead of Tailwind CSS (default: ``None``, options: "mini", "wind3", "wind4", *added in version 3.7.0*)
     :param prod_js: whether to use the production version of Vue and Quasar dependencies (default: `True`)
-    :param storage_secret: secret key for browser-based storage (default: `None`, a value is required to enable ui.storage.individual and ui.storage.browser)
+    :param storage_secret: secret key for browser-based storage (default: `None`, a value is required to enable ui.storage.user and ui.storage.browser)
     :param session_middleware_kwargs: additional keyword arguments passed to SessionMiddleware that creates the session cookies used for browser-based storage
     :param show_welcome_message: whether to show the welcome message (default: `True`)
     """
