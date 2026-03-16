4db503df52397a2d37a9592fce845f9d90a0e17b
Falko Schindler <falko@zauberzeug.com>
2026-03-16 09:40:54 +0100

    break long line to avoid the need for nopep8 marker

--- website/documentation/intro.py
+++ website/documentation/intro.py
@@ -35,7 +35,8 @@ def create_intro() -> None:
                 {'name': 'New York', 'lat': 40.7119, 'lon': -74.0027},
                 {'name': 'London', 'lat': 51.5074, 'lon': -0.1278},
                 {'name': 'Tokyo', 'lat': 35.6863, 'lon': 139.7722},
-            ]).on('row-click', lambda e: sub_pages._render('/map/{lat}/{lon}', lat=e.args[1]['lat'], lon=e.args[1]['lon']))  # nopep8 # HIDE
+            ]).on('row-click',  # HIDE
+                  lambda e: sub_pages._render('/map/{lat}/{lon}', lat=e.args[1]['lat'], lon=e.args[1]['lon']))  # HIDE
             # ]).on('row-click', lambda e: ui.navigate.to(f'/map/{e.args[1]["lat"]}/{e.args[1]["lon"]}'))
 
         def map_page(lat: float, lon: float):
