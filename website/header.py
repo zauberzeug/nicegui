cc38eca10916f237799ab69943477e66ef46678c
Falko Schindler <falko@zauberzeug.com>
2026-03-20 14:28:40 +0100

    load fonts more consistently

--- website/header.py
+++ website/header.py
@@ -16,7 +16,7 @@ STYLE_CSS = (Path(__file__).parent / 'static' / 'style.css').read_text(encoding=
 FONT_LINKS = '''
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Fira+Mono&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet" />
     <link href="https://unpkg.com/@phosphor-icons/web@2.1.1/src/duotone/style.css" rel="stylesheet" />
 '''
 
