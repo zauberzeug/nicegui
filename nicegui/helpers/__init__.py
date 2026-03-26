7a960fda66f0aa536e9fb6c31994ec45e7c1f006
Falko Schindler <falko@zauberzeug.com>
2026-03-26 21:49:16 +0100

    add deprecation wrappers for is_pytest and is_user_simulation

--- nicegui/helpers/__init__.py
+++ nicegui/helpers/__init__.py
@@ -1,3 +1,4 @@
+from .. import core
 from .elements import require_top_level_layout
 from .files import hash_file_path, is_file
 from .functions import expects_arguments, is_coroutine_function
@@ -12,8 +13,30 @@ __all__ = [
     'is_coroutine_function',
     'is_file',
     'is_port_open',
+    'is_pytest',
+    'is_user_simulation',
     'kebab_to_camel_case',
     'require_top_level_layout',
     'schedule_browser',
     'warn_once',
 ]
+
+
+def is_pytest() -> bool:  # DEPRECATED
+    """Check if the code is running in pytest.
+
+    .. deprecated::
+        Use ``core.is_pytest()`` instead.
+    """
+    warn_once('helpers.is_pytest() is deprecated, use core.is_pytest() instead')
+    return core.is_pytest()
+
+
+def is_user_simulation() -> bool:  # DEPRECATED
+    """Check if the code is running with user simulation.
+
+    .. deprecated::
+        Use ``core.is_user_simulation()`` instead.
+    """
+    warn_once('helpers.is_user_simulation() is deprecated, use core.is_user_simulation() instead')
+    return core.is_user_simulation()
