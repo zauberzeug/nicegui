f2921ed2ea8c93ca75f480e0a7740b0eb6963353
Falko Schindler <falko@zauberzeug.com>
2026-03-30 14:58:00 +0200

    mention new functions in overview.py

--- website/documentation/content/overview.py
+++ website/documentation/content/overview.py
@@ -456,8 +456,8 @@ def map_of_nicegui():
 
         Run async functions in the background.
 
-        - `create()`: create a background task
-        - `create_lazy()`: prevent two tasks with the same name from running at the same time
+        - `create()`, `create_or_defer()`: create a background task
+        - `create_lazy()`, `create_lazy_or_defer()`: prevent two tasks with the same name from running at the same time
         - `await_on_shutdown`: mark an async function to be awaited during shutdown (by default all background tasks are cancelled)
 
         #### `run`
