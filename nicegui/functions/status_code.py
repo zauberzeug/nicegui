3ba8ec3db676726ed31e6c09c683dd397aa95004
Falko Schindler <falko@zauberzeug.com>
2026-04-01 13:34:04 +0200

    add version information

--- nicegui/functions/status_code.py
+++ nicegui/functions/status_code.py
@@ -7,6 +7,8 @@ def status_code(code: int) -> None:
     Set the HTTP status code for the current page response.
 
     :param code: HTTP status code (e.g. 200, 404, 503)
+
+    *Added in version 3.10.0*
     """
     if not 100 <= code <= 599:
         raise ValueError(f'Invalid HTTP status code: {code}')
