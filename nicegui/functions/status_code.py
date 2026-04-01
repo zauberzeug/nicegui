334919f376904dab6ddd54d3cf9fd0ed6bb65175
Falko Schindler <falko@zauberzeug.com>
2026-04-01 13:38:06 +0200

    add timing note to docstring

--- nicegui/functions/status_code.py
+++ nicegui/functions/status_code.py
@@ -5,10 +5,11 @@ def status_code(code: int) -> None:
     """Status code
 
     Set the HTTP status code for the current page response.
-
-    :param code: HTTP status code (e.g. 200, 404, 503)
+    Must be called during page building, before the response is sent to the client.
 
     *Added in version 3.10.0*
+
+    :param code: HTTP status code (e.g. 200, 404, 503)
     """
     if not 100 <= code <= 599:
         raise ValueError(f'Invalid HTTP status code: {code}')
