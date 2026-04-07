6c217f73c61692ace54147c7bfb2f89c0d513dae
Torben <59419684+entorb@users.noreply.github.com>
2026-04-07 20:31:37 +0200

    reverted ruff formating

--- examples/trello_cards/draganddrop.py
+++ examples/trello_cards/draganddrop.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Protocol
+from collections.abc import Callable
+from typing import Protocol
 
 from nicegui import ui
-
-if TYPE_CHECKING:
-    from collections.abc import Callable
 
 
 class Item(Protocol):
@@ -17,23 +15,22 @@ drop_target: card | None = None
 
 
 class column(ui.column):
-    def __init__(
-        self, name: str, on_drop: Callable[[Item, str], None] | None = None
-    ) -> None:
+
+    def __init__(self, name: str, on_drop: Callable[[Item, str], None] | None = None) -> None:
         super().__init__()
-        with self.classes("bg-blue-grey-2 w-60 p-4 rounded shadow-2"):
-            ui.label(name).classes("text-bold ml-1")
+        with self.classes('bg-blue-grey-2 w-60 p-4 rounded shadow-2'):
+            ui.label(name).classes('text-bold ml-1')
         self.name = name
-        self.on("dragover.prevent", self.highlight)
-        self.on("dragleave", self.unhighlight)
-        self.on("drop", self.move_card)
+        self.on('dragover.prevent', self.highlight)
+        self.on('dragleave', self.unhighlight)
+        self.on('drop', self.move_card)
         self.on_drop = on_drop
 
     def highlight(self) -> None:
-        self.classes(remove="bg-blue-grey-2", add="bg-blue-grey-3")
+        self.classes(remove='bg-blue-grey-2', add='bg-blue-grey-3')
 
     def unhighlight(self) -> None:
-        self.classes(remove="bg-blue-grey-3", add="bg-blue-grey-2")
+        self.classes(remove='bg-blue-grey-3', add='bg-blue-grey-2')
 
     def move_card(self) -> None:
         global dragged, drop_target  # pylint: disable=global-statement # noqa: PLW0603
@@ -53,13 +50,14 @@ class column(ui.column):
 
 
 class card(ui.card):
+
     def __init__(self, item: Item) -> None:
         super().__init__()
         self.item = item
-        with self.props("draggable").classes("w-full cursor-pointer bg-grey-1"):
+        with self.props('draggable').classes('w-full cursor-pointer bg-grey-1'):
             ui.label(item.title)
-        self.on("dragstart", self.handle_dragstart)
-        self.on("dragover.prevent", self.handle_dragover)
+        self.on('dragstart', self.handle_dragstart)
+        self.on('dragover.prevent', self.handle_dragover)
 
     def handle_dragstart(self) -> None:
         global dragged  # pylint: disable=global-statement # noqa: PLW0603
