bc2c5aaca85c7499eaa906463893c19af2e6fe32
Falko Schindler <falko@zauberzeug.com>
2026-05-05 14:34:20 +0200

    auto-format index.mjs

--- nicegui/elements/uplot/src/index.mjs
+++ nicegui/elements/uplot/src/index.mjs
@@ -1,5 +1,5 @@
-import 'uplot/dist/uPlot.min.css';
-import { optionsUpdateState, dataMatch } from '../utils/uplot_helpers.js';
-import uPlot from 'uplot';
+import "uplot/dist/uPlot.min.css";
+import { optionsUpdateState, dataMatch } from "../utils/uplot_helpers.js";
+import uPlot from "uplot";
 
 export { uPlot, optionsUpdateState, dataMatch };
