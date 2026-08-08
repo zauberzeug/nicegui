2d2254238a1ae2bf4131a44dea7bae3e4e2eff66
SaadZahem <saadzahem7@gmail.com>
2026-08-08 20:21:31 +0300

    Removed guard added at beginning of #6258

--- nicegui/functions/refreshable.py
+++ nicegui/functions/refreshable.py
@@ -128,10 +128,8 @@ class refreshable(Generic[_P, _T]):
             target.kwargs.update(kwargs)
             try:
                 result = target.run(self.func, report_exceptions=report_exceptions)
-            except Exception as e:
-                if not context.slot_stack and not target.container.is_deleted:
-                    target.container.client.handle_exception(e)
-                if isinstance(e, TypeError) and 'got multiple values for argument' in str(e):
+            except TypeError as e:
+                if 'got multiple values for argument' in str(e):
                     function = str(e).split()[0].split('.')[-1]
                     parameter = str(e).split()[-1]
                     raise TypeError(f'{parameter} needs to be consistently passed to {function} '
