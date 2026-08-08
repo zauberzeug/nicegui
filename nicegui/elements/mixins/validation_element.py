0611fab8bdf129d96469a40731b569357dc61398
SaadZahem <saadzahem7@gmail.com>
2026-08-08 19:23:42 +0300

    Remove unused import

--- nicegui/elements/mixins/validation_element.py
+++ nicegui/elements/mixins/validation_element.py
@@ -4,7 +4,6 @@ from typing import Any, TypeAlias, cast
 from typing_extensions import Self
 
 from ... import background_tasks, helpers
-from ...context import context
 from ...events import ValueT
 from .value_element import ValueElement
 
