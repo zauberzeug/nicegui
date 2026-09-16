596ae3391ba5756fe92610c523783399d7580be4
Falko Schindler <falko@zauberzeug.com>
2026-09-16 12:27:22 +0200

    Update nicegui/client.py
    Co-authored-by: Evan Chan <37951241+evnchn@users.noreply.github.com>

--- nicegui/client.py
+++ nicegui/client.py
@@ -482,7 +482,7 @@ class Client:
                     else:
                         result = cast(Callable[[], Any], handler)()
             except Exception:  # one failing handler must not prevent the others from running
-                log.exception('Exception handler %s raised an exception', handler.__name__)
+                log.exception('Exception handler %s raised an exception', getattr(handler, '__name__', handler))
                 continue
             if helpers.should_await(result):
                 background_tasks.create(helpers.await_with_context(result, self.content),
